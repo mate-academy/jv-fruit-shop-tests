061ffc5ea1344276732c4660d59876e85415d20f
Ihor-Nykyforov <kifa1203@gmail.com>
2021-05-05 17:02:24 +0300

    Copied fruit-shop

---
+++ src/main/java/core/basesyntax/dto/FruitRecordDto.java
@@ -0,0 +1,44 @@
+package core.basesyntax.dto;
+
+import core.basesyntax.service.OperationType;
+
+import java.util.Objects;
+
+public class FruitRecordDto {
+    private OperationType operationType;
+    private String fruitName;
+    private int quantity;
+
+    public FruitRecordDto(OperationType operation, String fruit, int quantity) {
+        this.operationType = operation;
+        this.fruitName = fruit;
+        this.quantity = quantity;
+    }
+
+    public OperationType getOperationType() {
+        return operationType;
+    }
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FruitRecordDto that = (FruitRecordDto) o;
+        return quantity == that.quantity &&
+                operationType == that.operationType &&
+                Objects.equals(fruitName, that.fruitName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operationType, fruitName, quantity);
+    }
+}

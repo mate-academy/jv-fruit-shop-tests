e848843f504196b66f945646e0262d6d4f0fe123
Veronika <koraf9712@gmail.com>
2021-05-06 00:28:35 +0300

    Added all fruit shop files

---
+++ src/main/java/core/basesyntax/service/FruitService.java
@@ -0,0 +1,11 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.dto.FruitRecordDto;
+
+import java.util.List;
+
+public interface FruitService {
+    String getFruitReport();
+
+    void applyOperation(List<FruitRecordDto> fruitRecordDtoList);
+}

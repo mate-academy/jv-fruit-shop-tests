39fbfe760e06d052ee55342f0000cf54ec1121d5
the5tranger <mishaantonyuk00@gmail.com>
2021-05-06 16:29:34 +0300

    IT`S ALL

--- src/test/java/core/basesyntax/operations/AddOperationTest.java
+++ src/test/java/core/basesyntax/operations/AddOperationTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import core.basesyntax.dto.ProductDto;
 import core.basesyntax.storage.Storage;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 

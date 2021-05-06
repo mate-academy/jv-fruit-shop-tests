7b8156b8c1a2db13bd9e9588d177ac13e13566c2
Ksenia <ksenia.obertii@gmail.com>
2021-05-06 10:44:25 +0300

    changed files naming

--- src/test/java/core/basesyntax/service/fileservice/FileReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/service/fileservice/FileReaderServiceImplTest.java
@@ -9,9 +9,9 @@ import org.junit.Test;
 
 public class FileReaderServiceImplTest {
     private static FileReaderService fileReaderService;
-    private static final String NOT_EXIST_FILEPATH = "src/test/resources/invalid.csv";
-    private static final String EMPTY_FILEPATH = "src/test/resources/empty.csv";
-    private static final String VALID_FILEPATH = "src/test/resources/valid.csv";
+    private static final String NOT_EXIST_FILE = "src/test/resources/invalid.csv";
+    private static final String EMPTY_FILE = "src/test/resources/empty.csv";
+    private static final String VALID_FILE = "src/test/resources/valid.csv";
 
     @BeforeClass
     public static void beforeClass() {
@@ -25,13 +25,13 @@ public class FileReaderServiceImplTest {
 
     @Test(expected = RuntimeException.class)
     public void fileReaderService_filePathNotExist_notOk() {
-        fileReaderService.read(NOT_EXIST_FILEPATH);
+        fileReaderService.read(NOT_EXIST_FILE);
     }
 
     @Test
     public void fileReaderService_readEmptyFile_isOk() {
         List<String> expected = new ArrayList<>();
-        List<String> actual = fileReaderService.read(EMPTY_FILEPATH);
+        List<String> actual = fileReaderService.read(EMPTY_FILE);
         assertEquals(expected, actual);
     }
 
@@ -40,7 +40,7 @@ public class FileReaderServiceImplTest {
         List<String> expected = List.of("b,banana,20",
                 "b,apple,100",
                 "s,banana,100");
-        List<String> actual = fileReaderService.read(VALID_FILEPATH);
+        List<String> actual = fileReaderService.read(VALID_FILE);
         assertEquals(expected, actual);
     }
 }
--- src/test/java/core/basesyntax/service/fileservice/FileWriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/fileservice/FileWriterServiceImplTest.java
@@ -14,9 +14,9 @@ public class FileWriterServiceImplTest {
     private static FileWriterService fileWriterService;
     private static final String EMPTY_DATA = "";
     private static final String VALID_DATA = "b,banana,20\nb,apple,100\ns,banana,100";
-    private static final String VALID_FILEPATH = "src/test/resources/valid.csv";
-    private static final String INVALID_FILEPATH = "serece/test/invalid.csv";
-    private static final String WRITER_RESULT_FILEPATH = "src/test/resources/writerResult.csv";
+    private static final String VALID_FILE = "src/test/resources/valid.csv";
+    private static final String INVALID_FILE = "serece/test/invalid.csv";
+    private static final String WRITER_RESULT_FILE = "src/test/resources/writerResult.csv";
     private static final String EXCEPTION_MESSAGE = "Can't write data to file";
 
     @BeforeClass
@@ -26,7 +26,7 @@ public class FileWriterServiceImplTest {
 
     @Test(expected = RuntimeException.class)
     public void fileWriterService_dataIsNull_notOk() {
-        fileWriterService.write(null, WRITER_RESULT_FILEPATH);
+        fileWriterService.write(null, WRITER_RESULT_FILE);
     }
 
     @Test(expected = RuntimeException.class)
@@ -36,16 +36,16 @@ public class FileWriterServiceImplTest {
 
     @Test(expected = RuntimeException.class)
     public void fileWriterService_invalidFilePath_notOk() {
-        fileWriterService.write(VALID_DATA, INVALID_FILEPATH);
+        fileWriterService.write(VALID_DATA, INVALID_FILE);
     }
 
     @Test
     public void fileWriterService_dataIsEmpty_isOk() {
-        fileWriterService.write(EMPTY_DATA, WRITER_RESULT_FILEPATH);
+        fileWriterService.write(EMPTY_DATA, WRITER_RESULT_FILE);
         List<String> expected = new ArrayList<>();
         List<String> actual;
         try {
-            actual = Files.readAllLines(Path.of(WRITER_RESULT_FILEPATH));
+            actual = Files.readAllLines(Path.of(WRITER_RESULT_FILE));
         } catch (IOException e) {
             throw new RuntimeException(EXCEPTION_MESSAGE);
         }
@@ -54,12 +54,12 @@ public class FileWriterServiceImplTest {
 
     @Test
     public void fileWriterService_writeFile_isOk() {
-        fileWriterService.write(VALID_DATA, WRITER_RESULT_FILEPATH);
+        fileWriterService.write(VALID_DATA, WRITER_RESULT_FILE);
         List<String> expected;
         List<String> actual;
         try {
-            expected = Files.readAllLines(Path.of(WRITER_RESULT_FILEPATH));
-            actual = Files.readAllLines(Path.of(VALID_FILEPATH));
+            expected = Files.readAllLines(Path.of(WRITER_RESULT_FILE));
+            actual = Files.readAllLines(Path.of(VALID_FILE));
         } catch (IOException e) {
             throw new RuntimeException(EXCEPTION_MESSAGE);
         }

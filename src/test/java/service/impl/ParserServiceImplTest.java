d509d84b2142f70c3cd5cce9d30ab802f759ad31
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-05-06 16:29:17 +0300

    Renamed some variables in ParserServiceImplTest class.

--- src/test/java/service/impl/ParserServiceImplTest.java
+++ src/test/java/service/impl/ParserServiceImplTest.java
@@ -19,35 +19,35 @@ public class ParserServiceImplTest {
 
     @Test
     public void parseToDto_validData_Ok() {
-        List<String> dataFromFile = List.of("b,banana,20", "b,apple,30");
+        List<String> validOperationData = List.of("b,banana,20", "b,apple,30");
         List<FruitRecordDto> expected = List.of(
                 new FruitRecordDto(OperationType.BALANCE, "banana", 20),
                 new FruitRecordDto(OperationType.BALANCE, "apple", 30));
-        List<FruitRecordDto> actual = parserService.parseToDto(dataFromFile);
+        List<FruitRecordDto> actual = parserService.parseToDto(validOperationData);
         assertEquals(expected, actual);
     }
 
     @Test(expected = RuntimeException.class)
     public void parseToDto_invalidOperationType_RuntimeException() {
-        List<String> dataFromFile = List.of("c,banana,20", "b,apple,30");
-        parserService.parseToDto(dataFromFile);
+        List<String> invalidOperationData  = List.of("c,banana,20", "b,apple,30");
+        parserService.parseToDto(invalidOperationData );
     }
 
     @Test(expected = RuntimeException.class)
     public void parseToDto_invalidFruitName_RuntimeException() {
-        List<String> dataFromFile = List.of("b,ban11ana,20", "b,apple,30");
-        parserService.parseToDto(dataFromFile);
+        List<String> invalidOperationData = List.of("b,ban11ana,20", "b,apple,30");
+        parserService.parseToDto(invalidOperationData);
     }
 
     @Test(expected = RuntimeException.class)
     public void parseToDto_negativeQuantity_RuntimeException() {
-        List<String> dataFromFile = List.of("b,banana,20", "b,apple,-30");
-        parserService.parseToDto(dataFromFile);
+        List<String> invalidOperationData = List.of("b,banana,20", "b,apple,-30");
+        parserService.parseToDto(invalidOperationData);
     }
 
     @Test(expected = RuntimeException.class)
     public void parseToDto_quantityContainsNonDigits_RuntimeException() {
-        List<String> dataFromFile = List.of("b,banana,2a0", "b,apple,30");
-        parserService.parseToDto(dataFromFile);
+        List<String> invalidOperationData = List.of("b,banana,2a0", "b,apple,30");
+        parserService.parseToDto(invalidOperationData);
     }
 }

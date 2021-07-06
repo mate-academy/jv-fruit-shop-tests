e97cd0bf5a34ee7dd33dc51f2925c42f9695288b
Tamara-Paskalova <forptv@gmail.com>
2021-07-06 22:19:49 +0300

    created tests for all methods

--- src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
@@ -4,13 +4,14 @@ import core.basesyntax.service.WriterService;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 
 public class WriterServiceImpl implements WriterService {
     @Override
     public void writeToFile(String fileName, String report) {
         try {
             Files.writeString(new File(fileName).toPath(), report);
-        } catch (IOException e) {
+        } catch (InvalidPathException | IOException e) {
             throw new RuntimeException("Can't write to file " + fileName, e);
         }
     }

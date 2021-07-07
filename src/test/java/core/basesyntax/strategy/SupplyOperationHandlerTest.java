2b7be8903ef59af98e91bd755195c1c1953bd2c5
Nikitozin <nikitozin91@gmail.com>
2021-07-07 13:47:22 +0300

    Changes_In_Supply_Test

--- src/test/java/core/basesyntax/strategy/SupplyOperationHandlerTest.java
+++ src/test/java/core/basesyntax/strategy/SupplyOperationHandlerTest.java
@@ -4,6 +4,7 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.dto.FruitDto;
 import core.basesyntax.model.Fruit;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -32,17 +33,22 @@ public class SupplyOperationHandlerTest {
     }
 
     @Test
-    public void supplyHandler_checkReturnValue_Ok() {
-        FruitDto fruitDto = new FruitDto("s", "apple", 1);
-        int expected = 36;
+    public void supplyHandler_checkReturnTransaction_Ok() {
+        FruitDto fruitDto = new FruitDto("r", "apple", 20);
+        int expected = 55;
         int actual = handler.apply(fruitDto);
         Assert.assertEquals(expected, actual);
     }
 
+    @Before
+    public void setUp() {
+        Storage.storage.put(apple, 35);
+    }
+
     @Test
-    public void supplyHandler_checkReturnTransaction_Ok() {
-        FruitDto fruitDto = new FruitDto("r", "apple", 20);
-        int expected = 56;
+    public void supplyHandler_checkReturnValue_Ok() {
+        FruitDto fruitDto = new FruitDto("s", "apple", 1);
+        int expected = 36;
         int actual = handler.apply(fruitDto);
         Assert.assertEquals(expected, actual);
     }

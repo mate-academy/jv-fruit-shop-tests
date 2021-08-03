c711aa3a70924decdb0f205fa23f28f2a94bcd16
Dana Khromenko <danadiadius@gmail.com>
2021-08-03 11:23:14 +0300

    Removed file creating code in method writeDataToFile

--- src/test/java/core/basesyntax/FileWriter.java
+++ src/test/java/core/basesyntax/FileWriter.java
@@ -1,19 +1,10 @@
 package core.basesyntax;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 
 public class FileWriter {
     public static void writeDataToFile(String filePath, String data) {
-        File file = new File(filePath);
-        if (file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException("Can't create file " + filePath, e);
-            }
-        }
         try (BufferedWriter bufferedWriter = new BufferedWriter(
                 new java.io.FileWriter(filePath, false))) {
             bufferedWriter.write(data);

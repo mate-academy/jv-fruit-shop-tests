ff3851cc698f607981a3ae1bdf12acfeac87defa
Sinelife <ya.serzhan@gmail.com>
2021-09-19 12:43:39 +0300

    added code from previous exercise

---
+++ src/main/java/core/basesyntax/service/ReportMakerService.java
@@ -0,0 +1,9 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.Fruit;
+
+import java.util.Map;
+
+public interface ReportMakerService {
+    String getReport(Map<Fruit, Integer> map);
+}

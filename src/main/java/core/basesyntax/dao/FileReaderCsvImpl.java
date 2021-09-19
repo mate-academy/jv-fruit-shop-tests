4dedd08125be1c17a61c58627845db508d11fb25
Viktor Yukhno <pingvinomania@gmail.com>
2021-09-19 04:19:19 +0300

    deleted some files

--- src/main/java/core/basesyntax/dao/FileReaderCsvImpl.java
+++ src/main/java/core/basesyntax/dao/FileReaderCsvImpl.java
@@ -4,7 +4,7 @@ import core.basesyntax.services.ValidatorService;
 import core.basesyntax.services.ValidatorServiceImpl;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class FileReaderCsvImpl implements FileReader {
@@ -15,7 +15,7 @@ public class FileReaderCsvImpl implements FileReader {
         validatorService.nullInputDataValidator(fileName);
         List<String> dataFromFile;
         try {
-            dataFromFile = Files.readAllLines(Path.of(fileName));
+            dataFromFile = Files.readAllLines(Paths.get(fileName));
         } catch (IOException e) {
             throw new RuntimeException("Can't get data from file");
         }

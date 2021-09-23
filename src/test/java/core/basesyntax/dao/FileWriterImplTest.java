465453bb0d13113cddbe591d6ae1ea1ff1545b6b
Gleb Kreshchuk <gleb.net@gmail.com>
2021-09-23 16:37:36 +0300

    FruitShop Test Task completed according requirements

--- src/test/java/core/basesyntax/dao/FileWriterImplTest.java
+++ src/test/java/core/basesyntax/dao/FileWriterImplTest.java
@@ -1,5 +1,9 @@
 package core.basesyntax.dao;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -7,17 +11,34 @@ import org.junit.Test;
 public class FileWriterImplTest {
     public static final String CORRECT_PATH = "src/test/java/resources/output_test.csv";
     public static final String INCORRECT_PATH = "";
+    public static final String TEST_TEXT = "File is written OK!";
+    public static final String FIRST_LINE_WITH_DESCRIPTION = System.lineSeparator();
     private static FileWriterImpl writerService;
+    private static FileReader fileReader;
+    private static FileWriter fileWriter;
+    private List<String> stringFromOutputTestFile;
 
     @BeforeClass
     public static void setUp() {
-        FileWriter fileWriter = new FileWriterImpl();
+        fileWriter = new FileWriterImpl();
+        fileReader = new FileReaderImpl();
     }
 
     @Test
     public void write_correctPath_ok() {
-        FileWriter fileWriter = new FileWriterImpl();
-        Assert.assertTrue("File is written!", fileWriter.write(CORRECT_PATH, ""));
+        File file = new File(CORRECT_PATH);
+        file.delete();
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file " + CORRECT_PATH, e);
+        }
+        List<String> expected = new ArrayList<String>();
+        expected.add(TEST_TEXT);
+        fileWriter.write(CORRECT_PATH, FIRST_LINE_WITH_DESCRIPTION);
+        fileWriter.write(CORRECT_PATH, TEST_TEXT);
+        List<String> actual = fileReader.read(CORRECT_PATH);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test

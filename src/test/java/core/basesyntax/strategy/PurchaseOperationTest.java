e00bfc11cf06dc2366d2dd579ff366c110c9965b
Kristina Voytikh <61868920+kvoytikh@users.noreply.github.com>
2021-09-23 18:10:53 +0300

    purchase fixed
    purchase fixed

--- src/test/java/core/basesyntax/strategy/PurchaseOperationTest.java
+++ src/test/java/core/basesyntax/strategy/PurchaseOperationTest.java
@@ -15,15 +15,22 @@ public class PurchaseOperationTest {
     @Before
     public void setUp() {
         operation = new PurchaseOperation();
-        transaction = new Transaction(Transaction.Operation.PURCHASE,
-                new Fruit("banana"), 20);
         Storage.fruitStorage.put("banana", 20);
     }
 
     @Test
     public void apply_Ok() {
+        transaction = new Transaction(Transaction.Operation.PURCHASE,
+                new Fruit("banana"), 20);
         int actual = operation.apply(transaction);
         int expected = 0;
         assertEquals(expected, actual);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void apply_NotEnoughQuantity_NotOk() {
+        transaction = new Transaction(Transaction.Operation.PURCHASE,
+                new Fruit("banana"), 40);
+        operation.apply(transaction);
     }
 }

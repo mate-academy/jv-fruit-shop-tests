c89b84cb16f0ab03e99229708f0f037d6d0f4d6d
Dmytro Heleta <dmytroheleta@gmail.com>
2021-09-23 23:08:34 +0300

    added test files, added CsvFileReader test Class with tests

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -22,8 +22,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private static final String INPUT_FILE = "src/main/input.txt";
-    private static final String OUTPUT_FILE = "src/main/output.txt";
+    private static final String INPUT_FILE = "src/test/resources/input.txt";
+    private static final String OUTPUT_FILE = "src/test/resources/output.txt";
 
     public static void main(String[] args) {
         Map<String, ActivityHandler> activities = new HashMap<>();

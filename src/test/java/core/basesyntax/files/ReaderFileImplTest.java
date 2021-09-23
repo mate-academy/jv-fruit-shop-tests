230db908cac876b7269bded359ac0b3475ee9f93
Vladyslav Gural <vladgural@gmail.com>
2021-09-23 12:10:59 +0300

    decoupled some declaration and initialization variables removed test source file from 'main' to 'test' branch

--- src/test/java/core/basesyntax/files/ReaderFileImplTest.java
+++ src/test/java/core/basesyntax/files/ReaderFileImplTest.java
@@ -2,18 +2,25 @@ package core.basesyntax.files;
 
 import java.util.List;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ReaderFileImplTest {
-    private static final String SOURCE_FILE_GOOD = "src/main/resources/12-09-2021.csv";
-    private static final String SOURCE_FILE_BAD = "src/main/resources/sderfdsde.csv";
-    private Reader readerFileExist = new ReaderFileImpl(SOURCE_FILE_GOOD);
-    private Reader readerFileDoNotExist = new ReaderFileImpl(SOURCE_FILE_BAD);
+    private static final String SOURCE_FILE_GOOD = "src/test/resources/test_reader_file_impl.csv";
+    private static final String SOURCE_FILE_BAD = "src/test/resources/NOT_EXIST.csv";
+    private static Reader readerFileExist;
+    private static Reader readerFileDoNotExist;
+
+    @BeforeClass
+    public static void beforeClass() {
+        readerFileExist = new ReaderFileImpl(SOURCE_FILE_GOOD);
+        readerFileDoNotExist = new ReaderFileImpl(SOURCE_FILE_BAD);
+    }
 
     @Test
     public void fileExist_Ok() {
         List<String> actualList = readerFileExist.read();
-        Assert.assertTrue(actualList != null);
+        Assert.assertNotNull(actualList);
     }
 
     @Test(expected = RuntimeException.class)

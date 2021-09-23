f2d915983e2caec46697186713697ae77d0c23e7
Vladyslav Gural <vladgural@gmail.com>
2021-09-23 12:01:13 +0300

    renamed some variables like 'fr02' to 'expectedFruitRecordDto01', removed test source data to file

--- src/test/java/core/basesyntax/servise/ReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/servise/ReaderServiceImplTest.java
@@ -2,39 +2,28 @@ package core.basesyntax.servise;
 
 import core.basesyntax.files.Reader;
 import core.basesyntax.files.ReaderFileImpl;
-import core.basesyntax.files.Writer;
-import core.basesyntax.files.WriterFileImpl;
 import core.basesyntax.model.FruitRecordDto;
 import core.basesyntax.validation.LineValidator;
 import core.basesyntax.validation.TitleValidator;
 import core.basesyntax.validation.Validator;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ReaderServiceImplTest {
-    private static final String TEST_FILE = "src/main/resources/test.csv";
-    private static final String TEST_LINE_01 = "type,fruit,quantity";
-    private static final String TEST_LINE_02 = "b,banana,20";
-    private static final String TEST_LINE_03 = "p,banana,5";
+    private static final String TEST_FILE = "src/test/resources/test_reader_service_impl.csv";
     private static final String EXPECT_FR02_TYPE = "b";
     private static final String EXPECT_FR02_FRUIT = "banana";
     private static final String EXPECT_FR02_QUANTITY = "20";
     private static final String EXPECT_FR03_TYPE = "p";
     private static final String EXPECT_FR03_FRUIT = "banana";
     private static final String EXPECT_FR03_QUANTITY = "5";
-    private static List<String> testList;
     private static List<FruitRecordDto> expectedList;
     private static Reader reader;
-    private static Writer writer;
-    private static FruitRecordDto fr02;
-    private static FruitRecordDto fr03;
+    private static FruitRecordDto expectredFruitRecordDto01;
+    private static FruitRecordDto expectredFruitRecordDto02;
     private static Validator titleValidator;
     private static Validator lineValidator;
     private static ReaderService readerService;
@@ -42,35 +31,20 @@ public class ReaderServiceImplTest {
     @BeforeClass
     public static void beforeClass() {
         reader = new ReaderFileImpl(TEST_FILE);
-        writer = new WriterFileImpl(TEST_FILE);
-        fr02 = new FruitRecordDto();
-        fr03 = new FruitRecordDto();
-        fr02.setTypeOperation(EXPECT_FR02_TYPE);
-        fr02.setFruit(EXPECT_FR02_FRUIT);
-        fr02.setQuantity(EXPECT_FR02_QUANTITY);
-        fr03.setTypeOperation(EXPECT_FR03_TYPE);
-        fr03.setFruit(EXPECT_FR03_FRUIT);
-        fr03.setQuantity(EXPECT_FR03_QUANTITY);
+        expectredFruitRecordDto01 = new FruitRecordDto();
+        expectredFruitRecordDto02 = new FruitRecordDto();
+        expectredFruitRecordDto01.setTypeOperation(EXPECT_FR02_TYPE);
+        expectredFruitRecordDto01.setFruit(EXPECT_FR02_FRUIT);
+        expectredFruitRecordDto01.setQuantity(EXPECT_FR02_QUANTITY);
+        expectredFruitRecordDto02.setTypeOperation(EXPECT_FR03_TYPE);
+        expectredFruitRecordDto02.setFruit(EXPECT_FR03_FRUIT);
+        expectredFruitRecordDto02.setQuantity(EXPECT_FR03_QUANTITY);
         expectedList = new ArrayList<>();
-        expectedList.add(fr02);
-        expectedList.add(fr03);
-        testList = new ArrayList<>();
-        testList.add(TEST_LINE_01);
-        testList.add(TEST_LINE_02);
-        testList.add(TEST_LINE_03);
+        expectedList.add(expectredFruitRecordDto01);
+        expectedList.add(expectredFruitRecordDto02);
         titleValidator = new TitleValidator();
         lineValidator = new LineValidator();
         readerService = new ReaderServiceImpl(reader, titleValidator, lineValidator);
-        writer.write(testList);
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        try {
-            Files.delete(Path.of(TEST_FILE));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't delete file " + TEST_FILE);
-        }
     }
 
     @Test

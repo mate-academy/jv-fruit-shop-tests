59cd02b950c3c5d8bd2dc43b21243a3cfd979146
SolnalarmJulia <solnalarm@meta.ua>
2021-09-24 18:40:46 +0300

    refactoring

--- src/main/java/core/service/record/FruitRecordServiceImpl.java
+++ src/main/java/core/service/record/FruitRecordServiceImpl.java
@@ -10,6 +10,9 @@ public class FruitRecordServiceImpl implements FruitRecordService {
     public List<FruitRecord> parserFruit(List<String> createList) {
         List<FruitRecord> fruitList = new ArrayList<>();
         Mapper<String, FruitRecord> recordMap = new FruitRecordMapperImpl();
+        if (createList.get(0).equals("type,fruit,quantity")) {
+            createList.remove(0);
+        }
         for (String record : createList) {
             fruitList.add(recordMap.mappingToObject(record));
         }

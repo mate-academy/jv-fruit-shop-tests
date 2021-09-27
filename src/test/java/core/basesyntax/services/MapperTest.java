e9cb8b133dbd979a7440e5a0e5a97bb50a135711
Pasha Rakitin <dogovor@lan.com.ua>
2021-09-27 04:57:50 +0300

    test

---
+++ src/test/java/core/basesyntax/services/MapperTest.java
@@ -0,0 +1,54 @@
+package core.basesyntax.services;
+
+import static core.basesyntax.services.OperationType.BALANCE;
+import static core.basesyntax.services.OperationType.PURCHASE;
+import static core.basesyntax.services.OperationType.RETURN;
+import static core.basesyntax.services.OperationType.SUPPLY;
+import static org.junit.Assert.assertEquals;
+
+import core.basesyntax.model.FruitRecordDto;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapperTest {
+    private static Mapper mapper = new Mapper();
+
+    @Test
+    public void mapper_apply_isOk() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("type,fruit,quantity");
+        stringList.add("b,banana,100");
+        stringList.add("b,apple,100");
+        stringList.add("s,banana,10");
+        stringList.add("s,apple,20");
+        stringList.add("r,apple,7");
+        stringList.add("p,banana,100");
+        stringList.add("s,banana,100");
+        List<FruitRecordDto> expected = new ArrayList<>();
+        expected.add(new FruitRecordDto(BALANCE, "banana", 100));
+        expected.add(new FruitRecordDto(BALANCE, "apple", 100));
+        expected.add(new FruitRecordDto(SUPPLY, "banana", 10));
+        expected.add(new FruitRecordDto(SUPPLY, "apple", 20));
+        expected.add(new FruitRecordDto(RETURN, "apple", 7));
+        expected.add(new FruitRecordDto(PURCHASE, "banana", 100));
+        expected.add(new FruitRecordDto(SUPPLY, "banana", 100));
+        List<FruitRecordDto> actual = mapper.apply(stringList);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mapper_apply_isNotOk() {
+        RuntimeException e = null;
+        List<String> stringList = new ArrayList<>();
+        stringList.add("type,fruit,quantity");
+        stringList.add("b,banana,-100");
+        try {
+            mapper.apply(stringList);
+        } catch (RuntimeException ex) {
+            e = ex;
+        }
+        Assert.assertTrue(e instanceof RuntimeException);
+    }
+}
---
+++ src/test/java/core/basesyntax/services/OperationStrategyImplTest.java
@@ -0,0 +1,57 @@
+package core.basesyntax.services;
+
+import static org.junit.Assert.assertEquals;
+
+import core.basesyntax.operationhanlerservices.BalanceHandler;
+import core.basesyntax.operationhanlerservices.OperationHandler;
+import core.basesyntax.operationhanlerservices.PurchaseHandler;
+import core.basesyntax.operationhanlerservices.ReturnHandler;
+import core.basesyntax.operationhanlerservices.SupplayHandler;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OperationStrategyImplTest {
+    private static OperationStrategy operationStrategy;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Map<OperationType, OperationHandler> operationHandlerMap = new HashMap<>();
+        operationHandlerMap.put(OperationType.BALANCE, new BalanceHandler());
+        operationHandlerMap.put(OperationType.SUPPLY, new SupplayHandler());
+        operationHandlerMap.put(OperationType.RETURN, new ReturnHandler());
+        operationHandlerMap.put(OperationType.PURCHASE, new PurchaseHandler());
+        operationStrategy = new OperationStrategyImpl(operationHandlerMap) {
+        };
+    }
+
+    @Test
+    public void operationStrategy_getBalanceHandler_ok() {
+        String expected = BalanceHandler.class.getName();
+        String actual = operationStrategy.get(OperationType.BALANCE).getClass().getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void operationStrategy_getSupplayHandler_ok() {
+        String expected = SupplayHandler.class.getName();
+        String actual = operationStrategy.get(OperationType.SUPPLY).getClass().getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void operationStrategy_getReturnHandler_ok() {
+        String expected = ReturnHandler.class.getName();
+        String actual = operationStrategy.get(OperationType.RETURN).getClass().getName();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void operationStrategy_getPurchaseHandler_ok() {
+        String expected = PurchaseHandler.class.getName();
+        String actual = operationStrategy.get(OperationType.PURCHASE).getClass().getName();
+        assertEquals(expected, actual);
+    }
+}

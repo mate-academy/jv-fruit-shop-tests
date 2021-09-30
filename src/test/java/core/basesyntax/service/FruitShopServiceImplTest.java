287fa640424f80956ee685acd76f86105a299d1e
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-09-30 11:24:33 +0300

    Implemented FruitShopServiceTest

--- src/test/java/core/basesyntax/service/FruitShopServiceImplTest.java
+++ src/test/java/core/basesyntax/service/FruitShopServiceImplTest.java
@@ -1,36 +1,53 @@
 package core.basesyntax.service;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitOperation;
+import core.basesyntax.service.handler.BalanceHandler;
+import core.basesyntax.service.handler.PurchaseHandler;
+import core.basesyntax.service.handler.ReturnHandler;
+import core.basesyntax.service.handler.SupplyHandler;
+import core.basesyntax.service.inter.FruitOperationListCreateService;
 import core.basesyntax.service.inter.FruitShopService;
 import core.basesyntax.service.inter.Operation;
-import java.util.ArrayList;
+import core.basesyntax.service.inter.ReadService;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FruitShopServiceImplTest {
-    private static List<FruitOperation> incorrectOperations;
     private static List<FruitOperation> correctOperations;
     private static Map<String, Operation> correctOperationsHandlers;
-    private static Map<String, Operation> incorrectOperationsHandlers;
     private static FruitShopService fruitShopService;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
         fruitShopService = new FruitShopServiceImpl();
-        incorrectOperations = new ArrayList<>();
-        //incorrectOperations.add(new FruitOperation("24", "apple", 25));
-        //incorrectOperations.add(new FruitOperation("$6df", "banana", 62));
-        //correctOperations = new ArrayList<>();
-        //correctOperations.add(new FruitOperation("b", "apple", 86));
-        //correctOperations.add(new FruitOperation("p", "banana", 35));
-        //correctOperationsHandlers = new HashMap<>();
-        //correctOperationsHandlers.put("b", new BalanceHandler());
-        //correctOperationsHandlers.put("p", new PurchaseHandler());
+        correctOperationsHandlers = new HashMap<>();
+        correctOperationsHandlers.put("b", new BalanceHandler());
+        correctOperationsHandlers.put("p", new PurchaseHandler());
+        correctOperationsHandlers.put("r", new ReturnHandler());
+        correctOperationsHandlers.put("s", new SupplyHandler());
+        ReadService readService = new ReadServiceImpl();
+        List<String> strings = readService.readFromFile("src/test/java/resources/input.csv");
+        FruitOperationListCreateService fruitOperationListCreateService
+                = new FruitOperationListCreateServiceImpl();
+        correctOperations = fruitOperationListCreateService.getFruitOperationsList(strings);
     }
 
-    ///...
+    @Test
+    public void updateStorage_Ok() {
+        fruitShopService.updateStorageInfo(correctOperations, correctOperationsHandlers);
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("apple", 90);
+        expected.put("banana", 152);
+        Map<String, Integer> actual = Storage.fruitsQuantity;
+        System.out.println(expected);
+        System.out.println(actual);
+        Assert.assertEquals(expected, actual);
+    }
 
     @Test (expected = RuntimeException.class)
     public void nullOperation() {

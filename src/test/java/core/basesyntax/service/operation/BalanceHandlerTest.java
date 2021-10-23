837c6942a3e5a07e4137269bce669cd08a64f4ae
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2021-10-23 23:07:55 +0300

    Added tests for service.operation

--- src/test/java/core/basesyntax/service/operation/BalanceHandlerTest.java
+++ src/test/java/core/basesyntax/service/operation/BalanceHandlerTest.java
@@ -1,10 +1,20 @@
 package core.basesyntax.service.operation;
 
-import org.junit.Before;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class BalanceHandlerTest {
+    private static final String FRUIT_NAME = "apple";
+    private static final int QUANTITY = 123;
+    private static Map<String, Integer> fruitQuantityMap = new HashMap<>();
+    private static final OperationHandler BALANCE_HANDLER = new BalanceHandler();
 
-    @Before
-    public void setUp() {
+    @Test
+    public void checkApply_Ok() {
+        BALANCE_HANDLER.apply(fruitQuantityMap, FRUIT_NAME, QUANTITY);
+        Assert.assertTrue(fruitQuantityMap.containsKey(FRUIT_NAME)
+                && fruitQuantityMap.containsValue(QUANTITY));
     }
 }

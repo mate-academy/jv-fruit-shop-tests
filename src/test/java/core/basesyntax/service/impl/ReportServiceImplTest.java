aafe12d19ed3ac3202135209d48484880e2153da
Zachar Chorny <ufumoveldar@gmail.com>
2021-11-24 19:33:29 +0200

    edits made(2)

---
+++ src/test/java/core/basesyntax/service/impl/ReportServiceImplTest.java
@@ -0,0 +1,25 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.dao.FruitDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.service.ReportService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReportServiceImplTest {
+    private final ReportService report = new ReportServiceImpl(new FruitDaoImpl());
+
+    @After
+    public void setUp() {
+        Storage.fruits.clear();
+    }
+
+    @Test
+    public void report_ValidDate_OK() {
+        Storage.fruits.put(new Fruit("banana"), 20);
+        String expected = "fruit,quantity\n" + "banana,20";
+        Assert.assertEquals(expected, report.createReport());
+    }
+}

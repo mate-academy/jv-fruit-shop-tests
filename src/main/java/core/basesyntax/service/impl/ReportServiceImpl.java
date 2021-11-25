9df169f4a4520d5760b729c5059eaa775252e237
Zachar Chorny <ufumoveldar@gmail.com>
2021-11-25 22:12:26 +0200

    small optimization

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -2,9 +2,6 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.dao.FruitDao;
 import core.basesyntax.service.ReportService;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ReportServiceImpl implements ReportService {
@@ -18,9 +15,8 @@ public class ReportServiceImpl implements ReportService {
     public String createReport() {
         return "fruit,quantity" + System.lineSeparator()
                 + dao.getAll().entrySet().stream()
-                .sorted(Collections.reverseOrder(Map.Entry.comparingByKey(
-                        Comparator.comparingInt(fruit -> -fruit.getName().length()))))
                 .map(map -> map.getKey().getName() + "," + map.getValue().toString())
+                .sorted()
                 .collect(Collectors.joining(System.lineSeparator()));
     }
 }

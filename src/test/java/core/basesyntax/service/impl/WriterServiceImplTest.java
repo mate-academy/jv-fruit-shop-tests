7df40ed4fd16d9f668b107d85e77aad4de1980cf
Sergii Svystun <sergiisvystun@gmail.com>
2021-11-26 10:42:45 +0200

    inside class WriterServiceImplTest Strings with ways to files moved to constants

--- src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
@@ -10,6 +10,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WriterServiceImplTest {
+    private static final String TO_FILE_NAME = "src/test/resources/OutputTestData.csv";
+    private static final String FILE_WITH_ALREADY_INPUT_DATA =
+            "src/test/resources/FileWithTestedText.csv";
+    private static final String EMPTY_FILE = "src/test/resources/EmptyFile.csv";
+    private static final String FROM_FILE_NAME = "src/test/unknownFolder/OutputTestData.csv";
     private static WriterService writerService;
 
     @BeforeClass
@@ -19,51 +24,46 @@ public class WriterServiceImplTest {
 
     @Test(expected = RuntimeException.class)
     public void writeFile_folderWithFileDoesNotExist_notOk() {
-        String fromFileName = "src/test/unknownFolder/OutputTestData.csv";
         String testedInfo = "fruit,quantity";
-        writerService.writeFile(fromFileName, testedInfo);
+        writerService.writeFile(FROM_FILE_NAME, testedInfo);
     }
 
     @Test
     public void writeFile_ok() {
-        String toFileName = "src/test/resources/OutputTestData.csv";
-        String fileWithAlreadyInputData = "src/test/resources/FileWithTestedText.csv";
         String textToWrite = "fruit,quantity" + System.lineSeparator()
                 + "banana,152" + System.lineSeparator()
                 + "apple,90" + System.lineSeparator();
-        writerService.writeFile(toFileName, textToWrite);
+        writerService.writeFile(TO_FILE_NAME, textToWrite);
         List<String> fromWrittenFile;
         try {
-            fromWrittenFile = Files.readAllLines(Path.of(toFileName));
+            fromWrittenFile = Files.readAllLines(Path.of(TO_FILE_NAME));
         } catch (IOException e) {
-            throw new RuntimeException("Can't get data from list" + toFileName, e);
+            throw new RuntimeException("Can't get data from list" + TO_FILE_NAME, e);
         }
         List<String> fromFileWithTestedText;
         try {
-            fromFileWithTestedText = Files.readAllLines(Path.of(fileWithAlreadyInputData));
+            fromFileWithTestedText = Files.readAllLines(Path.of(FILE_WITH_ALREADY_INPUT_DATA));
         } catch (IOException e) {
-            throw new RuntimeException("Can't get data from list" + toFileName, e);
+            throw new RuntimeException("Can't get data from list" + TO_FILE_NAME, e);
         }
         Assert.assertEquals(fromFileWithTestedText,fromWrittenFile);
     }
 
     @Test
     public void writeFile_emptyFile_ok() {
-        String toFileName = "src/test/resources/OutputTestData.csv";
-        String emptyFile = "src/test/resources/EmptyFile.csv";
         String textToWrite = "";
-        writerService.writeFile(toFileName, textToWrite);
+        writerService.writeFile(TO_FILE_NAME, textToWrite);
         List<String> fromWrittenFile;
         try {
-            fromWrittenFile = Files.readAllLines(Path.of(toFileName));
+            fromWrittenFile = Files.readAllLines(Path.of(TO_FILE_NAME));
         } catch (IOException e) {
-            throw new RuntimeException("Can't get data from list" + toFileName, e);
+            throw new RuntimeException("Can't get data from list" + TO_FILE_NAME, e);
         }
         List<String> fromEmptyFile;
         try {
-            fromEmptyFile = Files.readAllLines(Path.of(emptyFile));
+            fromEmptyFile = Files.readAllLines(Path.of(EMPTY_FILE));
         } catch (IOException e) {
-            throw new RuntimeException("Can't get data from list" + toFileName, e);
+            throw new RuntimeException("Can't get data from list" + TO_FILE_NAME, e);
         }
         Assert.assertEquals(fromEmptyFile,fromWrittenFile);
     }

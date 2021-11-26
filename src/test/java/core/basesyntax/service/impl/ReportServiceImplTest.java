4a1f0db7dcc1732586ca0513619aea3b2cdf57f2
Dima Barshay <barshayerum8@gmail.com>
2021-11-26 18:40:51 +0200

    added a test to ReportServiceImplTest

--- src/test/java/core/basesyntax/service/impl/ReportServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportServiceImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.service.ReportService;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -13,6 +14,7 @@ public class ReportServiceImplTest {
     public static final String APPLE = "apple";
     public static final Integer NUMBER_OF_BANANAS = 152;
     public static final Integer NUMBER_OF_APPLES = 90;
+    public static final String FIRST_LINE = "fruit,quantity";
     public static final String CORRECT_REPORT = "fruit,quantity"
             + System.lineSeparator() + "banana,152"
             + System.lineSeparator() + "apple,90";
@@ -23,11 +25,20 @@ public class ReportServiceImplTest {
         reportService = new ReportServiceImpl();
     }
 
+    @Before
+    public void setUp() {
+        Storage.storage.clear();
+    }
+
     @Test
     public void correctReport_Ok() {
-        Storage.storage.clear();
         Storage.storage.put(new Fruit(BANANA), NUMBER_OF_BANANAS);
         Storage.storage.put(new Fruit(APPLE), NUMBER_OF_APPLES);
         assertEquals(CORRECT_REPORT, reportService.formReport());
+    }
+
+    @Test
+    public void emptyStorage_Ok() {
+        assertEquals(FIRST_LINE, reportService.formReport());
     }
 }

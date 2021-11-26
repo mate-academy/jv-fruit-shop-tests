37066c0beea50790100c79da99a1a0bd33a1096a
Dima Barshay <barshayerum8@gmail.com>
2021-11-26 17:38:21 +0200

    added a test to PurchaseOperationHandlerTest

--- src/test/java/core/basesyntax/strategy/PurchaseOperationHandlerTest.java
+++ src/test/java/core/basesyntax/strategy/PurchaseOperationHandlerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.model.TransactionDto;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,6 +13,8 @@ public class PurchaseOperationHandlerTest {
     public static final Integer INITIAL_QUANTITY = 100;
     public static final TransactionDto VALID_DTO =
             new TransactionDto("b", "apple", 20);
+    public static final TransactionDto LARGER_QUANTITY_DTO =
+            new TransactionDto("b", "apple", 110);
     public static final Integer PURCHASE_QUANTITY = 80;
     private static OperationHandler purchaseOperationHandler;
     private static Fruit fruit;
@@ -19,6 +22,10 @@ public class PurchaseOperationHandlerTest {
     @BeforeClass
     public static void beforeClass() {
         purchaseOperationHandler = new PurchaseOperationHandler();
+    }
+
+    @Before
+    public void setUp() {
         Storage.storage.clear();
         fruit = new Fruit("apple");
         Storage.storage.put(fruit, INITIAL_QUANTITY);
@@ -30,5 +37,10 @@ public class PurchaseOperationHandlerTest {
         Integer actual = Storage.storage.get(fruit);
         assertEquals(PURCHASE_QUANTITY,
                 actual);
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void largerQuantity_notOk() {
+        purchaseOperationHandler.apply(LARGER_QUANTITY_DTO);
     }
 }

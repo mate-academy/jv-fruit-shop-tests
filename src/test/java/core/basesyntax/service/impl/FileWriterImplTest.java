fdb7bf24f0a33417fe45e28dec9707a67e3e95f2
Dany Nesterov <nesterov1200997@gmail.com>
2021-11-26 16:43:20 +0200

    last try v2

--- src/test/java/core/basesyntax/service/impl/FileWriterImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileWriterImplTest.java
@@ -1,14 +1,13 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.service.FileWriter;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class FileWriterImplTest {
     private static FileWriter fileWriter;

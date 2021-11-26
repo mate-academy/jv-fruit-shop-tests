5a114c5091927f41898954d78de5a5ed9794e068
Oleg Shiriaev <mystidreamer@gmail.com>
2021-11-26 13:04:35 +0200

    fields names was changed

--- src/test/java/core/basesyntax/service/ReaderServiceTest.java
+++ src/test/java/core/basesyntax/service/ReaderServiceTest.java
@@ -8,9 +8,9 @@ import org.junit.Test;
 
 public class ReaderServiceTest {
     private static ReaderService readerService;
-    private static final String FILE_PATH_FIRST = "src/test/resources/notValidFile.csv";
-    private static final String FILE_PATH_SECOND = "src/test/resources/validFile.csv";
-    private static final String FILE_PATH_THIRD = "src/test/resources/emptyFile.csv";
+    private static final String NOT_VALID_FILE_PATH = "src/test/resources/notValidFile.csv";
+    private static final String VALID_FILE_PATH = "src/test/resources/validFile.csv";
+    private static final String EMPTY_FILE_PATH = "src/test/resources/emptyFile.csv";
     private static final String NOT_EXISTED_FILE = "src/test/resources/notExist.csv";
 
     @BeforeClass
@@ -24,7 +24,7 @@ public class ReaderServiceTest {
                 List.of("some info",
                         "b,banana,10",
                         "not valid info");
-        List<String> actual = readerService.readFromFile(FILE_PATH_FIRST);
+        List<String> actual = readerService.readFromFile(NOT_VALID_FILE_PATH);
         Assert.assertEquals(expected, actual);
     }
 
@@ -36,13 +36,13 @@ public class ReaderServiceTest {
                         "b,apple,100",
                         "s,banana,100",
                         "p,banana,13");
-        List<String> actual = readerService.readFromFile(FILE_PATH_SECOND);
+        List<String> actual = readerService.readFromFile(VALID_FILE_PATH);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void emptyInputData_Ok() {
-        List<String> dataFromFile = readerService.readFromFile(FILE_PATH_THIRD);
+        List<String> dataFromFile = readerService.readFromFile(EMPTY_FILE_PATH);
         int actual = dataFromFile.size();
         int expected = 0;
         Assert.assertEquals(expected, actual);

fe93d5428cc1e67b583aff5f74d08b00f37bb0f0
Maksym Mazlov <mazlovmaxim@gmail.com>
2021-11-29 18:34:55 +0200

    add more tests for Parser, configure pom.xml

--- src/test/java/core/basesyntax/service/ParserTest.java
+++ src/test/java/core/basesyntax/service/ParserTest.java
@@ -8,37 +8,47 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ParserTest {
-    private static ParserCsvLineService parser;
+    private static final String INCORRECT_AMOUNT_LINE = "type,fruit,quantity";
+    private static final String INCORRECT_LINE = "tayp2e!fruit,quantity";
+    private static final String EMPTY_LINE = "";
+    private static final String ONE_ELEMENT_LINE = "type";
+    private static Parser parser;
+    private static final String CORRECT_LINE = "s,banana,10";
 
     @BeforeClass
     public static void beforeClass() {
         parser = new ParserCsvLineService();
     }
 
-    @Test
-    public void parseValidLine_Ok() {
-        Transaction expected = new Transaction(Operation.BALANCE, new Fruit("banana"), 25);
-        Transaction actual = parser.parseLine("b,banana,25");
-        Assert.assertEquals(actual, expected);
-    }
-
     @Test(expected = RuntimeException.class)
-    public void parseInvalidLine_NotOk() {
-        parser.parseLine("a,56,");
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void parseNullLine_NotOk() {
+    public void parseLine_nullString_NotOk() {
         parser.parseLine(null);
     }
 
     @Test(expected = RuntimeException.class)
-    public void parseLineEmptyLine_NotOk() {
-        parser.parseLine("");
+    public void parseLine_emptyLine_NotOk() {
+        parser.parseLine(EMPTY_LINE);
     }
 
     @Test(expected = RuntimeException.class)
-    public void parseLineOneElementLine_NotOk() {
-        parser.parseLine("type");
+    public void parseLine_incorrectAmountLine_NotOk() {
+        parser.parseLine(INCORRECT_AMOUNT_LINE);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void parseLine_incorrectLine_NotOk() {
+        parser.parseLine(INCORRECT_LINE);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void parseLine_oneElementLine_NotOk() {
+        parser.parseLine(ONE_ELEMENT_LINE);
+    }
+
+    @Test
+    public void parseLine_correctLine_Ok() {
+        Transaction expected = new Transaction(Operation.SUPPLY, new Fruit("banana"), 10);
+        Transaction actual = parser.parseLine(CORRECT_LINE);
+        Assert.assertEquals(expected, actual);
     }
 }

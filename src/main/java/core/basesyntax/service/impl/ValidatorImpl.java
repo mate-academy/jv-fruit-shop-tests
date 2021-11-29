d0e4c2a548fa63a3af16583952708d70a20847ea
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-11-29 10:19:06 +0200

    All requested fixes done 4

--- src/main/java/core/basesyntax/service/impl/ValidatorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ValidatorImpl.java
@@ -5,13 +5,14 @@ import java.util.regex.Pattern;
 
 public class ValidatorImpl implements Validator {
     private static final int OPERATION_INDEX = 0;
-    private static final int DEFAULT_STRING_SIZE = 3;
-    private static final Pattern PATTERN = Pattern.compile("^[0-9]{1,10}$");
+    private static final int QUANTITY_INDEX = 2;
+    private static final int MIN_NUMBER_OF_ARGUMENTS = 3;
+    private static final Pattern VALID_QUANTITY_FORMAT = Pattern.compile("^[0-9]{1,10}$");
     
     @Override
     public boolean validateLine(String line) {
         String[] lineToBeChecked = line.split(",");
-        if (lineToBeChecked.length < DEFAULT_STRING_SIZE) {
+        if (lineToBeChecked.length < MIN_NUMBER_OF_ARGUMENTS) {
             return false;
         }
         if (!lineToBeChecked[OPERATION_INDEX].equals("b")
@@ -21,7 +22,7 @@ public class ValidatorImpl implements Validator {
             return false;
         }
         int quantity = 0;
-        if (!PATTERN.matcher(lineToBeChecked[2]).matches()) {
+        if (!VALID_QUANTITY_FORMAT.matcher(lineToBeChecked[QUANTITY_INDEX]).matches()) {
             return false;
         }
         return quantity == Math.abs(quantity) ? true : false;

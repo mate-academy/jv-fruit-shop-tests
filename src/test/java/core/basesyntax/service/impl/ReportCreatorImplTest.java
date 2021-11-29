eac76fe3bb7efaa0b85c18318385ed35771cdc13
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-11-29 09:39:40 +0200

    fixed

--- src/test/java/core/basesyntax/service/impl/ReportCreatorImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportCreatorImplTest.java
@@ -6,7 +6,6 @@ import core.basesyntax.model.Fruit;
 import core.basesyntax.service.ReportCreator;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -18,14 +17,10 @@ public class ReportCreatorImplTest {
         reportCreator = new ReportCreatorImpl(new FruitStorageDaoImpl());
     }
 
-    @Before
-    public void setUp() {
-        Storage.storage.put(new Fruit("banana"), 20);
-        Storage.storage.put(new Fruit("apple"), 1);
-    }
-
     @Test
     public void makeReport_correctInput_ok() {
+        Storage.storage.put(new Fruit("banana"), 20);
+        Storage.storage.put(new Fruit("apple"), 1);
         String expected = "fruit,quantity" + System.lineSeparator()
                 + "banana,20" + System.lineSeparator()
                 + "apple,1";
@@ -35,7 +30,6 @@ public class ReportCreatorImplTest {
 
     @Test
     public void makeReport_emptyStorage_notOk() {
-        Storage.storage.clear();
         String expected = "fruit,quantity";
         String actual = reportCreator.makeReport();
         Assert.assertEquals(actual, expected);

9d70c9b5cbe8aa5c3778e22a76b14686f3f50dc4
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2022-02-16 14:06:20 +0200

    Improving the code

--- src/test/java/core/basesyntax/FruitShopTest.java
+++ src/test/java/core/basesyntax/FruitShopTest.java
@@ -50,7 +50,7 @@ public class FruitShopTest {
     public void transferringNonExistentFileName_NotOk() {
         assertThrows(RuntimeException.class,() -> getFromReport.getFromReport("Non-Existent.csv"),
                 "File is not existent");
-    } //good
+    }
 
     @Test
     public void transferringExistentFileName_Ok() {
@@ -59,56 +59,56 @@ public class FruitShopTest {
                 "r,apple,10","p,apple,20","p,banana,5","s,banana,50");
         assertEquals(excepted,actual,"Excepted result is " + excepted.toString() + "but was "
                 + actual.toString());
-    } //good
+    }
 
     @Test
     public void transferringFileWithInvalidFormatAtFirstPosition_notOk() {
         assertThrows(RuntimeException.class,()
                 -> getFromReport.getFromReport("invalidFormatAtFirstLine.csv"),
                 "Values must be separated by commas");
-    } //good
+    }
 
     @Test
     public void transferringFileWithInvalidFormatAtThirdPosition_notOk() {
         assertThrows(RuntimeException.class,()
                 -> getFromReport.getFromReport("invalidFormatAtThirdLine.csv"),
                 "Values must be separated by commas");
-    } //good
+    }
 
     @Test
     public void fileWithOnlyCommas_notOk() {
         assertThrows(RuntimeException.class,()
                 -> getFromReport.getFromReport("onlyCommas"),
                 "All values must be specified in format t,fruit,count");
-    } // good
+    }
 
     @Test
     public void notSetTransaction_notOk() {
         assertThrows(RuntimeException.class,()
                 -> getFromReport.getFromReport("notSetTransaction"),
                 "All values must be specified in format t,fruit,count");
-    } //good
+    }
 
     @Test
     public void notSetFruit_notOk() {
         assertThrows(RuntimeException.class,()
                 -> getFromReport.getFromReport("notSetFruit"),
                 "All values must be specified in format t,fruit,count");
-    } //good
+    }
 
     @Test
     public void notSetValue_notOk() {
         assertThrows(RuntimeException.class,()
                 -> getFromReport.getFromReport("notSetValue"),
                 "All values must be specified in format t,fruit,count");
-    } // good
+    }
 
     @Test
     public void invalidNameOfTransaction_notOk() {
         assertThrows(RuntimeException.class,()
                 -> definition.definition("m","banana",30),
                 "Invalid name of transaction, it should be one of b,s,r,p");
-    } //good
+    }
 
     @Test
     public void validNameOfTransaction_Ok() {
@@ -116,14 +116,14 @@ public class FruitShopTest {
         int excepted = 30;
         int actual = service.getFromStorage("banana");
         assertEquals(excepted,actual);
-    } //good
+    }
 
     @Test
     public void invalidCount_notOk() {
         assertThrows(RuntimeException.class,()
                 -> transactions.makeAllTransactions("invalidCount.csv"),
                 "Invalid count of fruits");
-    } //good
+    }
 
     @Test
     public void addAnExistingFruit_notOk() {
@@ -131,14 +131,14 @@ public class FruitShopTest {
         assertThrows(RuntimeException.class,()
                 -> balance.applyOperations("banana",10),
                 "Some fruit has already existed");
-    } //good
+    }
 
     @Test
     public void addFruit_Ok() {
         balance.applyOperations("banana",30);
         Integer actual = service.getFromStorage("banana");
         assertEquals(30,actual);
-    } //good
+    }
 
     @Test
     public void purchaseMoreFruitThatWeHave_notOk() {
@@ -146,7 +146,7 @@ public class FruitShopTest {
         assertThrows(RuntimeException.class,()
                 -> purchase.applyOperations("apple",30),
                 "We can`t solve more fruits that we have");
-    } //good
+    }
 
     @Test
     public void purchaseFruit_Ok() {
@@ -154,7 +154,7 @@ public class FruitShopTest {
         purchase.applyOperations("apple",10);
         int actual = service.getFromStorage("apple");
         assertEquals(20,actual);
-    } //good
+    }
 
     @Test
     public void purchaseNonExistenceFruit_notOk() {
@@ -162,7 +162,7 @@ public class FruitShopTest {
         assertThrows(RuntimeException.class,()
                 -> purchase.applyOperations("orange",20),
                 "We can`t solve non-existence fruit");
-    } //good
+    }
 
     @Test
     public void supplyNonExistenceFruit_notOk() {
@@ -170,7 +170,7 @@ public class FruitShopTest {
         assertThrows(RuntimeException.class,()
                 -> supply.applyOperations("pineapple",20),
                 "We can`t supply non-existence fruit");
-    } //good
+    }
 
     @Test
     public void supplyExistenceFruit_Ok() {
@@ -178,7 +178,7 @@ public class FruitShopTest {
         supply.applyOperations("orange",20);
         int actual = service.getFromStorage("orange");
         assertEquals(30,actual);
-    } //good
+    }
 
     @Test
     public void returnNonExistenceFruit_notOk() {
@@ -186,7 +186,7 @@ public class FruitShopTest {
         assertThrows(RuntimeException.class,()
                 -> returning.applyOperations("pineapple",20),
                 "Customer can`t return non-existence fruit");
-    } //good
+    }
 
     @Test
     public void returnExistenceFruit_Ok() {
@@ -194,21 +194,21 @@ public class FruitShopTest {
         returning.applyOperations("orange",20);
         int actual = service.getFromStorage("orange");
         assertEquals(30,actual);
-    } //good
+    }
 
     @Test
     public void addToStorage_Ok() {
         service.addToStorage("apple",30);
         int size = service.getStorage().size();
         assertEquals(1,size,"Size would be 1");
-    } //good
+    }
 
     @Test
     public void getFromStorage_Ok() {
         service.addToStorage("apple",30);
         Integer actual = service.getFromStorage("apple");
         assertEquals(30,actual,"Count would be 30");
-    } //good
+    }
 
     @Test
     public void getStorage_Ok() {
@@ -217,7 +217,7 @@ public class FruitShopTest {
         Map<String, Integer> actual = service.getStorage();
         assertEquals(excepted,actual,"Return map would be " + excepted.toString()
                 + " but was " + actual.toString());
-    } //good
+    }
 
     @Test
     public void getNewReport_Ok() {
@@ -227,14 +227,14 @@ public class FruitShopTest {
                 + System.lineSeparator();
         assertEquals(excepted,actual,"Returning report would be " + excepted
                 + " but was " + actual);
-    } //good
+    }
 
     @Test
     public void getNewReportFromEmptyStorage_notOk() {
         assertThrows(RuntimeException.class,()
                 -> creating.createReport(service.getStorage()),
                 "Customer can`t return non-existence fruit");
-    } //good
+    }
 
     @Test
     public void getReport_Ok() {
@@ -244,5 +244,5 @@ public class FruitShopTest {
         String excepted = creating.createReport(service.getStorage());
         assertEquals(excepted,actual,"Result of getReport will be "
                 + excepted + " , but was " + actual);
-    } //good
+    }
 }

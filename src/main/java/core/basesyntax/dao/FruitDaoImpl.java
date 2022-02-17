debba9801501eb5a2d4b7aeee8611471692d164e
sviat <svyatoslav.grinik@gmail.com>
2022-02-17 19:19:39 +0200

    refactored FruitDaoImpl

--- src/main/java/core/basesyntax/dao/FruitDaoImpl.java
+++ src/main/java/core/basesyntax/dao/FruitDaoImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.dao;
 
 import core.basesyntax.model.Fruit;
 import java.util.Map;
+import java.util.Objects;
 
 public class FruitDaoImpl implements FruitDao {
 
@@ -13,7 +14,14 @@ public class FruitDaoImpl implements FruitDao {
 
     @Override
     public void addNewFruit(Fruit fruit, int quantity) {
-        storage.put(fruit, quantity);
+        if (Objects.isNull(fruit)) {
+            throw new RuntimeException("Fruit can't be null!");
+        } else if (quantity < 0) {
+            throw new RuntimeException("Quantity of fruits to add must be positive."
+                    + " But not " + quantity + "!");
+        } else {
+            storage.put(fruit, quantity);
+        }
     }
 
     @Override
@@ -28,14 +36,20 @@ public class FruitDaoImpl implements FruitDao {
                     storage.get(fruit) - quantityToSubtract);
         } else {
             throw new RuntimeException("Quantity to subtract must be less than actual quantity of"
-                    + " fruits. The actual quantity of " + fruit.getName() + " is: " + storage.get(fruit));
+                    + " fruits. The actual quantity of "
+                    + fruit.getName() + " is: " + storage.get(fruit));
         }
     }
 
     @Override
     public void addQuantityByFruit(Fruit fruit, int quantityToAdd) {
-        storage.replace(fruit,
-                storage.get(fruit) + quantityToAdd);
+        if (quantityToAdd > 0) {
+            storage.replace(fruit,
+                    storage.get(fruit) + quantityToAdd);
+        } else {
+            throw new RuntimeException("Quantity of fruits to add must be positive."
+                    + " But not " + quantityToAdd + "!");
+        }
     }
 
     @Override

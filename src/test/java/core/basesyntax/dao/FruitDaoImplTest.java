d57eccbfce1eb4f6fe583b8d02c9a4c138eba409
anyutamysko <a_misko@ukr.net>
2022-02-21 16:32:13 +0200

    deleted empty class

--- src/test/java/core/basesyntax/dao/FruitDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/FruitDaoImplTest.java
@@ -7,24 +7,18 @@ import static org.junit.Assert.assertThrows;
 import core.basesyntax.db.FruitsStorage;
 import core.basesyntax.model.Fruit;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FruitDaoImplTest {
     public static final String FRUIT_TYPE_APPLE = "appleFruit";
     private static Fruit fruitApple;
-    private FruitDao fruitDao;
-
-    @Before
-    public void setUp() {
-        fruitDao = new FruitDaoImpl();
-        fruitApple = new Fruit(FRUIT_TYPE_APPLE);
-    }
+    private static FruitDao fruitDao;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
-
+        fruitDao = new FruitDaoImpl();
+        fruitApple = new Fruit(FRUIT_TYPE_APPLE);
     }
 
     @AfterClass

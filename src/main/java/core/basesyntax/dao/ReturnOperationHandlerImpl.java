720db9c58af6db8808c22ee0be17b9a8b736d0ef
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-02-22 13:44:25 +0200

    PopulatingDbServiceImplTest was created

--- src/main/java/core/basesyntax/dao/ReturnOperationHandlerImpl.java
+++ src/main/java/core/basesyntax/dao/ReturnOperationHandlerImpl.java
@@ -5,6 +5,9 @@ import core.basesyntax.db.Storage;
 public class ReturnOperationHandlerImpl implements OperationHandler {
     @Override
     public void apply(String fruit, int quantity) {
+        if (!Storage.fruitStorage.containsKey(fruit)) {
+            Storage.fruitStorage.put(fruit, quantity);
+        }
         int currentQuantity = Storage.fruitStorage.get(fruit);
         Storage.fruitStorage.put(fruit, currentQuantity + quantity);
     }
---
+++ src/test/java/core/basesyntax/service/impl/PopulatingDbServiceImplTest.java
@@ -0,0 +1,122 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.dao.BalanceOperationHandlerImpl;
+import core.basesyntax.dao.OperationHandler;
+import core.basesyntax.dao.PurchaseOperationHandlerImpl;
+import core.basesyntax.dao.ReturnOperationHandlerImpl;
+import core.basesyntax.dao.SupplyOperationHandlerImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.models.FruitTransaction;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class PopulatingDbServiceImplTest {
+    public static final String BALANCE = "b";
+    public static final String SUPPLY = "s";
+    public static final String RETURN = "r";
+    public static final String PURCHASE = "p";
+    private static final Map<String, OperationHandler> operationTypes = new HashMap<>();
+    private static PopulatingDbServiceImpl populatingDb;
+    private static List<FruitTransaction> invalidTransactions;
+    private static List<FruitTransaction> balanceTransactions;
+    private static List<FruitTransaction> purchaseTransactions;
+    private static List<FruitTransaction> purchaseTransactionsInvalidFruits;
+    private static List<FruitTransaction> purchaseTransactionsInvalidCount;
+    private static List<FruitTransaction> supplyTransactions;
+    private static List<FruitTransaction> returnTransactions;
+
+
+    @BeforeClass
+    public static void beforeClass() {
+        operationTypes.put(BALANCE, new BalanceOperationHandlerImpl());
+        operationTypes.put(SUPPLY, new SupplyOperationHandlerImpl());
+        operationTypes.put(RETURN, new ReturnOperationHandlerImpl());
+        operationTypes.put(PURCHASE, new PurchaseOperationHandlerImpl());
+        populatingDb = new PopulatingDbServiceImpl(operationTypes);
+        invalidTransactions = new ArrayList<>(List.of(
+                new FruitTransaction("j", "banana", 10),
+                new FruitTransaction("l", "apple", 10)));
+        balanceTransactions = new ArrayList<>(List.of(
+                new FruitTransaction("b", "banana", 10),
+                new FruitTransaction("b", "apple", 10)));
+        purchaseTransactions = new ArrayList<>(List.of(
+                new FruitTransaction("p", "banana", 10),
+                new FruitTransaction("p", "apple", 10)));
+        purchaseTransactionsInvalidFruits = new ArrayList<>(List.of(
+                new FruitTransaction("p", "orange", 10),
+                new FruitTransaction("p", "pineapple", 10)));
+        purchaseTransactionsInvalidCount = new ArrayList<>(List.of(
+                new FruitTransaction("p", "banana", 100),
+                new FruitTransaction("p", "apple", 100)));
+        supplyTransactions = new ArrayList<>(List.of(
+                new FruitTransaction("s", "banana", 10),
+                new FruitTransaction("s", "apple", 10)));
+        returnTransactions = new ArrayList<>(List.of(
+                new FruitTransaction("r", "banana", 10),
+                new FruitTransaction("r", "apple", 10)));
+        Storage.fruitStorage.put("banana", 0);
+        Storage.fruitStorage.put("apple", 0);
+    }
+
+    @After
+    public void tearDown() {
+        Storage.fruitStorage.clear();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void PopulatingDb_InvalidOperation() {
+        populatingDb.prepareDB(invalidTransactions);
+    }
+
+    @Test
+    public void populatingDb_BalanceOperation() {
+        Storage.fruitStorage.clear();
+        Map<String, Integer> initialState = new HashMap<>(Storage.fruitStorage);
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("banana", 10);
+        expected.put("apple", 10);
+        populatingDb.prepareDB(balanceTransactions);
+        assertNotEquals(initialState.size(), Storage.fruitStorage.size());
+        assertEquals(expected, Storage.fruitStorage);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void populatingDb_purchaseOperation() {
+        Storage.fruitStorage.put("banana", 30);
+        Storage.fruitStorage.put("apple", 30);
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("banana", 20);
+        expected.put("apple", 20);
+        populatingDb.prepareDB(purchaseTransactions);
+        assertEquals(expected, Storage.fruitStorage);
+        populatingDb.prepareDB(purchaseTransactionsInvalidFruits);
+        populatingDb.prepareDB(purchaseTransactionsInvalidCount);
+    }
+
+    @Test
+    public void populatingDb_returnOperation() {
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("banana", 10);
+        expected.put("apple", 10);
+        populatingDb.prepareDB(returnTransactions);
+        assertEquals(expected, Storage.fruitStorage);
+    }
+
+    @Test
+    public void populatingDb_SupplyOperation() {
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("banana", 10);
+        expected.put("apple", 10);
+        populatingDb.prepareDB(supplyTransactions);
+        assertEquals(expected, Storage.fruitStorage);
+    }
+}

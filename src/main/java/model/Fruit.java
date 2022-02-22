d9f0e5129be890db98a7299e4644cb8762e9d974
DenTima <tymoshchuk.den@gmail.com>
2022-02-22 22:18:36 +0200

    Hope it's last fixing

--- src/main/java/model/Fruit.java
+++ src/main/java/model/Fruit.java
@@ -1,12 +1,18 @@
 package model;
 
+import java.util.Objects;
+
 public class Fruit {
     private String name;
     private int amount;
 
     public Fruit(String name, int amount) {
-        this.name = name;
-        this.amount = amount;
+        if (name == null || amount < 0) {
+            throw new RuntimeException("Invalid data in Fruit constructor");
+        } else {
+            this.name = name;
+            this.amount = amount;
+        }
     }
 
     public String getName() {
@@ -14,6 +20,10 @@ public class Fruit {
     }
 
     public void setName(String name) {
+        if (name == null || name.length() == 0) {
+            throw new RuntimeException("Invalid data in "
+                    + "Fruit setName(String name)");
+        }
         this.name = name;
     }
 
@@ -22,6 +32,32 @@ public class Fruit {
     }
 
     public void setAmount(int amount) {
+        if (amount < 0) {
+            throw new RuntimeException("Invalid data in "
+                    + "Fruit setAmount(int amount)");
+        }
         this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fruit fruit = (Fruit) o;
+        return amount == fruit.amount && Objects.equals(name, fruit.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, amount);
+    }
+
+    @Override
+    public Fruit clone() {
+        return new Fruit(name, amount);
     }
 }

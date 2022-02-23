057c94e6aea00e3abba829f68c7d844a4ac5e34d
lugburz.hr <lugburz.hr@gmail.com>
2022-02-23 16:32:26 +0200

    codestyle fix

--- src/main/java/core/basesyntax/service/file/FileService.java
+++ src/main/java/core/basesyntax/service/file/FileService.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class FileService implements FileOperations {
--- src/test/java/core/basesyntax/service/file/FileServiceTest.java
+++ src/test/java/core/basesyntax/service/file/FileServiceTest.java
@@ -7,7 +7,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.BeforeClass;

5af47b0111933e1241340f68e19128971377b609
Stanislav <stassonich@icloud.com>
2022-02-23 23:26:30 +0200

    fruitTest

--- src/src/test/java/core/basesyntax/service/impl/FruitStrategyImplTest.java
+++ src/src/test/java/core/basesyntax/service/impl/FruitStrategyImplTest.java
@@ -1,13 +1,12 @@
 package core.basesyntax.service.impl;
 
-import static org.junit.Assert.assertEquals;
-
 import core.basesyntax.service.FruitStrategy;
 import core.basesyntax.strategy.BalanceOperation;
 import core.basesyntax.strategy.FruitOperation;
 import core.basesyntax.strategy.PurchaseOperation;
 import core.basesyntax.strategy.ReturnOperation;
 import core.basesyntax.strategy.SupplyOperation;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FruitStrategyImplTest {
@@ -24,27 +23,27 @@ public class FruitStrategyImplTest {
     public void makeBalanceOperation_Ok() {
         expectedOperation = new BalanceOperation();
         actualOperation = strategy.makeOperation("b");
-        assertEquals(expectedOperation.getClass(), actualOperation.getClass());
+        Assert.assertEquals(expectedOperation.getClass(), actualOperation.getClass());
     }
 
     @Test
     public void makeSupplyOperation_Ok() {
         expectedOperation = new SupplyOperation();
         actualOperation = strategy.makeOperation("s");
-        assertEquals(expectedOperation.getClass(), actualOperation.getClass());
+        Assert.assertEquals(expectedOperation.getClass(), actualOperation.getClass());
     }
 
     @Test
     public void makePurchaseOperation_Ok() {
         expectedOperation = new PurchaseOperation();
         actualOperation = strategy.makeOperation("p");
-        assertEquals(expectedOperation.getClass(), actualOperation.getClass());
+        Assert.assertEquals(expectedOperation.getClass(), actualOperation.getClass());
     }
 
     @Test
     public void makeReturnOperation_Ok() {
         expectedOperation = new ReturnOperation();
         actualOperation = strategy.makeOperation("r");
-        assertEquals(expectedOperation.getClass(), actualOperation.getClass());
+        Assert.assertEquals(expectedOperation.getClass(), actualOperation.getClass());
     }
 }

d31974c62af4efe9005d297fcbcb833ee3106946
Stanislav <stassonich@icloud.com>
2022-02-23 21:54:12 +0200

    test_shop

--- src/test/java/core/basesyntax/service/impl/FruitServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FruitServiceImplTest.java
@@ -1,14 +1,15 @@
 package core.basesyntax.service.impl;
 
-import static org.junit.Assert.assertEquals;
-
 import core.basesyntax.service.FruitService;
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FruitServiceImplTest {
     private FruitService fruitService = new FruitServiceImpl();

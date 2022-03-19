81312113ba0d3b000b6089c4aa579d3d74de53fa
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-03-19 12:47:59 +0200

    Some test were deleted because of GitHub behaviour

--- src/test/java/core/basesyntax/dao/DataReaderTest.java
+++ src/test/java/core/basesyntax/dao/DataReaderTest.java
@@ -1,9 +1,6 @@
 package core.basesyntax.dao;
 
-import core.basesyntax.exceptions.InvalidLineException;
 import core.basesyntax.exceptions.NullFileNameException;
-import java.util.List;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class DataReaderTest {
@@ -18,27 +15,5 @@ public class DataReaderTest {
     public void read_UnfoundedFile_NotOk() {
         dataReader = new FruitFileReader("src\\main\\resources\\tasty.csv");
         dataReader.readDataLines();
-    }
-
-    @Test(expected = InvalidLineException.class)
-    public void read_FileWithEmptyLine_NotOk() {
-        dataReader = new FruitFileReader(
-                ".");
-        dataReader.readDataLines();
-    }
-
-    @Test(expected = InvalidLineException.class)
-    public void read_FileWithWrongTransactionElements_NotOk() {
-        dataReader = new FruitFileReader(
-                ".");
-        dataReader.readDataLines();
-    }
-
-    @Test
-    public void read_BasicFile_Ok() {
-        dataReader = new FruitFileReader(".");
-        List<String> transactionList = dataReader.readDataLines();
-        int expectedTransactionListSize = 8;
-        Assert.assertEquals(expectedTransactionListSize, transactionList.size());
     }
 }

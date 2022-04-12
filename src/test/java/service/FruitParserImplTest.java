50031e807c0a8e6eea8236dd9683f6de1a0acd6c
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-04-12 13:58:30 +0200

    added FruitParser FruitParserImpl and FruitParserImplTest

---
+++ src/test/java/service/FruitParserImplTest.java
@@ -0,0 +1,44 @@
+package service;
+
+import model.FruitTransaction;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FruitParserImplTest {
+    private static FruitParser fruitParser;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        fruitParser = new FruitParserImpl();
+    }
+
+    @Test
+    public void parsedData_Ok() {
+        FruitTransaction expected = new FruitTransaction();
+        expected.setOperation(FruitTransaction.Operation.BALANCE);
+        expected.setFruit("banana");
+        expected.setQuantity(20);
+        String actualData = " b,banana,20";
+        FruitTransaction actual = fruitParser.parseFruitTransaction(actualData);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullOperationData_NotOk() {
+        String actualData = " ,banana,20";
+        fruitParser.parseFruitTransaction(actualData);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullFruitData_NotOk() {
+        String actualData = " b,,20";
+        fruitParser.parseFruitTransaction(actualData);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void nullQuantityData_NotOk() {
+        String actualData = " b,banana, ";
+        fruitParser.parseFruitTransaction(actualData);
+    }
+}

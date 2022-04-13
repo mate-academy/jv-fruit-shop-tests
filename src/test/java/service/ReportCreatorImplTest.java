e7d2b824424d6aa911145397b13e1ad38278e4e6
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-04-13 10:39:38 +0200

    corrected code

--- src/test/java/service/ReportCreatorImplTest.java
+++ src/test/java/service/ReportCreatorImplTest.java
@@ -2,33 +2,42 @@ package service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import model.FruitTransaction;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ReportCreatorImplTest {
+    private final FruitTransaction secondFruitTransaction = new FruitTransaction();
+    private final FruitTransaction extraPositionInReport = new FruitTransaction();
+    private final FruitTransaction fruitTransaction = new FruitTransaction();
+
     @Test
+
     public void createReport_Ok() {
-        FruitTransaction secondFruitTransaction;
-        FruitTransaction fruitTransaction = new FruitTransaction();
-        secondFruitTransaction = new FruitTransaction();
         fruitTransaction.setFruit("banana");
         fruitTransaction.setQuantity(172);
+        extraPositionInReport.setFruit("banana");//дубликат банана
+        extraPositionInReport.setQuantity(172);//дубликат кол-во банана
         secondFruitTransaction.setFruit("apple");
         secondFruitTransaction.setQuantity(190);
         List<FruitTransaction> transactions = new ArrayList<>();
         transactions.add(fruitTransaction);
         transactions.add(secondFruitTransaction);
-        StringBuilder builder = new StringBuilder();
-        builder.append("fruit,quantity");
-        builder.append(System.lineSeparator());
-        builder.append("banana,172");
-        builder.append(System.lineSeparator());
-        builder.append("apple,190");
-        builder.append(System.lineSeparator());
-        String expected = builder.toString();
+        transactions.add(extraPositionInReport);//добавили дубликат в лист
+
+        List<FruitTransaction> cleanListWithoutDuplicates = transactions.stream()
+                .distinct().collect(Collectors.toList());
+
+        String expected = "fruit,quantity"
+                + System.lineSeparator()
+                + "banana,172"
+                + System.lineSeparator()
+                + "apple,190"
+                + System.lineSeparator();
         ReportCreator reportCreator = new ReportCreatorImpl();
-        String actual = reportCreator.createReport(transactions);
+        String actual = reportCreator.createReport(cleanListWithoutDuplicates);
+
         Assert.assertEquals(expected, actual);
     }
 }
--- src/test/java/service/WriterImplTest.java
+++ src/test/java/service/WriterImplTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -10,11 +11,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WriterImplTest {
+    private static final String FILE_SEPARATOR = FileSystems.getDefault().getSeparator();
+    private static final String OUTPUT_FILE = "src" + FILE_SEPARATOR
+            + "test" + FILE_SEPARATOR + "resources" + FILE_SEPARATOR + "testOutput.csv";
     private static Writer writer;
     private static StringBuilder reportBuilder;
 
     @BeforeClass
-    public static void beforeClass() throws Exception {
+    public static void beforeClass() {
         writer = new WriterImpl();
         reportBuilder = new StringBuilder();
         reportBuilder.append("fruit,quantity");
@@ -27,17 +31,16 @@ public class WriterImplTest {
     @Test
     public void writeValidData_Ok() {
         String data = reportBuilder.toString();
-        writer.write(data, "src/test/resources/testOutput.csv");
-        String actualResult = new String();
+        writer.write(data, OUTPUT_FILE);
+        String actualResult;
         try {
-            List<String> actual = Files.readAllLines(Path.of("src/test/resources/testOutput.csv"));
+            List<String> actual = Files.readAllLines(Path.of(OUTPUT_FILE));
             actualResult = actual.stream().collect(Collectors.joining(System.lineSeparator()));
         } catch (IOException e) {
             throw new RuntimeException("Can't read data from file "
-                    + "src/test/resources/testOutput.csv");
+                    + OUTPUT_FILE);
         }
-        String expectedResult = new String(data);
-        Assert.assertEquals(expectedResult, actualResult);
+        Assert.assertEquals(data, actualResult);
     }
 
     @Test(expected = RuntimeException.class)

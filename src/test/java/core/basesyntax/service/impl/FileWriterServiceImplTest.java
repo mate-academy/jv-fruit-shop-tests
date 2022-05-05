c270bf305447a3f863a99ed765901c3e0701d835
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-05-05 16:43:33 +0300

    fixed

--- src/test/java/core/basesyntax/service/impl/FileWriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileWriterServiceImplTest.java
@@ -2,24 +2,22 @@ package core.basesyntax.service.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import core.basesyntax.service.FileReaderService;
 import core.basesyntax.service.FileWriterService;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FileWriterServiceImplTest {
     private static FileWriterService fileWriterService;
+    private static FileReaderService fileReaderService;
 
     @BeforeClass
     public static void beforeClass() {
         fileWriterService = new FileWriterServiceImpl();
+        fileReaderService = new FileReaderServiceImpl();
     }
 
     @Test
@@ -32,9 +30,9 @@ public class FileWriterServiceImplTest {
                 "banana,52",
                 "apple,9");
         fileWriterService.writeToFile(toFileName, report);
-        File testOutputFile = new File("src/test/resources/testOutputData");
+        File testOutputFile = new File("testOutputFile");
         List<String> actual = new ArrayList<>();
-        actual = read("testOutputFile");
+        actual = fileReaderService.readFromFile("testOutputFile");
         assertEquals(expected, actual);
     }
 
@@ -44,16 +42,5 @@ public class FileWriterServiceImplTest {
                 + "banana,52\n"
                 + "apple,9\n";
         fileWriterService.writeToFile("", report);
-    }
-
-    private static List<String> read(String filePath) {
-        List<String> readData = new ArrayList<>();
-        try (Stream<String> lines = Files.lines(Path.of(filePath))) {
-            readData = lines.collect(Collectors.toList());
-        } catch (IOException e) {
-            throw new RuntimeException("Cant`t read data from file"
-                    + Path.of(filePath).getFileName().toString(), e);
-        }
-        return readData;
     }
 }

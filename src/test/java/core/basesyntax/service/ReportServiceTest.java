49370fc0f84260975795f41aa5cfd233a7bc4431
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-05-10 19:22:50 +0300

    Code refactoring

--- src/test/java/core/basesyntax/service/ReportServiceTest.java
+++ src/test/java/core/basesyntax/service/ReportServiceTest.java
@@ -5,32 +5,18 @@ import static org.junit.Assert.assertTrue;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.service.impl.ReportServiceImpl;
 import core.basesyntax.storage.FruitStorage;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ReportServiceTest {
-    private static final String PATH = "src/test/resources/test.csv";
     private static ReportService reportService;
 
     @BeforeClass
     public static void init() {
         reportService = new ReportServiceImpl();
-    }
-
-    @After
-    public void clearFile() {
-        try {
-            Files.write(Path.of(PATH), Collections.EMPTY_LIST);
-        } catch (IOException e) {
-            throw new RuntimeException("Can't clear file", e);
-        }
     }
 
     @After

5fe47364b9fc135a45aa138eaa2064e9112630b9
Misha Polunin <polunin1986@gmail.com>
2022-06-01 11:14:01 +0300

    - I resolved this awful task

--- src/test/java/core/basesyntax/service/impl/FileReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileReaderServiceImplTest.java
@@ -1,6 +1,7 @@
 package core.basesyntax.service.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.service.FileReaderService;
@@ -56,5 +57,56 @@ public class FileReaderServiceImplTest {
     public void readFile_WrongPathToFile_notOk() {
         assertThrows(RuntimeException.class,
                 () -> fileReaderService.readFromFile("src/resources/file.csv"));
+    }
+
+    @Test
+    public void readFileWithWrongType_NotOk() {
+        List<String> exceptedFile = new ArrayList<>();
+        exceptedFile.add("type,fruit,quantity");
+        exceptedFile.add("w,banana,20");
+        exceptedFile.add("b,apple,100");
+        exceptedFile.add("s,bananas,1100");
+        exceptedFile.add("p,banana,13");
+        exceptedFile.add("r,applse,10");
+        exceptedFile.add("p,apple,20");
+        exceptedFile.add("p,banana,5");
+        exceptedFile.add("s,banana,50");
+
+        List<String> actualFile = fileReaderService.readFromFile("src/main/resources/file.csv");
+        assertNotEquals(exceptedFile,actualFile);
+    }
+
+    @Test
+    public void readFileWithWrongFruit_NotOk() {
+        List<String> exceptedFile = new ArrayList<>();
+        exceptedFile.add("type,fruit,quantity");
+        exceptedFile.add("b,banana,20");
+        exceptedFile.add("b,apple,100");
+        exceptedFile.add("s,bananas,1100");
+        exceptedFile.add("p,banana,13");
+        exceptedFile.add("r,applse,10");
+        exceptedFile.add("p,apple,20");
+        exceptedFile.add("p,banana,5");
+        exceptedFile.add("s,banana,50");
+
+        List<String> actualFile = fileReaderService.readFromFile("src/main/resources/file.csv");
+        assertNotEquals(exceptedFile,actualFile);
+    }
+
+    @Test
+    public void readFileWithWrongQuantity_NotOk() {
+        List<String> exceptedFile = new ArrayList<>();
+        exceptedFile.add("type,fruit,quantity");
+        exceptedFile.add("b,banana,20");
+        exceptedFile.add("b,apple,100");
+        exceptedFile.add("s,banana,1100");
+        exceptedFile.add("p,banana,13");
+        exceptedFile.add("r,apple,10");
+        exceptedFile.add("p,apple,20");
+        exceptedFile.add("p,banana,5");
+        exceptedFile.add("s,banana,50");
+
+        List<String> actualFile = fileReaderService.readFromFile("src/main/resources/file.csv");
+        assertNotEquals(exceptedFile,actualFile);
     }
 }

72621130e521f9a49a9c91b7475101b4cbe80520
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-06-05 13:45:57 +0300

    вроде так

---
+++ src/main/java/core/basesyntax/FruitCounter.java
@@ -0,0 +1,52 @@
+package core.basesyntax;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FruitCounter {
+    private static final int INDEX_OPERATION = 0;
+    private static final int INDEX_FRUIT = 1;
+    private static final int INDEX_AMOUNT = 2;
+
+    List<String> countFruits(List<String> uncountedFruits) {
+        List<String> fruitTypes = new ArrayList<>();
+        List<Integer> fruitAmount = new ArrayList<>();
+
+        for (String uncountedFruit : uncountedFruits) {
+            String[] line = uncountedFruit.split(",");
+            int currentFruitIndex;
+            switch (line[INDEX_OPERATION]) {
+                case "b":
+                    fruitTypes.add(line[INDEX_FRUIT]);
+                    fruitAmount.add(Integer.parseInt(line[INDEX_AMOUNT]));
+                    break;
+                case "s":
+                case "r":
+                    currentFruitIndex = fruitTypes.indexOf(line[INDEX_FRUIT]);
+                    fruitAmount.set(currentFruitIndex,
+                            fruitAmount.get(currentFruitIndex)
+                                    + Integer.parseInt(line[INDEX_AMOUNT]));
+                    break;
+                case "p":
+                    currentFruitIndex = fruitTypes.indexOf(line[INDEX_FRUIT]);
+                    fruitAmount.set(currentFruitIndex,
+                            fruitAmount.get(currentFruitIndex)
+                                    - Integer.parseInt(line[INDEX_AMOUNT]));
+                    if (fruitAmount.get(currentFruitIndex) < 0) {
+                        throw new RuntimeException("Do not have enough fruits");
+                    }
+                    break;
+                default:
+                    throw new RuntimeException("Invalid operation. "
+                            + "Allowed operations are: b - for balance, "
+                            + "s - for supply, r - for return, p - for purchase");
+            }
+        }
+        List<String> countedFruits = new ArrayList<>();
+        countedFruits.add("fruit,quantity");
+        for (int i = 0; i < fruitTypes.size(); i++) {
+            countedFruits.add(fruitTypes.get(i) + "," + fruitAmount.get(i));
+        }
+        return countedFruits;
+    }
+}
---
+++ src/main/java/core/basesyntax/MyFileReader.java
@@ -0,0 +1,25 @@
+package core.basesyntax;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class MyFileReader {
+    List<String> getDryInfo(File file) {
+        List<String> info;
+        try {
+            info = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (info.size() < 2) {
+            throw new RuntimeException("Not enough data");
+        }
+        if (!info.get(0).equals("type,fruit,quantity")) {
+            throw new RuntimeException("Inappropriate format(1 line)");
+        }
+        info.remove(0);
+        return info;
+    }
+}

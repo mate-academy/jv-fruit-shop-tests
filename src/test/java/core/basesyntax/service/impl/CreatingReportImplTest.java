d2c7294792fa5a0ac39474adb4549f066d68eb1c
Andrey Chumak <chumakandrey7@gmail.com>
2022-06-17 20:26:09 +0300

    Refactored after student review

--- src/test/java/core/basesyntax/service/impl/CreatingReportImplTest.java
+++ src/test/java/core/basesyntax/service/impl/CreatingReportImplTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import core.basesyntax.db.FruitsStorage;
 import core.basesyntax.service.CreatingReport;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 

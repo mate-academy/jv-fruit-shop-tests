26a2b2f9cacf004d332ede2ae61e126ea60d3661
Ilya <zimmboco@gmail.com>
2022-07-08 13:35:05 +0300

    hw-18

--- src/main/java/core/basesyntax/service/impl/OperationSupplierImpl.java
+++ src/main/java/core/basesyntax/service/impl/OperationSupplierImpl.java
@@ -12,15 +12,16 @@ import java.util.Map;
 
 public class OperationSupplierImpl implements OperationSupplier {
     private final Map<String, FruitOperation> operationMap;
-    public OperationSupplierImpl() {
-    operationMap = new HashMap<>();
-    operationMap.put("b", new BalanceFruitOperation());
-    operationMap.put("s", new SupplyFruitOperation());
-    operationMap.put("p", new PurchaseFruitOperation());
-    operationMap.put("r", new ReturnFruitOperation());
-        }
 
-@Override
+    public OperationSupplierImpl() {
+        operationMap = new HashMap<>();
+        operationMap.put("b", new BalanceFruitOperation());
+        operationMap.put("s", new SupplyFruitOperation());
+        operationMap.put("p", new PurchaseFruitOperation());
+        operationMap.put("r", new ReturnFruitOperation());
+    }
+
+    @Override
     public FruitOperation getOperation(Fruit fruit) {
         return operationMap.get(fruit.getOperation());
     }

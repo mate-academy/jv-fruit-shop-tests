1691275e0a913a93fc2bf81b2ed3cc46661d60c0
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-26 14:08:36 +0300

    Added Before test with String report initialization.

--- src/test/java/core/basesyntax/dao/FileWriterImplTest.java
+++ src/test/java/core/basesyntax/dao/FileWriterImplTest.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -17,16 +19,21 @@ public class FileWriterImplTest {
     public ExpectedException expectedEx = ExpectedException.none();
 
     private final core.basesyntax.dao.FileWriter outputCsvFile = new FileWriterImpl();
+    private String report;
+
+    @Before
+    public void setUp() {
+        report = "fruit,quantity\n"
+                + "cherry,47\n"
+                + "banana,152\n"
+                + "apple,110";
+    }
 
     @Test
     public void writeToFile_Ok() {
         Path filePath = Path.of(FILENAME);
-        String expectedLines = "fruit,quantity\n"
-                + "cherry,47\n"
-                + "banana,152\n"
-                + "apple,110";
 
-        outputCsvFile.writeToFile(filePath.toString(), expectedLines);
+        outputCsvFile.writeToFile(filePath.toString(), report);
         List<String> lines;
         try {
             lines = Files.readAllLines(filePath);
@@ -35,16 +42,12 @@ public class FileWriterImplTest {
         }
         String actualLines = String.join("\n", lines);
         assertEquals("Wrote lines to file " + FILENAME + " don't equals to expected",
-                expectedLines, actualLines);
+                report, actualLines);
     }
 
     @Test(expected = RuntimeException.class)
     public void writeToFile_InvalidFileName_NotOk() {
         String invalidFile = FILENAME + "/%";
-        String report = "fruit,quantity\n"
-                + "cherry,47\n"
-                + "banana,152\n"
-                + "apple,110";
         outputCsvFile.writeToFile(invalidFile, report);
     }
 
@@ -53,10 +56,6 @@ public class FileWriterImplTest {
         String invalidFile = FILENAME + "/%";
         expectedEx.expect(RuntimeException.class);
         expectedEx.expectMessage("Can't write to file " + invalidFile);
-        String report = "fruit,quantity\n"
-                + "cherry,47\n"
-                + "banana,152\n"
-                + "apple,110";
         outputCsvFile.writeToFile(invalidFile, report);
     }
 }

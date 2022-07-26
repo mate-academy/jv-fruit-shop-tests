ccd326d187d0db2c4a730936232babbcff76d6eb
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-26 14:01:35 +0300

    Removed new throw in readFromFile_NonExistentFileExceptionMessage_Ok. Renamed methods Removed empty lines.

--- src/test/java/core/basesyntax/dao/FileReaderCsvImplTest.java
+++ src/test/java/core/basesyntax/dao/FileReaderCsvImplTest.java
@@ -20,7 +20,7 @@ public class FileReaderCsvImplTest {
     private final FileReader fileReader = new FileReaderCsvImpl();
 
     @Test
-    public void readFromGoodFile_Ok() throws Exception {
+    public void readFromFile_Ok() throws Exception {
         String expectedLines = "type,fruit,quantity\n"
                 + "b,banana,20\n"
                 + "b,apple,100\n"
@@ -30,49 +30,40 @@ public class FileReaderCsvImplTest {
                 + "s,apple,20\n"
                 + "p,banana,5\n"
                 + "s,banana,50";
-
         try (FileWriter fileWriter = new FileWriter(FILENAME)) {
             fileWriter.write(expectedLines);
         } catch (IOException e) {
             throw new IOException("Can't write file to " + FILENAME, e);
         }
-
         List<String> lines = fileReader.readFromFile(FILENAME);
         String actualLines = String.join("\n", lines);
-
         assertEquals("Expected lines from file don't equals read readFromFile method",
                 expectedLines, actualLines);
     }
 
     @Test
-    public void readFromEmptyFile_Ok() throws Exception {
+    public void readFromFile_emptyFile_Ok() throws Exception {
         String expectedLines = "";
-
         try (FileWriter fileWriter = new FileWriter(FILENAME)) {
             fileWriter.write(expectedLines);
         } catch (IOException e) {
             throw new IOException("Can't write file to " + FILENAME, e);
         }
-
         List<String> lines = fileReader.readFromFile(FILENAME);
         String actualLines = String.join("\n", lines);
-
         assertEquals("Expected lines from file don't equals read readFromFile method",
                 expectedLines, actualLines);
     }
 
     @Test(expected = RuntimeException.class)
-    public void readFromNonExistentFile_NotOk() {
+    public void readFromFile_NonExistentFile_NotOk() {
         fileReader.readFromFile(NON_EXISTENT_FILENAME);
     }
 
     @Test
-    public void exceptionMessageReadFromNonExistentFile_NotOk() {
+    public void readFromFile_NonExistentFileExceptionMessage_Ok() {
         expectedEx.expect(RuntimeException.class);
         expectedEx.expectMessage("Can't read from file " + NON_EXISTENT_FILENAME);
-
         fileReader.readFromFile(NON_EXISTENT_FILENAME);
-        System.out.println("=======Starting Exception process=======");
-        throw new RuntimeException("Can't read from file " + NON_EXISTENT_FILENAME);
     }
 }

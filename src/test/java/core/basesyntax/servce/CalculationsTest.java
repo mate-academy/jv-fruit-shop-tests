5a9eb9628268b9be262ac1e3e369b6f77aee6ec9
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-08-23 20:20:54 +0300

    All files from previous project are added

---
+++ src/test/java/core/basesyntax/servce/CalculationsTest.java
@@ -0,0 +1,12 @@
+package core.basesyntax.servce;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculationsTest {
+
+    @Test
+    public void generateReport() {
+    }
+}

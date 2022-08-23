8bfc67a10b46651cee668222de7ed7ae8ddf0daf
Benatti <ssunica0@gmail.com>
2022-08-23 17:03:19 +0300

    deleted unused import

--- src/test/java/core/service/impl/WriterServiceImplTest.java
+++ src/test/java/core/service/impl/WriterServiceImplTest.java
@@ -8,7 +8,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 

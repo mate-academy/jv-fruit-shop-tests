f2e5769562e545995cd1978d57932b49543f1e72
Serhii Buria <sergeiburya@gmail.com>
2022-08-29 10:15:57 +0300

    added test class implementation for StrategyImpl

---
+++ src/test/java/core/basesyntax/strategy/StrategyImplTest.java
@@ -0,0 +1,60 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.model.FruitOperation;
+import core.basesyntax.service.FruitService;
+import core.basesyntax.service.OperationHandler;
+import core.basesyntax.service.impl.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class StrategyImplTest {
+    private final StorageDao storageDao = new StorageDaoImpl();
+    private final FruitService fruitService = new FruitServiceImpl(storageDao);
+    private final Map<FruitOperation.Operation, OperationHandler> operationHandlerMap
+            = new HashMap<>();
+    private final Strategy strategy = new StrategyImpl(operationHandlerMap);
+
+    @Before
+    public void setUp() {
+        operationHandlerMap.put(FruitOperation.Operation.BALANCE, new BalanceOperationHandlerImpl(fruitService));
+        operationHandlerMap.put(FruitOperation.Operation.SUPPLY, new SupplyOperationHandlerImpl(fruitService));
+        operationHandlerMap.put(FruitOperation.Operation.PURCHASE, new PurchaseOperationHandlerImpl(fruitService));
+        operationHandlerMap.put(FruitOperation.Operation.RETURN, new ReturnOperationHandlerImpl(fruitService));
+    }
+
+    @Test
+    public void getHandler_Balance_Ok() {
+        OperationHandler actualBalance = strategy.get(FruitOperation
+                .Operation.BALANCE);
+        OperationHandler expectedBalance = new BalanceOperationHandlerImpl(fruitService);
+        assertEquals(expectedBalance.getClass(), actualBalance.getClass());
+    }
+    @Test
+    public void getHandler_Purchase_Ok() {
+        OperationHandler actualPurchase = strategy.get(FruitOperation
+                .Operation.PURCHASE);
+        OperationHandler expectedPurchase = new PurchaseOperationHandlerImpl(fruitService);
+        assertEquals(expectedPurchase.getClass(), actualPurchase.getClass());
+    }
+    @Test
+    public void getHandler_Supply_Ok() {
+        OperationHandler actualSupply = strategy.get(FruitOperation
+                .Operation.SUPPLY);
+        OperationHandler expectedSupply = new SupplyOperationHandlerImpl(fruitService);
+        assertEquals(expectedSupply.getClass(), actualSupply.getClass());
+    }
+
+    @Test
+    public void getHandler_Return_Ok() {
+        OperationHandler actualReturn = strategy.get(FruitOperation
+                .Operation.RETURN);
+        OperationHandler expectedReturn = new ReturnOperationHandlerImpl(fruitService);
+        assertEquals(expectedReturn.getClass(), actualReturn.getClass());
+    }
+}

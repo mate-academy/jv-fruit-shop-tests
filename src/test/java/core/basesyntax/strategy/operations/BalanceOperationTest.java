7d785036a54d7fc110e0b32dc353e572149254ac
Slava Verbanov <v.verbanov@gmail.com>
2022-08-30 08:35:03 +0200

    remove redundant comments and Main class

--- src/test/java/core/basesyntax/strategy/operations/BalanceOperationTest.java
+++ src/test/java/core/basesyntax/strategy/operations/BalanceOperationTest.java
@@ -1,13 +1,9 @@
 package core.basesyntax.strategy.operations;
 
-//import static org.junit.Assert.assertTrue;
-
 import core.basesyntax.dao.FruitDao;
 import core.basesyntax.dao.FruitDaoImpl;
 import core.basesyntax.db.StorageFruits;
 import core.basesyntax.exceptions.WrongDataException;
-//import core.basesyntax.model.Fruit;
-//import core.basesyntax.model.FruitTransaction;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,18 +22,6 @@ public class BalanceOperationTest {
     public void apply_dailyTransactionNull_notOk() {
         balanceOperation.apply(null);
     }
-
-    /*@Test
-    public void apply_allOk() {
-        FruitTransaction fruitTransaction = new FruitTransaction(
-                FruitTransaction.Operation.BALANCE, "kiwi", 200);
-        balanceOperation.apply(fruitTransaction);
-        Fruit actual = StorageFruits.fruits.get(0);
-        Fruit expected = new Fruit("kiwi", 200);
-        boolean isEqual = actual.equals(expected);
-        assertTrue(isEqual);
-    }
-    */
 
     @After
     public void tearDown() throws Exception {

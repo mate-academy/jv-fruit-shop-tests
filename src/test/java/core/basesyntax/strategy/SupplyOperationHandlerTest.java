4f940b831de21268a21de2fcdd7706debec3c5d7
LubusUzh <lubus.uzh@gmail.com>
2022-09-02 00:05:57 +0300

    Add: tests to SupplyOperationHandler

--- src/test/java/core/basesyntax/strategy/SupplyOperationHandlerTest.java
+++ src/test/java/core/basesyntax/strategy/SupplyOperationHandlerTest.java
@@ -1,7 +1,47 @@
 package core.basesyntax.strategy;
 
-import static org.junit.Assert.*;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.Transaction;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class SupplyOperationHandlerTest {
+    private SupplyOperationHandler supplyOperationHandler = new SupplyOperationHandler();
+    private Fruit fruit;
 
+    @Before
+    public void setUp() throws Exception {
+        fruit = new Fruit("apple");
+    }
+
+    @Test
+    public void applySupply_OK() {
+        Fruit apple = new Fruit("apple");
+        Storage.storage.put(apple, 14);
+        supplyOperationHandler.apply(new Transaction("s", new Fruit("apple"), 13));
+        Integer expected = 27;
+        Integer actual = Storage.storage.get(fruit);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void negativeSupplyValue_NotOK() {
+        Fruit apple = new Fruit("apple");
+        Storage.storage.put(apple, 14);
+        supplyOperationHandler.apply(new Transaction("s", new Fruit("apple"), -13));
+        Integer expected = Integer.valueOf(27);
+        Integer actual = Storage.storage.get(fruit);
+        assertFalse(expected.equals(actual));
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void nullSupplyValue_NotOK() {
+        Fruit apple = new Fruit("apple");
+        Storage.storage.put(apple, 14);
+        supplyOperationHandler.apply(new Transaction("s", new Fruit("apple"), null));
+    }
 }

0b6955d7f31de96f019bcaec8b4001895758b419
LubusUzh <lubus.uzh@gmail.com>
2022-09-01 18:39:50 +0300

    Add: tests to WriteToFile and remove redundant code from WriteToFileImpl

--- src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/WriterServiceImpl.java
@@ -4,18 +4,13 @@ import core.basesyntax.service.WriterService;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WriterServiceImpl implements WriterService {
     @Override
     public boolean writeToFile(String filePath, String report) {
-        File file = new File(filePath);
         try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file " + filePath, e);
-        }
-        try {
-            Files.write(file.toPath(), report.getBytes());
+            Files.write(Path.of(filePath), report.getBytes());
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file " + filePath, e);
         }
--- src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
@@ -1,7 +1,59 @@
 package core.basesyntax.service.impl;
 
+import core.basesyntax.service.ReportCreatorService;
+import core.basesyntax.service.WriterService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.Assert.*;
 
 public class WriterServiceImplTest {
+    private WriterService writerService;
+    private ReportCreatorService reportCreatorService;
 
+    @Before
+    public void setUp() throws Exception {
+        writerService = new WriterServiceImpl();
+        reportCreatorService = new ReportCreatorServiceImpl();
+    }
+
+    @Test
+    public void writeToFile_OK() {
+        String path = "src/main/resources/report.csv";
+        String expected = null;
+        try {
+            expected = Files.readString(Path.of("src/main/resources/report.csv"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String actual = reportCreatorService.createReport();
+        writerService.writeToFile(path, actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void nullPath_WriteToFile_NotOK() {
+        writerService.writeToFile(null, reportCreatorService.createReport());
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void wrongPath_WriteToFile_NotOK() {
+        String path = "src/main/resources/smth/report.csv";
+        writerService.writeToFile(path, reportCreatorService.createReport());
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void nullReport_WriteToFile_NotOK() {
+        String path = "src/main/resources/report.csv";
+        writerService.writeToFile(path, null);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void nullValues_WriteToFile_NotOK() {
+        writerService.writeToFile(null, null);
+    }
 }

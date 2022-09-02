96d7993159271240b99c284c87a64b2fd5deef0a
Anna Popova <anitapopova1996@gmail.com>
2022-09-02 08:32:10 +0300

    Changed FruitServiceImplTest creatorReport() and removed inner static class.

--- src/test/java/core/basesyntax/service/impl/FruitServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FruitServiceImplTest.java
@@ -3,6 +3,8 @@ package core.basesyntax.service.impl;
 import static org.junit.Assert.assertEquals;
 
 import core.basesyntax.dao.FruitDao;
+import core.basesyntax.dao.FruitDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.Fruit;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.FruitService;
@@ -19,12 +21,11 @@ import org.junit.Test;
 
 public class FruitServiceImplTest {
     private static FruitDao fruitDao;
-    private static Map<Fruit, Integer> test;
     private static FruitService fruitService;
     private static Map<FruitTransaction.Operation, OperationHandler> defaultMap;
 
     @BeforeClass
-    public static void beforeClass() throws Exception {
+    public static void beforeClass() {
         defaultMap = new HashMap<>();
         defaultMap.put(FruitTransaction.Operation.BALANCE,
                 new FruitBalanceHandler(fruitDao));
@@ -38,45 +39,17 @@ public class FruitServiceImplTest {
 
     @Before
     public void setUp() throws Exception {
-        fruitDao = new FruitDaoTestImpl();
-        test = new HashMap<>();
-        fruitService = new FruitServiceImpl(defaultMap,fruitDao);
+        fruitDao = new FruitDaoImpl();
+        fruitService = new FruitServiceImpl(defaultMap, fruitDao);
     }
 
     @Test
     public void createReport() {
-        test.put(new Fruit("banana"), 152);
-        test.put(new Fruit("apple"), 90);
-        String expected = "fruits,quantity" + System.lineSeparator()
+        Storage.fruits.put(new Fruit("banana"), 152);
+        Storage.fruits.put(new Fruit("apple"), 90);
+        String expected = fruitService.createReport();
+        String actual = "fruits,quantity" + System.lineSeparator()
                 + "banana,152" + System.lineSeparator() + "apple,90";
-        String actual = fruitService.createReport();
         assertEquals(expected, actual);
-    }
-
-    private static class FruitDaoTestImpl implements FruitDao {
-        @Override
-        public void set(Fruit fruitData, int quantity) {
-        }
-
-        @Override
-        public void add(Fruit fruit, int quantity) {
-        }
-
-        @Override
-        public void subtract(Fruit fruitData, int quantity) {
-        }
-
-        @Override
-        public int get(Fruit fruitName) {
-            return 0;
-        }
-
-        @Override
-        public Map<Fruit, Integer> getAll() {
-            Map<Fruit, Integer> test = new HashMap<>();
-            test.put(new Fruit("banana"), 152);
-            test.put(new Fruit("apple"), 90);
-            return test;
-        }
     }
 }

8b12c10de481bfae6d952ef916e9fbe53095b7ac
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-09-02 06:24:09 +0300

    add ReportServiceImplTest

---
+++ src/test/java/core/basesyntax/service/ReportServiceImplTest.java
@@ -0,0 +1,25 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.Fruit;
+import core.basesyntax.service.impl.ReportServiceImpl;
+import core.basesyntax.storage.Storage;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReportServiceImplTest {
+    @Test
+    public void report_correctData_ok() {
+        Map<Fruit, Integer> data = Storage.dataBase;
+        data.put(new Fruit("banana"), 107);
+        data.put(new Fruit("apple"), 110);
+        ReportService reportService = new ReportServiceImpl();
+        String actual = reportService.report();
+        System.out.println(actual);
+        String expected = "fruit,quantity" + System.lineSeparator()
+                + "banana,107" + System.lineSeparator()
+                + "apple,110" + System.lineSeparator();
+        System.out.println(expected);
+        Assert.assertEquals(expected, actual);
+    }
+}

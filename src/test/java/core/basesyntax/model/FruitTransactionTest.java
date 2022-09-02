d8ac59a9350cbd9bb5567be9662ebc658a6c769a
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-09-02 12:40:50 +0300

    created FruitTransactionTest

---
+++ src/test/java/core/basesyntax/model/FruitTransactionTest.java
@@ -0,0 +1,32 @@
+package core.basesyntax.model;
+
+import static core.basesyntax.model.FruitTransaction.Operation.BALANCE;
+import static core.basesyntax.model.FruitTransaction.Operation.PURCHASE;
+import static core.basesyntax.model.FruitTransaction.Operation.RETURN;
+import static core.basesyntax.model.FruitTransaction.Operation.SUPPLY;
+import static core.basesyntax.model.FruitTransaction.getOperationByLetter;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.NoSuchElementException;
+
+public class FruitTransactionTest {
+
+    @Test
+    public void getExistingOperation_Ok() {
+        Assert.assertEquals(BALANCE, getOperationByLetter("b"));
+        Assert.assertEquals(SUPPLY, getOperationByLetter("s"));
+        Assert.assertEquals(PURCHASE, getOperationByLetter("p"));
+        Assert.assertEquals(RETURN, getOperationByLetter("r"));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void getByInvalidLetter_NotOk() {
+        getOperationByLetter("u");
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void getByNull_NotOk() {
+        getOperationByLetter(null);
+    }
+
+}

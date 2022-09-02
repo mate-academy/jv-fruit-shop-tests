9b3b37ac0e1078ef1ad5469f07824ffee3a6f4c0
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-09-02 07:23:29 +0300

    add SupplyOperationHandlerImplTest

---
+++ src/test/java/core/basesyntax/strategy/SupplyOperationHandlerImplTest.java
@@ -0,0 +1,38 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.storage.Storage;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SupplyOperationHandlerImplTest {
+    private Map<Fruit, Integer> dataBase = Storage.dataBase;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Storage.dataBase.clear();
+    }
+
+    @Test
+    public void apply_dataInDatabase_ok() {
+        Fruit fruit = new Fruit("banana");
+        dataBase.put(fruit, 113);
+        OperationHandler operationHandler = new ReturnOperationHandlerImpl();
+        FruitTransaction transaction = new FruitTransaction(
+                FruitTransaction.Operation.RETURN, new Fruit("banana"), 15);
+        operationHandler.apply(transaction);
+        Map<Fruit, Integer> expected = new HashMap<>();
+        expected.put(fruit, 128);
+        Assert.assertEquals("Incorrect output.", expected, dataBase);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dataBase.clear();
+    }
+}

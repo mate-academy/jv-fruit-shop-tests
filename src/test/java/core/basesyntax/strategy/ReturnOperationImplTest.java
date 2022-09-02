c44cf7bfa7f33b90c70a4a3769a325ffc8472b43
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-02 12:23:12 +0200

    implement КÑReturnOperationImplTest

--- src/test/java/core/basesyntax/strategy/ReturnOperationImplTest.java
+++ src/test/java/core/basesyntax/strategy/ReturnOperationImplTest.java
@@ -1,7 +1,29 @@
 package core.basesyntax.strategy;
 
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.Transaction;
+import core.basesyntax.storage.Storage;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ReturnOperationImplTest {
+    private OperationHandler operationHandler;
+    private Fruit apple;
 
+    @Before
+    public void setUp() throws Exception {
+        operationHandler = new ReturnOperationImpl();
+        apple = new Fruit("apple");
+    }
+
+    @Test
+    public void applyReturn_OK() {
+        Storage.storage.put(apple, 10);
+        operationHandler.apply(new Transaction("p", apple, 10));
+        Integer expected = 20;
+        Integer actual = Storage.storage.get(apple);
+        assertEquals(expected, actual);
+    }
 }

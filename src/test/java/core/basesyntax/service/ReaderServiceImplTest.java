6c3ce03ab4e672dc87e2c222c63b2b9923866dbc
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-09-03 13:37:04 +0300

    creating private reader method

--- src/test/java/core/basesyntax/service/ReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/service/ReaderServiceImplTest.java
@@ -2,6 +2,7 @@ package core.basesyntax.service;
 
 import core.basesyntax.service.impl.ReaderServiceImpl;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.junit.Assert;
@@ -12,15 +13,13 @@ import org.junit.rules.ExpectedException;
 public class ReaderServiceImplTest {
     private static final String FILE_NOT_EXIST = "report";
     @Rule
-    public ExpectedException thrown = ExpectedException.none();
+    public ExpectedException expectedException = ExpectedException.none();
     private ReaderService readerService = new ReaderServiceImpl();
 
     @Test
     public void reading_file_which_not_exist_Not_OKey() {
-        thrown.expect(RuntimeException.class);
-        thrown.expectMessage("File " + FILE_NOT_EXIST + " could not be read");
-        File directory = new File(FILE_NOT_EXIST);
-        boolean bool = directory.mkdir();
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("File " + FILE_NOT_EXIST + " could not be read");
         readerService.readFromFile(FILE_NOT_EXIST);
     }
 
@@ -28,7 +27,10 @@ public class ReaderServiceImplTest {
     public void reading_file_Okey() throws Exception {
         readerService.readFromFile("src/test/java/resources/yo.txt");
         Assert.assertEquals("yo",
-                Files.readAllLines(Paths.get("src/test/java/resources/yo.txt")).get(0));
+                readFileline());
+    }
+    private String readFileline() throws IOException {
+     return Files.readAllLines(Paths.get("src/test/java/resources/yo.txt")).get(0);
     }
 }
 

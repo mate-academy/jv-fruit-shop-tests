2568a3046548ac3f1d1fd4af5a3491d180e6bf8a
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-09-03 11:29:31 +0300

    copied the code form the previous task

---
+++ src/main/java/core/basesyntax/operation/BalanceOperationHandler.java
@@ -0,0 +1,36 @@
+package core.basesyntax.operation;
+
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.model.FruitTransaction;
+import java.util.Objects;
+
+public class BalanceOperationHandler implements OperationHandler {
+    private final FruitDao fruitDao;
+
+    public BalanceOperationHandler(FruitDao fruitDao) {
+        this.fruitDao = fruitDao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BalanceOperationHandler that = (BalanceOperationHandler) o;
+        return Objects.equals(fruitDao, that.fruitDao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fruitDao);
+    }
+
+    @Override
+    public void handle(FruitTransaction transaction) {
+        fruitDao.addFruit(transaction.getFruit(), transaction.getQuantity());
+
+    }
+}
---
+++ src/main/java/core/basesyntax/operation/SupplyOperationHandler.java
@@ -0,0 +1,40 @@
+package core.basesyntax.operation;
+
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.model.FruitTransaction;
+import java.util.Objects;
+
+public class SupplyOperationHandler implements OperationHandler {
+    private final FruitDao fruitDao;
+
+    public SupplyOperationHandler(FruitDao fruitDao) {
+        this.fruitDao = fruitDao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SupplyOperationHandler that = (SupplyOperationHandler) o;
+        return Objects.equals(fruitDao, that.fruitDao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fruitDao);
+    }
+
+    @Override
+    public void handle(FruitTransaction transaction) {
+        String fruit = transaction.getFruit();
+        if (fruitDao.contains(fruit)) {
+            fruitDao.addFruit(fruit, fruitDao.getQuantity(fruit) + transaction.getQuantity());
+        } else {
+            fruitDao.addFruit(fruit, transaction.getQuantity());
+        }
+    }
+}

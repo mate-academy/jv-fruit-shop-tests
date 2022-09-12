c111f53f24293514a10bf126e70d6d3bee378f64
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-09-12 19:55:09 +0300

    added test methods for check how correctly class ReaderServiceImpl work

---
+++ src/test/java/core/basesyntax/service/impl/ReaderServiceImplTest.java
@@ -0,0 +1,30 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.ReaderService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReaderServiceImplTest {
+    private static final String RIGHT_FILE_NAME
+            = "src/main/java/core/basesyntax/resources/inputFile.csv";
+    private static final String WRONG_FILE_NAME
+            = "src/main/java/core/basesyntax/resources/input.csv";
+    private ReaderService readerService;
+
+    @Before
+    public void setUp() {
+        readerService = new ReaderServiceImpl();
+    }
+
+    @Test
+    public void readFromPathIs_Ok() {
+        Assert.assertTrue(readerService.readFromFile(RIGHT_FILE_NAME).size() > 0);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void readFromPathIs_NotOk() {
+        readerService.readFromFile(WRONG_FILE_NAME);
+    }
+}

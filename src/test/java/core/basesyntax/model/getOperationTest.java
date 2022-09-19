35c61462263a68129767d7f38edcd945c52fb346
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-09-19 10:54:52 +0300

    created getOperationTest class

---
+++ src/test/java/core/basesyntax/model/getOperationTest.java
@@ -0,0 +1,23 @@
+package core.basesyntax.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class getOperationTest {
+
+    @Test
+    public void findOperationByName_Ok() {
+        Transaction.Operation expected = Transaction.Operation.BALANCE;
+        Transaction.Operation actual = Transaction.findOperationByName("b");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findOperationByName_NotOk() {
+        try {
+            Transaction.Operation actual = Transaction.findOperationByName("wrongOperation");
+        } catch (Exception e) {
+            Assert.assertSame(RuntimeException.class, e.getClass());
+        }
+    }
+}

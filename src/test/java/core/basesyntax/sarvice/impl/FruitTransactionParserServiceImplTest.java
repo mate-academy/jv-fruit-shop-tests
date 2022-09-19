be0548a2ab9027cabc8e0590aa8df11494c80698
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2022-09-19 21:44:40 +0300

    Changed the test parse_notEmptyList_Ok

--- src/test/java/core/basesyntax/sarvice/impl/FruitTransactionParserServiceImplTest.java
+++ src/test/java/core/basesyntax/sarvice/impl/FruitTransactionParserServiceImplTest.java
@@ -1,41 +1,36 @@
 package core.basesyntax.sarvice.impl;
 
+import static org.junit.Assert.assertEquals;
+
 import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.FruitTransactionParserService;
 import core.basesyntax.service.impl.FruitTransactionParserServiceImpl;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FruitTransactionParserServiceImplTest {
-    private static FruitTransactionParserServiceImpl parser;
-    private static List<FruitTransaction> expected;
+    private static FruitTransactionParserService parser;
 
     @BeforeClass
     public static void beforeClass() {
         parser = new FruitTransactionParserServiceImpl();
-        expected = new LinkedList<>();
     }
 
-    @Before
-    public void setUp() {
+    @Test
+    public void parse_notEmptyList_Ok() {
+        List<FruitTransaction> expected = new ArrayList<>();
         expected.add(new FruitTransaction(
                 FruitTransaction.Operation.BALANCE, "banana", 20));
         expected.add(new FruitTransaction(
                 FruitTransaction.Operation.BALANCE, "apple", 100));
         expected.add(new FruitTransaction(
                 FruitTransaction.Operation.SUPPLY, "banana", 100));
-    }
-
-    @Test
-    public void parse_notEmptyList_Ok() {
         List<String> inputData = new ArrayList<>();
         inputData.add("type,fruit,quantity");
         inputData.addAll(List.of("b,banana,20", "b,apple,100", "s,banana,100"));
         List<FruitTransaction> actual = parser.parse(inputData);
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 }

49ebcd9f178e6a6c67c9c551bf3b288918aa667e
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-10-03 22:45:33 +0300

    checkstyle changes

--- src/test/java/core/basesyntax/tests/ServicesTest.java
+++ src/test/java/core/basesyntax/tests/ServicesTest.java
@@ -17,7 +17,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class ServicesTest {
     private ReadingService readingService = new ReadingServiceImpl();

59fe0ba3bd027e5928f2c3f743f5fcd86167c07f
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-05 21:30:12 +0300

    Add Writer and Reader tests

---
+++ src/test/java/core/basesyntax/service/WriteReadImpTest.java
@@ -0,0 +1,15 @@
+package core.basesyntax.service;
+
+import core.basesyntax.service.base.WriterAndReadServiceTestBase;
+
+public class WriteReadImpTest extends WriterAndReadServiceTestBase<CsvFileWriterService,CsvFileReaderService> {
+    @Override
+    protected CsvFileWriterService createWriterInstance() {
+        return new CsvFileWriterService();
+    }
+
+    @Override
+    protected CsvFileReaderService createReaderInstance() {
+        return new CsvFileReaderService();
+    }
+}

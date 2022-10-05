07e9db181aa99d5232bf0d26d3f24ea27ed2364c
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-05 19:53:31 +0300

    Add test for FruitService

---
+++ src/test/java/core/basesyntax/service/FruitServiceImplTest.java
@@ -0,0 +1,17 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.Operation;
+import java.util.Map;
+
+public class FruitServiceImplTest extends FruitServiceTestBase<FruitServiceImpl> {
+    @Override
+    protected FruitServiceImpl createInstance() {
+        Map<Operation, OperationHandler> operations = Map.of(
+                Operation.PURCHASE, new PurchaseOperationHandler(),
+                Operation.BALANCE, new BalanceOperationHandler(),
+                Operation.RETURN, new ReturnOperationHandler(),
+                Operation.SUPPLY, new SupplyOperationHandler()
+        );
+        return new FruitServiceImpl(operations);
+    }
+}

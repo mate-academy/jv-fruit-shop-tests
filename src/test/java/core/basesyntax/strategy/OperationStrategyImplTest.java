bb41add5c96165848733c455ac5b071f9d3cc99a
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-05 14:31:33 +0300

    Add OperationStrategy test

---
+++ src/test/java/core/basesyntax/strategy/OperationStrategyImplTest.java
@@ -0,0 +1,22 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.model.Operation;
+import core.basesyntax.service.BalanceOperationHandler;
+import core.basesyntax.service.OperationHandler;
+import core.basesyntax.service.PurchaseOperationHandler;
+import core.basesyntax.service.ReturnOperationHandler;
+import core.basesyntax.service.SupplyOperationHandler;
+import java.util.Map;
+
+public class OperationStrategyImplTest extends OperationStrategyTestBase<OperationStrategyImpl>  {
+    @Override
+    protected OperationStrategyImpl createInstance() {
+        Map<Operation, OperationHandler> operations = Map.of(
+                Operation.PURCHASE, new PurchaseOperationHandler(),
+                Operation.BALANCE, new BalanceOperationHandler(),
+                Operation.RETURN, new ReturnOperationHandler(),
+                Operation.SUPPLY, new SupplyOperationHandler()
+        );
+        return new OperationStrategyImpl(operations);
+    }
+}

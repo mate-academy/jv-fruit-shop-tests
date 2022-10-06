f89b6a7144882e6508df0c0a65eff381e36aafd8
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-06 17:48:26 +0300

    Recreate FruitServiceTest logic

--- src/test/java/core/basesyntax/service/FruitServiceImplTest.java
+++ src/test/java/core/basesyntax/service/FruitServiceImplTest.java
@@ -1,18 +1,85 @@
 package core.basesyntax.service;
 
-import core.basesyntax.model.Operation;
-import core.basesyntax.service.base.FruitServiceTestBase;
-import java.util.Map;
+import static org.junit.Assert.assertEquals;
 
-public class FruitServiceImplTest extends FruitServiceTestBase<FruitServiceImpl> {
-    @Override
-    protected FruitServiceImpl createInstance() {
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.Operation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FruitServiceImplTest {
+    private static FruitService service;
+    private List<String> transactions;
+
+    @BeforeClass
+    public static void beforeClass() {
         Map<Operation, OperationHandler> operations = Map.of(
                 Operation.PURCHASE, new PurchaseOperationHandler(),
                 Operation.BALANCE, new BalanceOperationHandler(),
                 Operation.RETURN, new ReturnOperationHandler(),
                 Operation.SUPPLY, new SupplyOperationHandler()
         );
-        return new FruitServiceImpl(operations);
+        service = new FruitServiceImpl(operations);
+    }
+
+    @Before
+    public void setUp() {
+        transactions = List.of(
+                "fruit,quantity",
+                "b,banana,10",
+                "b,apple,10",
+                "s,banana,1",
+                "s,apple,1",
+                "p,banana,2",
+                "p,apple,2",
+                "r,banana,1",
+                "r,apple,1"
+        );
+    }
+
+    @After
+    public void tearDown() {
+        Storage.storage.clear();
+    }
+
+    @Test
+    public void add_Correct_Data_To_Storage_ok() {
+        service.addToStorage(transactions);
+        Map<Fruit, Integer> expected = Map.of(
+                new Fruit("banana"), 10,
+                new Fruit("apple"), 10
+        );
+        assertEquals(expected, Storage.storage);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void add_Null_To_Storage_notOk() {
+        transactions = new ArrayList<>();
+        transactions.add("fruit,quantity");
+        transactions.add(null);
+        service.addToStorage(transactions);
+    }
+
+    @Test
+    public void generate_Report_Empty_Storage_ok() {
+        assertEquals("fruit,quantity" + System.lineSeparator(), service.generateReport());
+    }
+
+    @Test
+    public void generate_Report_Full_Storage_ok() {
+        service.addToStorage(transactions);
+        String expected = "fruit,quantity"
+                + System.lineSeparator()
+                + "banana,10"
+                + System.lineSeparator()
+                + "apple,10"
+                + System.lineSeparator();
+        assertEquals(expected, service.generateReport());
     }
 }

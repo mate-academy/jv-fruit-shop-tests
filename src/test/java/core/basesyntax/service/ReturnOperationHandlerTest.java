d40d2a0ccbb14533c4d0a793257ffef018219b82
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-06 20:04:32 +0300

    Add ReturnOperationHandlerTest

---
+++ src/test/java/core/basesyntax/service/ReturnOperationHandlerTest.java
@@ -0,0 +1,45 @@
+package core.basesyntax.service;
+
+import static org.junit.Assert.*;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.model.Operation;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ReturnOperationHandlerTest {
+    private static OperationHandler handler;
+    private static Operation operation;
+    private static Fruit fruit;
+    private FruitTransaction transaction;
+
+    @BeforeClass
+    public static void beforeClass() {
+        operation = Operation.RETURN;
+        fruit = new Fruit("banana");
+        handler = new ReturnOperationHandler();
+        Storage.storage.clear();
+        Storage.storage.put(fruit,9);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        Storage.storage.clear();
+    }
+
+    @Test
+    public void handle_Return_Operation_ok() {
+        transaction = new FruitTransaction(operation, fruit, 1);
+        handler.handle(transaction);
+        assertEquals(Integer.valueOf(10), Storage.storage.get(fruit));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void handle_Return_Operation_notOk() {
+        transaction = new FruitTransaction(operation, fruit, -1);
+        handler.handle(transaction);
+    }
+}

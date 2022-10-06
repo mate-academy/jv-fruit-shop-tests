ec803e9f6995b8fb56628bc4e33dda54d41b0ad7
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-06 20:06:33 +0300

    Add SupplyOperationHandlerTest

---
+++ src/test/java/core/basesyntax/service/SupplyOperationHandlerTest.java
@@ -0,0 +1,45 @@
+package core.basesyntax.service;
+
+import static org.junit.Assert.*;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.model.Operation;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SupplyOperationHandlerTest {
+    private static OperationHandler handler;
+    private static Operation operation;
+    private static Fruit fruit;
+    private FruitTransaction transaction;
+
+    @BeforeClass
+    public static void beforeClass() {
+        operation = Operation.SUPPLY;
+        fruit = new Fruit("banana");
+        handler = new SupplyOperationHandler();
+        Storage.storage.clear();
+        Storage.storage.put(fruit,10);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        Storage.storage.clear();
+    }
+
+    @Test
+    public void handle_Supply_Operation_ok() {
+        transaction = new FruitTransaction(operation, fruit, 1);
+        handler.handle(transaction);
+        assertEquals(Integer.valueOf(11), Storage.storage.get(fruit));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void handle_Supply_Operation_notOk() {
+        transaction = new FruitTransaction(operation, fruit, -1);
+        handler.handle(transaction);
+    }
+}

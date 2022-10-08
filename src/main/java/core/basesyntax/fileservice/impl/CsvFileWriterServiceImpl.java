bbb4b9419d08c4b1a5e16e6d2b94d554520c5089
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-10-08 12:21:49 +0200

    File.writer was changed on BufferedWriter

--- src/main/java/core/basesyntax/fileservice/impl/CsvFileWriterServiceImpl.java
+++ src/main/java/core/basesyntax/fileservice/impl/CsvFileWriterServiceImpl.java
@@ -3,11 +3,8 @@ package core.basesyntax.fileservice.impl;
 import core.basesyntax.fileservice.CsvFileWriterService;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class CsvFileWriterServiceImpl implements CsvFileWriterService {
     @Override

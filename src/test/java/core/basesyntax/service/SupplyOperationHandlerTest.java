83b71294277d0d3ef557af5b26d2deb1dc2abc6f
BP2117575 <marjanavv@gmail.com>
2022-10-14 15:46:01 +0200

    fruit shop tests, checkstyle

--- src/test/java/core/basesyntax/service/SupplyOperationHandlerTest.java
+++ src/test/java/core/basesyntax/service/SupplyOperationHandlerTest.java
@@ -4,7 +4,6 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class SupplyOperationHandlerTest {

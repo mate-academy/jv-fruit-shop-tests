77fde07fba9b5664ca492c49809756b02133299c
Yura Hryniv <superyura444@gmail.com>
2022-10-30 14:41:21 +0200

    Modified ReportServiceTest

--- src/test/java/core/basesyntax/ReportServiceTest.java
+++ src/test/java/core/basesyntax/ReportServiceTest.java
@@ -21,16 +21,16 @@ public class ReportServiceTest {
         Storage.storageContents.put("banana", 152);
         Storage.storageContents.put("apple", 90);
         Storage.storageContents.put("oranges", 270);
-        String expected = "fruit,quantity\r\n"
-                + "banana,152\r\n"
-                + "apple,90\r\n"
+        String expected = "fruit,quantity" + System.lineSeparator()
+                + "banana,152" + System.lineSeparator()
+                + "apple,90" + System.lineSeparator()
                 + "oranges,270";
         String actual = reportService.generate();
         assertEquals(expected, actual);
     }
 
     @Test(expected = RuntimeException.class)
-    public void reportService_generateReportFromEmptyStorage_ok() {
+    public void reportService_generateReportFromEmptyStorage_notOk() {
         String report = reportService.generate();
     }
 }

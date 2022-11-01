336987ba29b0f84016a8c42b9b2d6d615b091707
Nazarii Pertsak <pertsak98@gmail.com>
2022-11-01 11:10:37 +0200

    moved expected variable assigning to @BeforeClass method

--- src/test/java/core/basesyntax/service/read/impl/FruitTransactionParserImplTest.java
+++ src/test/java/core/basesyntax/service/read/impl/FruitTransactionParserImplTest.java
@@ -15,12 +15,19 @@ public class FruitTransactionParserImplTest {
     private static final String FRUIT_NAME = "fruit";
     private static final String FRUIT_QUANTITY_NAME = "quantity";
     private static FruitTransactionParser fruitTransactionParser;
+    private static List<FruitTransaction> expected;
 
     @BeforeClass
     public static void beforeClass() {
         fruitTransactionParser =
                 new FruitTransactionParserImpl(CSV_SEPARATOR, OPERATION_TYPE_NAME,
                         FRUIT_QUANTITY_NAME, FRUIT_NAME);
+        expected = new ArrayList<>();
+        expected.add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 20));
+        expected.add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "apple", 100));
+        expected.add(new FruitTransaction(FruitTransaction.Operation.SUPPLY, "banana", 103));
+        expected.add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 13));
+        expected.add(new FruitTransaction(FruitTransaction.Operation.RETURN, "apple", 10));
     }
 
     @Test
@@ -41,12 +48,6 @@ public class FruitTransactionParserImplTest {
         testData.add("s,banana,103");
         testData.add("p,banana,13");
         testData.add("r,apple,10");
-        List<FruitTransaction> expected = new ArrayList<>();
-        expected.add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 20));
-        expected.add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "apple", 100));
-        expected.add(new FruitTransaction(FruitTransaction.Operation.SUPPLY, "banana", 103));
-        expected.add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 13));
-        expected.add(new FruitTransaction(FruitTransaction.Operation.RETURN, "apple", 10));
         List<FruitTransaction> actual = fruitTransactionParser.parse(testData);
         assertEquals(expected, actual);
     }

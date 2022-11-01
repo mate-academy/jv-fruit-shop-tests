6d46b8f15b2c290c98ecf13aaeb4eee448255f4e
shmax <mshveda28@gmail.com>
2022-11-01 15:40:41 +0200

    Fix checkstyle error

--- src/test/java/core/basesyntax/service/FileWriterServiceTest.java
+++ src/test/java/core/basesyntax/service/FileWriterServiceTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import core.basesyntax.service.impl.FileWriterServiceImpl;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import org.junit.Test;
 

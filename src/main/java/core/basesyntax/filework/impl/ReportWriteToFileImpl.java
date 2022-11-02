4f0f8966613d9b0cd0a621fd4b8e6b25a5829474
Andrew Miroshnikov <giggle2594@gmail.com>
2022-11-02 16:55:00 +0200

    Tests with 100% coverage!!!

--- src/main/java/core/basesyntax/filework/impl/ReportWriteToFileImpl.java
+++ src/main/java/core/basesyntax/filework/impl/ReportWriteToFileImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.filework.impl;
 
 import core.basesyntax.filework.ReportWriteToFile;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;

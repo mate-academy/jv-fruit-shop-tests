e33c9b60c909fd071d904b3c1d740a7dfb56a07b
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-11-02 10:51:25 +0200

    added fixes

--- src/test/java/core/basesyntax/FileWriterServiceTest.java
+++ src/test/java/core/basesyntax/FileWriterServiceTest.java
@@ -1,30 +1,14 @@
 package core.basesyntax;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import core.basesyntax.service.FileWriterService;
 import core.basesyntax.service.impl.FileWriterServiceImpl;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import org.junit.Test;
 
 public class FileWriterServiceTest {
-    private static final String FILE_PATH_EXIST = "src/main/resources/report.csv";
     private static final String FILE_PATH_NOT_EXIST = "src/main/files/report.csv";
-    private static final String REPORT_SEPARATOR = System.lineSeparator();
-    private static final String TEST_REPORT = "fruit,quantity" + REPORT_SEPARATOR
-            + "banana,152" + REPORT_SEPARATOR + "apple,90";
-
     private final FileWriterService fileWriterService = new FileWriterServiceImpl();
-
-    @Test
-    public void writeToFile_ExistPath_Ok() {
-        fileWriterService.writeToFile(FILE_PATH_EXIST, TEST_REPORT);
-        String actual = readFromFile();
-        assertEquals(TEST_REPORT, actual);
-    }
 
     @Test
     public void readFromFile_NotExistFile_notOk() {
@@ -34,11 +18,4 @@ public class FileWriterServiceTest {
         });
     }
 
-    private String readFromFile() {
-        try {
-            return Files.readString(Path.of(FileWriterServiceTest.FILE_PATH_EXIST));
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file", e);
-        }
-    }
 }
--- src/test/java/core/basesyntax/FruitStorageDaoTest.java
+++ src/test/java/core/basesyntax/FruitStorageDaoTest.java
@@ -8,21 +8,41 @@ import core.basesyntax.dao.FruitStorageDao;
 import core.basesyntax.dao.FruitStorageDaoImpl;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.model.Operation;
+import core.basesyntax.service.operation.BalanceOperationHandler;
 import core.basesyntax.service.operation.OperationHandler;
+import core.basesyntax.service.operation.PurchaseOperationHandler;
+import core.basesyntax.service.operation.ReturnOperationHandler;
+import core.basesyntax.service.operation.SupplyOperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
 import core.basesyntax.strategy.OperationStrategyImpl;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FruitStorageDaoTest {
     private static final String TEST_FRUIT_NAME = "banana";
     private static final int TEST_QUANTITY = 20;
+    private static final int TEST_QUANTITY_2 = 40;
     private final Map<Operation, OperationHandler> operationMap = new HashMap<>();
     private final OperationStrategy operationStrategy = new OperationStrategyImpl(operationMap);
     private final FruitStorageDao fruitStorageDao = new FruitStorageDaoImpl(operationStrategy);
+
+    @Before
+    public void setUp() {
+        operationMap.put(Operation.BALANCE, new BalanceOperationHandler());
+        operationMap.put(Operation.SUPPLY, new SupplyOperationHandler());
+        operationMap.put(Operation.PURCHASE, new PurchaseOperationHandler());
+        operationMap.put(Operation.RETURN, new ReturnOperationHandler());
+    }
+
+    @After
+    public void afterEach() {
+        storage.clear();
+    }
 
     @Test
     public void addToStorage_correctData_Ok() {
@@ -31,6 +51,21 @@ public class FruitStorageDaoTest {
                 TEST_FRUIT_NAME, TEST_QUANTITY));
         fruitStorageDao.addToStorage(activities);
         Map<String, Integer> expected = Map.of(TEST_FRUIT_NAME, TEST_QUANTITY);
+        assertEquals(expected, storage);
+    }
+
+    @Test
+    public void addToStorage_alreadyExistKey_Ok() {
+        List<FruitTransaction> activities = new ArrayList<>();
+        activities.add(new FruitTransaction(Operation.BALANCE,
+                TEST_FRUIT_NAME, TEST_QUANTITY));
+        activities.add(new FruitTransaction(Operation.BALANCE,
+                TEST_FRUIT_NAME, TEST_QUANTITY_2));
+        fruitStorageDao.addToStorage(activities);
+        Integer expectedValue = operationStrategy
+                .get(Operation.BALANCE)
+                .apply(TEST_QUANTITY, TEST_QUANTITY_2);
+        Map<String, Integer> expected = Map.of(TEST_FRUIT_NAME, expectedValue);
         assertEquals(expected, storage);
     }
 

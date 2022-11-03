cd48d857796a37d42ddc75f79f9c0526b026bd34
galenkoigor <galenkoigor@gmail.com>
2022-11-03 13:09:56 +0100

    refactored with mentor's recommendations

--- src/test/java/core/basesyntax/service/ReportingServiceTest.java
+++ src/test/java/core/basesyntax/service/ReportingServiceTest.java
@@ -1,6 +1,7 @@
 package core.basesyntax.service;
 
-import core.basesyntax.service.impl.CsvFormatDataProcessingServiceImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
 import core.basesyntax.service.impl.CsvReportServiceImpl;
 import java.util.List;
 import org.junit.AfterClass;
@@ -9,21 +10,21 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ReportingServiceTest {
+    private static final int EXPECTED_BANANA_AMOUNT = 162;
+    private static final int EXPECTED_APPLE_AMOUNT = 80;
     private static ReportingService reportingService;
     private static List<String> reportList;
-    private static DataProcessingService processingService;
 
     @BeforeClass
     public static void setUp() {
         reportList = InitialisationService.getReportList();
-        processingService = new CsvFormatDataProcessingServiceImpl(
-                InitialisationService.getFruitDaoStrategyMap());
         reportingService = new CsvReportServiceImpl(reportList.get(0));
-        processingService.processingData(InitialisationService.getDataList());
     }
 
     @Test
     public void generateReport_ok() {
+        Storage.storage.put(new Fruit("banana"), EXPECTED_BANANA_AMOUNT);
+        Storage.storage.put(new Fruit("apple"), EXPECTED_APPLE_AMOUNT);
         Assert.assertEquals(reportList, reportingService.generateReport());
     }
 

c871ae8b3c89ef9481288d06b26daea4ac219217
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-11-03 11:38:56 +0200

    Loaded fruit-shop solution

---
+++ src/test/java/core/basesyntax/FileReaderImplTest.java
@@ -0,0 +1,38 @@
+package core.basesyntax;
+
+import core.basesyntax.service.FileReader;
+import core.basesyntax.service.impl.FileReaderImpl;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FileReaderImplTest {
+    private static FileReader fileReader;
+    private static final String INPUT_FILE_PATH = "src/main/resources/input.csv";
+    private static final String FAKE_FILE_PATH = "src/main/***/resources/input.csv";
+
+    @BeforeClass
+    public static void setUp() {
+        fileReader = new FileReaderImpl();
+    }
+
+    @Test
+    public void read_FromFile_Ok() {
+        List<String> expected = List.of("b,banana,20", "b,apple,100", "s,banana,100",
+                 "p,banana,13", "r,apple,10", "p,apple,20", "p,banana,5", "s,banana,50");
+        List<String> actual = fileReader.read(INPUT_FILE_PATH);
+        actual.remove(0);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void read_FromFakeFile_NotOk () {
+        assertThrows(RuntimeException.class, () -> fileReader.read(FAKE_FILE_PATH));
+    }
+}

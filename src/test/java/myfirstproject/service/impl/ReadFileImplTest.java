94af07eb71c54f6e2f7344f213d0024466c8dfae
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-11-04 18:28:03 +0200

    Wrote tests for services

--- src/test/java/myfirstproject/service/impl/ReadFileImplTest.java
+++ src/test/java/myfirstproject/service/impl/ReadFileImplTest.java
@@ -2,20 +2,49 @@ package myfirstproject.service.impl;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import myfirstproject.dao.FruitDao;
+import myfirstproject.dao.impl.FruitDaoImpl;
+import myfirstproject.model.Operation;
 import myfirstproject.service.ReadFile;
+import myfirstproject.service.WriteFile;
+import myfirstproject.strategy.BalanceOperation;
+import myfirstproject.strategy.OperationHandler;
+import myfirstproject.strategy.PurchaseOperation;
+import myfirstproject.strategy.ReturnOperation;
+import myfirstproject.strategy.SeparationOfOperations;
+import myfirstproject.strategy.SupplyOperation;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ReadFileImplTest {
-    private static final String FILE_EXIST = "src/test/resources/sourceFile.csv";
+
+    private static final String PATH_TO_NEW_FILE = "src/test/resources/sourceFile.csv";
+    private static final String PATH_TO_RESULT = "src/test/resources/reportFile.csv";
     private static final String FILE_NOT_EXIST = "wrong/file.csv";
     private static ReadFile fileReaderTest;
     private static final List<String[]> temporalList = new ArrayList<>();
 
     @BeforeClass
-    public static void setUp() {
+    public static void before() {
+        final FruitDao fruitDao = new FruitDaoImpl();
+        final ReadFile reader = new ReadFileImpl();
+        final WriteFile writer = new WriteFileImpl();
+        final SeparationOfOperations separation = new SeparationOfOperations();
+        final List<String[]> temporalList = new ArrayList<>();
+
+        Map<String, OperationHandler> operation = new HashMap<>();
+        operation.put(Operation.BALANCE.getOperation(), new BalanceOperation());
+        operation.put(Operation.SUPPLY.getOperation(), new SupplyOperation());
+        operation.put(Operation.PURCHASE.getOperation(), new PurchaseOperation());
+        operation.put(Operation.RETURN.getOperation(), new ReturnOperation());
+        reader.readFile(temporalList, PATH_TO_NEW_FILE);
+        separation.toDoEachOperation(temporalList, operation);
+        writer.writeToFile(PATH_TO_RESULT, fruitDao.getAll());
+
         fileReaderTest = new ReadFileImpl();
     }
 
@@ -25,7 +54,7 @@ public class ReadFileImplTest {
     }
 
     @Test
-    public void readFile_ok() {
+    public void readFile_Ok() {
         List<String[]> expectedList = new ArrayList<>();
         StringBuilder expectedBuilder = new StringBuilder();
         StringBuilder actualBuilder = new StringBuilder();
@@ -40,7 +69,7 @@ public class ReadFileImplTest {
         expectedList.add("p,banana,5".split(","));
         expectedList.add("s,banana,50".split(","));
 
-        fileReaderTest.readFile(temporalList,FILE_EXIST);
+        fileReaderTest.readFile(temporalList,PATH_TO_NEW_FILE);
         for (String[] s : expectedList) {
             expectedBuilder.append(Arrays.toString(s));
             expectedBuilder.append(System.lineSeparator());
@@ -54,4 +83,5 @@ public class ReadFileImplTest {
         String actual = actualBuilder.toString();
         Assert.assertEquals(actual, expected);
     }
+
 }
--- src/test/java/myfirstproject/service/impl/WriteFileImplTest.java
+++ src/test/java/myfirstproject/service/impl/WriteFileImplTest.java
@@ -3,18 +3,47 @@ package myfirstproject.service.impl;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import myfirstproject.dao.FruitDao;
+import myfirstproject.dao.impl.FruitDaoImpl;
 import myfirstproject.model.Fruit;
+import myfirstproject.model.Operation;
+import myfirstproject.service.ReadFile;
 import myfirstproject.service.WriteFile;
+import myfirstproject.strategy.BalanceOperation;
+import myfirstproject.strategy.OperationHandler;
+import myfirstproject.strategy.PurchaseOperation;
+import myfirstproject.strategy.ReturnOperation;
+import myfirstproject.strategy.SeparationOfOperations;
+import myfirstproject.strategy.SupplyOperation;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WriteFileImplTest {
     private static final String PATH_TO_NEW_FILE = "src/test/resources/reportFile.csv";
+    private static final String PATH_TO_RESULT = "src/test/resources/reportFile.csv";
+    private static final String WRONG_PATH = "src/resources/reportFile.csv";
+    private static WriteFile writer;
 
     @BeforeClass
     public static void before() {
+        final FruitDao fruitDao = new FruitDaoImpl();
+        final ReadFile reader = new ReadFileImpl();
+        final SeparationOfOperations separation = new SeparationOfOperations();
+        final List<String[]> temporalList = new ArrayList<>();
+        writer = new WriteFileImpl();
+        Map<String, OperationHandler> operation = new HashMap<>();
+        operation.put(Operation.BALANCE.getOperation(), new BalanceOperation());
+        operation.put(Operation.SUPPLY.getOperation(), new SupplyOperation());
+        operation.put(Operation.PURCHASE.getOperation(), new PurchaseOperation());
+        operation.put(Operation.RETURN.getOperation(), new ReturnOperation());
+        reader.readFile(temporalList, PATH_TO_NEW_FILE);
+        separation.toDoEachOperation(temporalList, operation);
+        writer.writeToFile(PATH_TO_RESULT, fruitDao.getAll());
+
         WriteFile writeFile = new WriteFileImpl();
         Map<Fruit, Integer> mapToWrite = new HashMap<>();
         Fruit fruit = new Fruit("banana");
@@ -23,8 +52,14 @@ public class WriteFileImplTest {
         writeFile.writeToFile(PATH_TO_NEW_FILE, mapToWrite);
     }
 
+    @Test(expected = RuntimeException.class)
+    public void readWrongFile_not_Ok() {
+        writer.writeToFile(WRONG_PATH, new HashMap<>());
+    }
+
     @Test
     public void isCreatedFileExist_Ok() {
         assertTrue(new File(PATH_TO_NEW_FILE).exists());
     }
+
 }

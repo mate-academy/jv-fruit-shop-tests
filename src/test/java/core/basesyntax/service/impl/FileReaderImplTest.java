8c188c415daca9ffb53c086731811165e0be0463
Luka Lozovyk <lukalozovyk@gmail.com>
2022-11-06 21:16:20 +0200

    Checkstyle corrected

--- src/test/java/core/basesyntax/service/impl/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileReaderImplTest.java
@@ -1,11 +1,11 @@
 package core.basesyntax.service.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import core.basesyntax.service.FileReader;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class FileReaderImplTest {
     private static final FileReader reader = new FileReaderImpl();

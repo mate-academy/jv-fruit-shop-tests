457fcbed007da66dc2774aecd7b5b8d6185f3e10
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-11-07 16:54:12 +0200

    deleted CsvFileReader from CsvFileWriterImplTest

--- src/test/java/core/basesyntax/services/impl/CsvFileReaderImplTest.java
+++ src/test/java/core/basesyntax/services/impl/CsvFileReaderImplTest.java
@@ -2,15 +2,24 @@ package core.basesyntax.services.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import core.basesyntax.services.CsvFileReader;
 import java.util.List;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CsvFileReaderImplTest {
+    private static CsvFileReader csvFileReader;
+
+    @BeforeClass
+    public static void globalSetUp() {
+        csvFileReader = new CsvFileReaderImpl();
+    }
+
     @Test
     public void getAcceptedFileAsListMethodTest_Ok() {
         List<String> expectedList = List.of("How are u doing",
                 "wass up", "100%N....");
-        List<String> actualList = new CsvFileReaderImpl()
+        List<String> actualList = csvFileReader
                 .getAcceptedFileAsList("src/test/testSource/inputTestFile.csv");
         assertEquals(expectedList, actualList);
     }
--- src/test/java/core/basesyntax/services/impl/CsvFileWriterImplTest.java
+++ src/test/java/core/basesyntax/services/impl/CsvFileWriterImplTest.java
@@ -2,19 +2,32 @@ package core.basesyntax.services.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import core.basesyntax.services.CsvFileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CsvFileWriterImplTest {
+    private static CsvFileWriter csvFileWriter;
+
+    @BeforeClass
+    public static void globalSetUp() {
+        csvFileWriter = new CsvFileWriterImpl();
+    }
+
     @Test
-    public void createReportFileMethodTest_Ok() {
+    public void createReportFileMethodTest_Ok() throws IOException {
         List<String> expectedList = List.of("fruit,quantity",
                 "How are u doing",
                 "wass up","100%N....");
-        List<String> data = List.of("How are u doing","wass up","100%N....");
-        new CsvFileWriterImpl().createReportFile("src/test/testSource/reportTest.csv", data);
-        List<String> actualList = new CsvFileReaderImpl()
-                .getAcceptedFileAsList("src/test/testSource/reportTest.csv");
+        List<String> data = List.of("How are u doing",
+                "wass up",
+                "100%N....");
+        List<String> actualList = Files.readAllLines(Path.of("src/test/testSource/reportTest.csv"));
+        csvFileWriter.createReportFile("src/test/testSource/reportTest.csv", data);
         assertEquals(expectedList, actualList);
     }
 }

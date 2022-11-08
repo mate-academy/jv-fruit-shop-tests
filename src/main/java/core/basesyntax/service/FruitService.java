55d3b044e62e4353176711f90f2d0df3ec36e1ee
Sidir0m <romangolovatyi@yahoo.com>
2022-11-08 10:00:43 +0200

    Add solution from last task

---
+++ src/main/java/core/basesyntax/service/FruitService.java
@@ -0,0 +1,11 @@
+package core.basesyntax.service;
+
+import core.basesyntax.service.impl.FruitTransaction;
+
+import java.util.List;
+
+public interface FruitService {
+    void applyFruitTransactions(List<FruitTransaction> fruitTransaction);
+
+    void createResultFile(String report, String filePath);
+}

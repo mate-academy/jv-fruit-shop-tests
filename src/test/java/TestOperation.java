a44694446f157816fa402566be86785334191e7b
thebitler <yurchyknikita@gmail.com>
2022-11-08 14:15:26 +0100

    Added test for exciting operations input for Operation.getByValue() method

--- src/test/java/TestOperation.java
+++ src/test/java/TestOperation.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.fail;
+
 import fruitshop.model.Operation;
 import org.junit.Test;
 
@@ -5,7 +7,18 @@ public class TestOperation {
     private static final char UNEXCITING_OPERATION = 'f';
 
     @Test(expected = RuntimeException.class)
-    public void getInvalidOperation_notOk() {
+    public void getByValue_getInvalidOperation_notOk() {
         Operation.getByValue(UNEXCITING_OPERATION);
+    }
+
+    @Test
+    public void getByValue_getValidOperation_isOk() {
+        try {
+            for (char character : new char[]{ 'b', 's', 'p', 'r' }) {
+                Operation.getByValue(character);
+            }
+        } catch (Exception e) {
+            fail("For existing operations method shouldn't throw an error");
+        }
     }
 }

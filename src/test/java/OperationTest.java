e1d51cff0e01621948121625620c3c7e0722c1d1
thebitler <yurchyknikita@gmail.com>
2022-11-08 18:21:47 +0100

    removed try/catch - fail

--- src/test/java/OperationTest.java
+++ src/test/java/OperationTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import fruitshop.model.Operation;
 import org.junit.Test;
@@ -14,25 +13,21 @@ public class OperationTest {
 
     @Test
     public void getByValue_getValidOperation_isOk() {
-        try {
-            Operation actualOperationByValueBalance = Operation.getByValue('b');
-            assertEquals("Expected to return corresponding operation",
-                    actualOperationByValueBalance, Operation.BALANCE
-            );
-            Operation actualOperationByValueSupply = Operation.getByValue('s');
-            assertEquals("Expected to return corresponding operation",
-                    actualOperationByValueSupply, Operation.SUPPLY
-            );
-            Operation actualOperationByValuePurchase = Operation.getByValue('p');
-            assertEquals("Expected to return corresponding operation",
-                    actualOperationByValuePurchase, Operation.PURCHASE
-            );
-            Operation actualOperationByValueReturn = Operation.getByValue('r');
-            assertEquals("Expected to return corresponding operation",
-                    actualOperationByValueReturn, Operation.RETURN
-            );
-        } catch (Exception e) {
-            fail("For existing operations method shouldn't throw an error");
-        }
+        Operation actualOperationByValueBalance = Operation.getByValue('b');
+        assertEquals("Expected to return corresponding operation",
+                actualOperationByValueBalance, Operation.BALANCE
+        );
+        Operation actualOperationByValueSupply = Operation.getByValue('s');
+        assertEquals("Expected to return corresponding operation",
+                actualOperationByValueSupply, Operation.SUPPLY
+        );
+        Operation actualOperationByValuePurchase = Operation.getByValue('p');
+        assertEquals("Expected to return corresponding operation",
+                actualOperationByValuePurchase, Operation.PURCHASE
+        );
+        Operation actualOperationByValueReturn = Operation.getByValue('r');
+        assertEquals("Expected to return corresponding operation",
+                actualOperationByValueReturn, Operation.RETURN
+        );
     }
 }

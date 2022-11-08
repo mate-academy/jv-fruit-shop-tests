f7b6367a2b9bcd0bb5a5e14fcfe226a1e5fcc2ba
thebitler <yurchyknikita@gmail.com>
2022-11-08 18:13:50 +0100

    Removed negative

--- src/test/java/TransactionsCalculatorServiceTest.java
+++ src/test/java/TransactionsCalculatorServiceTest.java
@@ -40,25 +40,20 @@ public class TransactionsCalculatorServiceTest {
         FruitShopStorage.fruitTransactions.addAll(List.of(
                 new FruitTransaction(Operation.getByValue('b'), "banana", 3682),
                 new FruitTransaction(Operation.getByValue('b'), "apple", 100),
-                new FruitTransaction(Operation.getByValue('b'), "pineapple", 1427),
                 new FruitTransaction(Operation.getByValue('b'), "apple", 10040),
                 new FruitTransaction(Operation.getByValue('b'), "peach", 13),
                 new FruitTransaction(Operation.getByValue('s'), "banana", 100),
                 new FruitTransaction(Operation.getByValue('s'), "peach", 145),
-                new FruitTransaction(Operation.getByValue('s'), "pineapple", 3466),
                 new FruitTransaction(Operation.getByValue('r'), "peach", 42),
                 new FruitTransaction(Operation.getByValue('s'), "banana", 50),
                 new FruitTransaction(Operation.getByValue('p'), "peach", 31),
                 new FruitTransaction(Operation.getByValue('p'), "peach", 169),
-                new FruitTransaction(Operation.getByValue('r'), "pineapple", 123),
-                new FruitTransaction(Operation.getByValue('p'), "apple", 20),
-                new FruitTransaction(Operation.getByValue('p'), "pineapple", 657575)
+                new FruitTransaction(Operation.getByValue('p'), "apple", 20)
         ));
         List<FruitReport> actual = transactionsCalculator.calculate();
         List<FruitReport> expected = List.of(
                 new FruitReport("banana",3832),
                 new FruitReport("apple",10020),
-                new FruitReport("pineapple",-652559),
                 new FruitReport("peach",0)
         );
         assertEquals("Expected sizes to be equal", expected.size(), actual.size());

31585f8226acab6abaa42531391ef5a2826bfbbb
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-11-08 12:05:09 +0200

    Removed Test assertTrue(file.exists());

--- src/test/java/myfirstproject/service/impl/WriteFileImplTest.java
+++ src/test/java/myfirstproject/service/impl/WriteFileImplTest.java
@@ -1,8 +1,5 @@
 package myfirstproject.service.impl;
 
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -28,24 +25,22 @@ public class WriteFileImplTest {
     private static final String PATH_TO_RESULT = "src/test/resources/reportFile.csv";
     private static final String WRONG_PATH = "src/resources/reportFile.csv";
     private static WriteFile writer;
-    private static File file;
 
     @BeforeClass
     public static void before() {
-        file = new File(PATH_TO_NEW_FILE);
         final FruitDao fruitDao = new FruitDaoImpl();
         final ReadFile reader = new ReadFileImpl();
         final SeparationOfOperations separation = new SeparationOfOperations();
         final List<String[]> temporalList = new ArrayList<>();
-        writer = new WriteFileImpl();
         Map<String, OperationHandler> operation = new HashMap<>();
         operation.put(Operation.BALANCE.getOperation(), new BalanceOperation());
         operation.put(Operation.SUPPLY.getOperation(), new SupplyOperation());
         operation.put(Operation.PURCHASE.getOperation(), new PurchaseOperation());
         operation.put(Operation.RETURN.getOperation(), new ReturnOperation());
+        writer = new WriteFileImpl();
+        writer.writeToFile(PATH_TO_RESULT, fruitDao.getAll());
         reader.readFile(temporalList, PATH_TO_NEW_FILE);
         separation.toDoEachOperation(fruitDao, temporalList, operation);
-        writer.writeToFile(PATH_TO_RESULT, fruitDao.getAll());
         WriteFile writeFile = new WriteFileImpl();
         Map<Fruit, Integer> mapToWrite = new HashMap<>();
         Fruit fruit = new Fruit("banana");
@@ -57,10 +52,5 @@ public class WriteFileImplTest {
     @Test(expected = RuntimeException.class)
     public void readWrongFile_not_Ok() {
         writer.writeToFile(WRONG_PATH, Collections.EMPTY_MAP);
-    }
-
-    @Test
-    public void isCreatedFileExist_Ok() {
-        assertTrue(file.exists());
     }
 }

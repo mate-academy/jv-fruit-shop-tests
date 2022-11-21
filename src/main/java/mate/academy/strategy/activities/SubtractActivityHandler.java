c90b02acca6d2704ec987e664ec72e1fda95bc3f
Katya Humeniuk <kapelka.boo@gmail.com>
2022-11-21 16:58:28 +0200

    Fixed after review.

--- src/main/java/mate/academy/strategy/activities/SubtractActivityHandler.java
+++ src/main/java/mate/academy/strategy/activities/SubtractActivityHandler.java
@@ -8,8 +8,13 @@ public class SubtractActivityHandler implements ActivityHandler {
     @Override
     public void handle(FruitTransaction transaction) {
         if (fruits.containsKey(transaction.getFruit())) {
-            fruits.put(transaction.getFruit(),
-                    fruits.get(transaction.getFruit()) - transaction.getQuantity());
+            if (fruits.get(transaction.getFruit()) - transaction.getQuantity() >= 0) {
+                fruits.put(transaction.getFruit(),
+                        fruits.get(transaction.getFruit()) - transaction.getQuantity());
+            } else {
+                throw new RuntimeException("Can`t subtract fruits, there is not enough "
+                        + transaction.getFruit());
+            }
         } else {
             throw new RuntimeException("Can`t subtract non-existent" + transaction.getFruit());
         }
--- src/test/java/mate/academy/service/TransactionServiceTest.java
+++ src/test/java/mate/academy/service/TransactionServiceTest.java
@@ -1,5 +1,8 @@
 package mate.academy.service;
 
+import static mate.academy.db.Storage.fruits;
+import static org.junit.Assert.fail;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,6 +42,75 @@ public class TransactionServiceTest {
     public void tearDown() {
         fruitTransactionList.clear();
         expectedResult.clear();
+        fruits.clear();
+    }
+
+    @Test
+    public void testAddActivityHandler_ok() {
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 20));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "apple", 100));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.SUPPLY, "banana", 100));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 13));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.RETURN, "apple", 10));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.SUPPLY, "banana", 50));
+        Map<String, Integer> actualResult = transactionService.processedData(fruitTransactionList);
+        expectedResult.put("banana", 157);
+        expectedResult.put("apple",110);
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void testSubtractActivityHandler_ok() {
+        expectedResult.put("banana", 100);
+        expectedResult.put("apple", 100);
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "apple", 120));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 125));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "apple", 20));
+        fruitTransactionList
+                .add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 25));
+        Map<String, Integer> actualResult = transactionService.processedData(fruitTransactionList);
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void testSubtractActivityHandlerNonExistedFruits_notOk() {
+        try {
+            fruitTransactionList
+                    .add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "apple", 20));
+            fruitTransactionList
+                    .add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 25));
+            transactionService.processedData(fruitTransactionList);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail("You must throw exception if trying subtract non-existed fruits.");
+    }
+
+    @Test
+    public void tryToSubtractWhenNotEnoughFruits_notOk() {
+        try {
+            fruitTransactionList
+                    .add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 20));
+            fruitTransactionList
+                    .add(new FruitTransaction(FruitTransaction.Operation.BALANCE, "apple", 10));
+            fruitTransactionList
+                    .add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "apple", 20));
+            fruitTransactionList
+                    .add(new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 25));
+            transactionService.processedData(fruitTransactionList);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail("You must throw exception if there is not enough fruits.");
     }
 
     @Test

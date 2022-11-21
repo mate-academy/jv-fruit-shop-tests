e323f3194768dc235c5095beac3e0cc8f2fc45fc
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-21 13:22:58 +0200

    fixes reportService

--- src/test/java/core/basesyntax/service/ServiceReportImplTest.java
+++ src/test/java/core/basesyntax/service/ServiceReportImplTest.java
@@ -3,16 +3,21 @@ package core.basesyntax.service;
 import static core.basesyntax.db.Storage.storage;
 import static org.junit.Assert.assertEquals;
 
+
+import core.basesyntax.dao.FileWriterImpl;
 import org.junit.Test;
 
 public class ServiceReportImplTest {
+    private static final String PATH_OUTPUT = "src/test/resources/output.csv";
     private static final String expectedString = "Malala,1234567890";
     private static final ServiceReportImpl serviceReport = new ServiceReportImpl();
 
     @Test
     public void checkMakingReport_OK() {
         storage.put("Malala", 1234567890);
+        String string = serviceReport.makeReport();
         assertEquals(expectedString, serviceReport.makeReport());
+        new FileWriterImpl().write(PATH_OUTPUT, string);
         storage.clear();
     }
 }

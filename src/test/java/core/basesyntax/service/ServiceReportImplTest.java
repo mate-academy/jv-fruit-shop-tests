c9d10518f7ff58d76bd879c643731d11eb2dad86
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-21 12:43:57 +0200

    added report test

---
+++ src/test/java/core/basesyntax/service/ServiceReportImplTest.java
@@ -0,0 +1,18 @@
+package core.basesyntax.service;
+
+import static core.basesyntax.db.Storage.storage;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ServiceReportImplTest {
+    private static final String expectedString = "alala,1234567890";
+
+    private static final ServiceReportImpl serviceReport = new ServiceReportImpl();
+    @Test
+    public void checkMakingReport_OK() {
+        storage.put("Malala", 1234567890);
+        assertEquals(expectedString, serviceReport.makeReport());
+        storage.clear();
+    }
+}

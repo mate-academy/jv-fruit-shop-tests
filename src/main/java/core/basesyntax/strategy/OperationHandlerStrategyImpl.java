23c5738b42284e1ca788b2adbc1e44c1b336eb41
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-21 14:02:26 +0200

    added strategy test

--- src/main/java/core/basesyntax/strategy/OperationHandlerStrategyImpl.java
+++ src/main/java/core/basesyntax/strategy/OperationHandlerStrategyImpl.java
@@ -4,10 +4,10 @@ import core.basesyntax.service.model.FruitTransaction;
 import core.basesyntax.service.operations.OperationHandler;
 import java.util.Map;
 
-public class OperatioHandlerStrategyImpl implements OperationHandlerStrategy {
+public class OperationHandlerStrategyImpl implements OperationHandlerStrategy {
     private final Map<FruitTransaction.TypeOperation, OperationHandler> listOperations;
 
-    public OperatioHandlerStrategyImpl(Map<FruitTransaction.TypeOperation,
+    public OperationHandlerStrategyImpl(Map<FruitTransaction.TypeOperation,
                                        OperationHandler> listOperations) {
         this.listOperations = listOperations;
     }
---
+++ src/test/java/core/basesyntax/strategy/OperationHandlerStrategyImplTest.java
@@ -0,0 +1,51 @@
+package core.basesyntax.strategy;
+
+import static org.junit.Assert.assertEquals;
+
+import core.basesyntax.service.model.FruitTransaction;
+import core.basesyntax.service.operations.BalanceOperationHandler;
+import core.basesyntax.service.operations.OperationHandler;
+import core.basesyntax.service.operations.PurchaseOperationHandler;
+import core.basesyntax.service.operations.ReturnOperationHandler;
+import core.basesyntax.service.operations.SupplyOperationHandler;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+public class OperationHandlerStrategyImplTest {
+    private OperationHandler operationHandler;
+    private final OperationHandlerStrategyImpl operationHandlerStrategy;
+
+    public OperationHandlerStrategyImplTest() {
+        Map<FruitTransaction.TypeOperation, OperationHandler> listOperations = new HashMap<>();
+        listOperations.put(FruitTransaction.TypeOperation.BALANCE, new BalanceOperationHandler());
+        listOperations.put(FruitTransaction.TypeOperation.SUPPLY, new SupplyOperationHandler());
+        listOperations.put(FruitTransaction.TypeOperation.PURCHASE, new PurchaseOperationHandler());
+        listOperations.put(FruitTransaction.TypeOperation.RETURN, new ReturnOperationHandler());
+        operationHandlerStrategy = new OperationHandlerStrategyImpl(listOperations);
+    }
+
+    @Test
+    public void checkReturnBalanceOperationClass_OK() {
+        operationHandler = operationHandlerStrategy.chooseOperation(FruitTransaction.TypeOperation.BALANCE);
+        assertEquals(operationHandler.getClass(), BalanceOperationHandler.class);
+    }
+
+    @Test
+    public void checkReturnSupplyOperationClass_OK() {
+        operationHandler = operationHandlerStrategy.chooseOperation(FruitTransaction.TypeOperation.SUPPLY);
+        assertEquals(operationHandler.getClass(), SupplyOperationHandler.class);
+    }
+
+    @Test
+    public void checkReturnPurchaseOperationClass_OK() {
+        operationHandler = operationHandlerStrategy.chooseOperation(FruitTransaction.TypeOperation.SUPPLY);
+        assertEquals(operationHandler.getClass(), PurchaseOperationHandler.class);
+    }
+
+    @Test
+    public void checkReturnReturnOperationClass_OK() {
+        operationHandler = operationHandlerStrategy.chooseOperation(FruitTransaction.TypeOperation.SUPPLY);
+        assertEquals(operationHandler.getClass(), ReturnOperationHandler.class);
+    }
+}

fe3f0418803c642de1888d06840982d844d9144c
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-12-25 22:20:38 +0000

    Added excluded of Main to pom.xml.

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Yo pi ca yey");
+    }
+}

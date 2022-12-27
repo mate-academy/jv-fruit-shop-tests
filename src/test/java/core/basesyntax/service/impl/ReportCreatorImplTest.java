4c2436eccc7f856434cda212876b6d593bc8ffc4
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-12-27 17:13:20 +0000

    Added ReportCreatorImplTest.

--- src/test/java/core/basesyntax/service/impl/ReportCreatorImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportCreatorImplTest.java
@@ -1,5 +1,37 @@
 package core.basesyntax.service.impl;
 
-public class ReportCreatorImplTest {
+import static org.junit.Assert.assertEquals;
 
+import core.basesyntax.db.Storage;
+import core.basesyntax.service.ReportCreator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReportCreatorImplTest {
+    private static final String TEST_HEADER = "fruit,quantity\n";
+    private static final String TEST_FRUIT_FIRST = "apple";
+    private static final String TEST_FRUIT_SECOND = "banana";
+    private static final int TEST_BALANCE_FIRST = 50;
+    private static final int TEST_BALANCE_SECOND = 100;
+    private ReportCreator reportCreator;
+
+    @Before
+    public void setUp() {
+        reportCreator = new ReportCreatorImpl();
+    }
+
+    @After
+    public void tearDown() {
+        Storage.fruits.clear();
+    }
+
+    @Test
+    public void createReport_Work_Ok() {
+        Storage.fruits.put(TEST_FRUIT_FIRST, TEST_BALANCE_FIRST);
+        Storage.fruits.put(TEST_FRUIT_SECOND, TEST_BALANCE_SECOND);
+        String actual = reportCreator.createReport();
+        String expected = TEST_HEADER + "banana,100\n" + "apple,50";
+        assertEquals(expected, actual);
+    }
 }

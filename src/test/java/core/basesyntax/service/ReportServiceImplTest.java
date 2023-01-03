27a20c2aa2d8ffb8e744834bf97a2e6601f1c96f
Roman Usyk <roman.usik.93@ukr.net>
2023-01-03 11:17:19 +0200

    some mistakes were corrected

--- src/test/java/core/basesyntax/service/ReportServiceImplTest.java
+++ src/test/java/core/basesyntax/service/ReportServiceImplTest.java
@@ -20,9 +20,9 @@ public class ReportServiceImplTest {
         String secondFruit = "watermelon";
         int quantitySecondFruit = 15;
 
-        String expected = TITLE_FOR_RESULT + NEW_LINE + firstFruit + PUNCTUATION_MARK +
-                quantityFirstFruit + NEW_LINE + secondFruit + PUNCTUATION_MARK +
-                quantitySecondFruit;
+        String expected = TITLE_FOR_RESULT + NEW_LINE + firstFruit + PUNCTUATION_MARK
+                + quantityFirstFruit + NEW_LINE + secondFruit + PUNCTUATION_MARK
+                + quantitySecondFruit;
 
         Map<String, Integer> reportMap = new HashMap<>();
         reportMap.put(firstFruit, quantityFirstFruit);

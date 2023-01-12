189deb923c3d09a2e959bfe4440391e2330cec3d
Uka0001 <ukraine0001@gmail.com>
2023-01-12 08:50:16 +0200

    write report tests added

---
+++ src/test/java/core/basesyntax/strategy/OperationCalculatorStrategyTest.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OperationCalculatorStrategyTest {
+
+    @Test
+    public void getCountStrategy() {
+    }
+}

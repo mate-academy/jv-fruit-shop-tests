ad411a725d0bb8c1625470bb56833f1ebfda849a
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-01-14 21:33:07 +0100

    just commit

--- src/test/java/core/basesyntax/service/impl/ReadFileServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReadFileServiceImplTest.java
@@ -1,7 +1,24 @@
 package core.basesyntax.service.impl;
 
-import junit.framework.TestCase;
+import core.basesyntax.service.ReadFileService;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class ReadFileServiceImplTest extends TestCase {
+import java.nio.file.Path;
+
+public class ReadFileServiceImplTest {
+    ReadFileService readFileService = new ReadFileServiceImpl();
+
+    @Test
+    public void readFromFile_validPath_ok() {
+        String expected = "Just some string";
+        String actual = readFileService.readFromFile(Path.of("src/test/resources/test_data.csv"));
+        Assert.assertEquals("Strings must be equals", expected, actual);
+    }
+
+    @Test
+    public void readFromFile_invalidPath_notOk() {
+
+    }
 
 }

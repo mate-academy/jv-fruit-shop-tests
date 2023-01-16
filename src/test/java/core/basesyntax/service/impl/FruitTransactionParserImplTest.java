964100d0ceb4030e35df0e015a25c5b4f1e96f47
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-01-16 14:07:39 +0100

    before clean verify

--- src/test/java/core/basesyntax/service/impl/FruitTransactionParserImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FruitTransactionParserImplTest.java
@@ -1,4 +1,42 @@
 package core.basesyntax.service.impl;
 
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.FruitTransactionParser;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FruitTransactionParserImplTest {
+    private static final String HEADER = "fruit,quantity";
+    private static FruitTransactionParser fruitTransactionParser;
+
+    @BeforeClass
+    public static void setUp() {
+        fruitTransactionParser = new FruitTransactionParserImpl();
+    }
+
+    @Test
+    public void parseTransactions_validData_ok() {
+        List<FruitTransaction> expected = new ArrayList<>();
+        FruitTransaction firstTransaction = new FruitTransaction();
+        firstTransaction.setOperation(FruitTransaction.Operation.BALANCE);
+        firstTransaction.setFruit("apple");
+        firstTransaction.setQuantity(10);
+        FruitTransaction secondTransaction = new FruitTransaction();
+        secondTransaction.setOperation(FruitTransaction.Operation.RETURN);
+        secondTransaction.setFruit("apple");
+        secondTransaction.setQuantity(10);
+        expected.add(firstTransaction);
+        expected.add(secondTransaction);
+        String input = HEADER
+                + System.lineSeparator()
+                + "b,apple,10"
+                + System.lineSeparator()
+                + "r,apple,10";
+        List<FruitTransaction> actual = fruitTransactionParser.parseTransactions(input);
+        Assert.assertEquals(expected, actual);
+    }
 }
--- src/test/java/core/basesyntax/service/impl/TransactionProcessorImplTest.java
+++ src/test/java/core/basesyntax/service/impl/TransactionProcessorImplTest.java
@@ -4,16 +4,21 @@ import core.basesyntax.db.FruitDao;
 import core.basesyntax.model.FruitTransaction;
 import java.util.ArrayList;
 import java.util.List;
+
+import core.basesyntax.service.TransactionProcessor;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TransactionProcessorImplTest {
     private static List<FruitTransaction> transactions;
+    private static TransactionProcessor transactionProcessor;
     private static FruitTransaction fruitTransaction;
 
     @BeforeClass
     public static void setUp() {
+        transactionProcessor = new TransactionProcessorImpl();
         transactions = new ArrayList<>();
         fruitTransaction = new FruitTransaction();
     }
@@ -21,14 +26,18 @@ public class TransactionProcessorImplTest {
     @Test
     public void process_validData_ok() {
         fruitTransaction.setOperation(FruitTransaction.Operation.BALANCE);
-        fruitTransaction.setFruit("marakuja");
+        fruitTransaction.setFruit("dragon fruit");
         fruitTransaction.setQuantity(100);
         transactions.add(fruitTransaction);
-
+        transactionProcessor.process(transactions);
+        Integer expected = 100;
+        Integer actual = FruitDao.getQuantity("dragon fruit");
+        Assert.assertEquals(expected, actual);
     }
 
     @AfterClass
     public static void tearDown() {
         FruitDao.storage.clear();
+        transactions.clear();
     }
 }

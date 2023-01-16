20ef3cb9bf8fd24c1c29dbfb408e45f686cfc76e
Mykola Radzivon <kolian201497@gmail.com>
2023-01-17 01:27:43 +0200

    attempt to fixed maven

--- src/test/java/core/basesyntax/FruitTransactionParserTest.java
+++ src/test/java/core/basesyntax/FruitTransactionParserTest.java
@@ -13,21 +13,18 @@ public class FruitTransactionParserTest {
             new FruitTransactionParserImpl();
 
     @Test
-    public void toTransaction_ok() {
+    public void toTransaction_validCase_ok() {
         String data = "type,fruit,quantity" + System.lineSeparator()
                 + "b,banana,200\n" + System.lineSeparator()
                 + "b,apple,100\n" + System.lineSeparator()
                 + "s,banana,100\n" + System.lineSeparator()
-                + "p,banana,13\n" + System.lineSeparator()
-                + "r,apple,10 \n";
-
+                + "p,banana,13\n";
         List<FruitTransaction> actual = FRUIT_TRANSACTION_PARSER.toTransaction(data);
         List<FruitTransaction> expected = List.of(
                 new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 200),
                 new FruitTransaction(FruitTransaction.Operation.BALANCE, "apple", 100),
                 new FruitTransaction(FruitTransaction.Operation.SUPPLY, "banana", 100),
-                new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 13),
-                new FruitTransaction(FruitTransaction.Operation.RETURN, "apple", 10)
+                new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 13)
         );
         assertEquals("Invalid transaction", expected, actual);
     }

3273278f5b0776774397486260bc3856eaad75b5
Дмитро <dima.smyk99.19@gmail.com>
2023-01-17 13:00:37 +0200

    refactor

--- src/test/java/core/basesyntax/service/ReportBuilderServiceTest.java
+++ src/test/java/core/basesyntax/service/ReportBuilderServiceTest.java
@@ -3,16 +3,21 @@ package core.basesyntax.service;
 import static org.junit.Assert.assertEquals;
 
 import core.basesyntax.db.StorageOfData;
+import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.impl.ReportBuilderServiceImpl;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ReportBuilderServiceTest {
     private static ReportBuilderService reportBuilder;
+    private static FruitTransaction fruit;
+
 
     @BeforeClass
     public static void setUp() {
         reportBuilder = new ReportBuilderServiceImpl();
+        fruit = new FruitTransaction();
+        fruit.setFruit("name");
     }
 
     @Test
@@ -26,11 +31,12 @@ public class ReportBuilderServiceTest {
     public void create_trueBuild_ok() {
         StringBuilder builder = new StringBuilder("fruit,quantity");
         String expected = builder.append(System.lineSeparator())
-                .append("fruitName")
+                .append(fruit.getFruit())
                 .append(",")
                 .append(10)
                 .toString();
-        StorageOfData.fruitsData.put("fruitName", 10);
+        System.out.println(expected);
+        StorageOfData.fruitsData.put(fruit.getFruit(), 10);
         String actual = reportBuilder.buildReport();
         assertEquals(expected, actual);
 

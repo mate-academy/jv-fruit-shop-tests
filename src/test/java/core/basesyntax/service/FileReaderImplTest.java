99a025e012d6bacff8768e0ddc4beac44b0f1c52
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-02-10 17:52:59 +0200

    FileWriter tests added

--- src/test/java/core/basesyntax/service/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/service/FileReaderImplTest.java
@@ -1,11 +1,12 @@
 package core.basesyntax.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import java.util.List;
 import org.junit.Test;
 
 public class FileReaderImplTest {
+    FileWriter fileWriter = new FileWriterImpl();
     FileReader fileReader = new FileReaderImpl();
     private String filepath;
 
@@ -45,7 +46,19 @@ public class FileReaderImplTest {
     @Test
     public void validPathWriteToFile_Ok() {
         filepath = "src/main/resources/TestFile.csv";
+        fileWriter.writeToFile("one, two, three, four", filepath);
         List<String> actual = fileReader.readFromFile(filepath);
-        assertEquals(11, actual.size());
+        assertEquals(1, actual.size());
+    }
+
+    @Test
+    public void invalidFilePath_NotOk() {
+        filepath = "абв/іі/123";
+        try {
+            fileReader.readFromFile(filepath);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail("Runtime exception should be thrown in case of invalid filepath");
     }
 }

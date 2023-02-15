5c9c3ecc0852804f3ca8d6806c75a015e7807064
Watxesnavo <112902418+Watxesnavo@users.noreply.github.com>
2023-02-16 00:34:56 +0100

    1st try 15/23.

--- src/test/java/core/basesyntax/service/impl/ReportGeneratorServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportGeneratorServiceImplTest.java
@@ -15,35 +15,35 @@ public class ReportGeneratorServiceImplTest {
     @Before
     public void setUp() {
         transactionsList = new ArrayList<>();
-        FruitTransaction transaction = new FruitTransaction();
+        final FruitTransaction transaction = new FruitTransaction();
         transaction.setOperation(FruitTransaction.Operation.BALANCE);
         transaction.setFruit("banana");
         transaction.setQuantity(100);
         transactionsList.add(transaction);
-        FruitTransaction transaction1 = new FruitTransaction();
-        transaction.setOperation(FruitTransaction.Operation.SUPPLY);
-        transaction.setFruit("banana");
-        transaction.setQuantity(10);
+        final FruitTransaction transaction1 = new FruitTransaction();
+        transaction1.setOperation(FruitTransaction.Operation.SUPPLY);
+        transaction1.setFruit("banana");
+        transaction1.setQuantity(10);
         transactionsList.add(transaction1);
-        FruitTransaction transaction2 = new FruitTransaction();
-        transaction.setOperation(FruitTransaction.Operation.PURCHASE);
-        transaction.setFruit("banana");
-        transaction.setQuantity(7);
+        final FruitTransaction transaction2 = new FruitTransaction();
+        transaction2.setOperation(FruitTransaction.Operation.PURCHASE);
+        transaction2.setFruit("banana");
+        transaction2.setQuantity(7);
         transactionsList.add(transaction2);
-        FruitTransaction transaction3 = new FruitTransaction();
-        transaction.setOperation(FruitTransaction.Operation.BALANCE);
-        transaction.setFruit("apple");
-        transaction.setQuantity(10);
+        final FruitTransaction transaction3 = new FruitTransaction();
+        transaction3.setOperation(FruitTransaction.Operation.BALANCE);
+        transaction3.setFruit("apple");
+        transaction3.setQuantity(10);
         transactionsList.add(transaction3);
-        FruitTransaction transaction4 = new FruitTransaction();
-        transaction.setOperation(FruitTransaction.Operation.RETURN);
-        transaction.setFruit("apple");
-        transaction.setQuantity(2);
+        final FruitTransaction transaction4 = new FruitTransaction();
+        transaction4.setOperation(FruitTransaction.Operation.RETURN);
+        transaction4.setFruit("apple");
+        transaction4.setQuantity(2);
         transactionsList.add(transaction4);
-        FruitTransaction transaction5 = new FruitTransaction();
-        transaction.setOperation(FruitTransaction.Operation.SUPPLY);
-        transaction.setFruit("apple");
-        transaction.setQuantity(100);
+        final FruitTransaction transaction5 = new FruitTransaction();
+        transaction5.setOperation(FruitTransaction.Operation.SUPPLY);
+        transaction5.setFruit("apple");
+        transaction5.setQuantity(100);
         transactionsList.add(transaction5);
     }
 

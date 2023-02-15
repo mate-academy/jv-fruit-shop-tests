1fd58d9bc740c0663116cc55e428db8432174af3
Watxesnavo <112902418+Watxesnavo@users.noreply.github.com>
2023-02-16 00:31:38 +0100

    1st try 15/23.

--- src/test/java/core/basesyntax/service/impl/ReportGeneratorServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportGeneratorServiceImplTest.java
@@ -19,31 +19,31 @@ public class ReportGeneratorServiceImplTest {
         transaction.setOperation(FruitTransaction.Operation.BALANCE);
         transaction.setFruit("banana");
         transaction.setQuantity(100);
+        transactionsList.add(transaction);
         FruitTransaction transaction1 = new FruitTransaction();
         transaction.setOperation(FruitTransaction.Operation.SUPPLY);
         transaction.setFruit("banana");
         transaction.setQuantity(10);
+        transactionsList.add(transaction1);
         FruitTransaction transaction2 = new FruitTransaction();
         transaction.setOperation(FruitTransaction.Operation.PURCHASE);
         transaction.setFruit("banana");
         transaction.setQuantity(7);
+        transactionsList.add(transaction2);
         FruitTransaction transaction3 = new FruitTransaction();
         transaction.setOperation(FruitTransaction.Operation.BALANCE);
         transaction.setFruit("apple");
         transaction.setQuantity(10);
+        transactionsList.add(transaction3);
         FruitTransaction transaction4 = new FruitTransaction();
         transaction.setOperation(FruitTransaction.Operation.RETURN);
         transaction.setFruit("apple");
         transaction.setQuantity(2);
+        transactionsList.add(transaction4);
         FruitTransaction transaction5 = new FruitTransaction();
         transaction.setOperation(FruitTransaction.Operation.SUPPLY);
         transaction.setFruit("apple");
         transaction.setQuantity(100);
-        transactionsList.add(transaction);
-        transactionsList.add(transaction1);
-        transactionsList.add(transaction2);
-        transactionsList.add(transaction3);
-        transactionsList.add(transaction4);
         transactionsList.add(transaction5);
     }
 

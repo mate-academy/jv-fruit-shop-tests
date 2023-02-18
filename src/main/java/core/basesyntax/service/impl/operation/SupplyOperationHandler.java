2e5291dad1a5f0317cfb133d3d799e0f80d87f93
oleksandr-petrunovskyi <102808605+oleksandr-petrunovskyi@users.noreply.github.com>
2023-02-18 17:59:37 +0200

    SupplyOperationHandler refactored

--- src/main/java/core/basesyntax/service/impl/operation/SupplyOperationHandler.java
+++ src/main/java/core/basesyntax/service/impl/operation/SupplyOperationHandler.java
@@ -13,9 +13,12 @@ public class SupplyOperationHandler implements OperationHandler {
 
     @Override
     public void handle(FruitTransaction transaction) {
-        int quantityFromDb = warehouseDao.getQuantity(transaction.getFruit());
         String fruit = transaction.getFruit();
+        if (transaction.getQuantity() < 0) {
+            throw new RuntimeException("Fruit quantity can't be negative");
+        }
         if (warehouseDao.isPresent(fruit)) {
+            int quantityFromDb = warehouseDao.getQuantity(transaction.getFruit());
             warehouseDao.setQuantity(fruit, quantityFromDb + transaction.getQuantity());
         } else {
             warehouseDao.setQuantity(fruit, transaction.getQuantity());

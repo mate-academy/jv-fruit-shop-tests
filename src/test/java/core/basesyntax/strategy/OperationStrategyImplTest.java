aa0d78c122de62690eb1217cb53891d74f7fc272
Watxesnavo <112902418+Watxesnavo@users.noreply.github.com>
2023-02-19 19:49:34 +0100

    2nd try 19/23.

---
+++ src/test/java/core/basesyntax/strategy/OperationStrategyImplTest.java
@@ -0,0 +1,35 @@
+package core.basesyntax.strategy;
+
+import core.basesyntax.model.FruitTransaction;
+import org.junit.Assert;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OperationStrategyImplTest {
+    private Map<FruitTransaction.Operation, OperationHandler> operationHandlersMap;
+    private OperationStrategy operationStrategy;
+
+    @Before
+    public void setUp() {
+        operationHandlersMap = new HashMap<>();
+        operationHandlersMap.put(FruitTransaction.Operation.PURCHASE,
+                new PurchaseOperationHandler());
+        operationHandlersMap.put(FruitTransaction.Operation.RETURN,
+                new ReturnOperationHandler());
+        operationHandlersMap.put(FruitTransaction.Operation.SUPPLY,
+                new SupplyOperationHandler());
+        operationHandlersMap.put(FruitTransaction.Operation.BALANCE,
+                new BalanceOperationHandler());
+        operationStrategy = new OperationStrategyImpl(operationHandlersMap);
+    }
+
+    @Test
+    public void getMethod_Ok() {
+        OperationHandler actual = operationStrategy.get(FruitTransaction.Operation.BALANCE);
+        OperationHandler expected = new BalanceOperationHandler();
+        Assert.assertEquals(expected instanceof BalanceOperationHandler,
+                actual instanceof BalanceOperationHandler);
+    }
+}

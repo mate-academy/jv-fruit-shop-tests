6ec933efe7f3db88abb2bb877ec0ca01260d15da
Mr.GvvD <mr.gvvd@gmail.com>
2023-02-22 22:29:49 +0200

    maven issues were fixed.

--- src/test/java/core/basesyntax/service/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/service/FileReaderImplTest.java
@@ -1,9 +1,6 @@
 package core.basesyntax.service;
 
 import core.basesyntax.service.impl.FileReaderImpl;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
@@ -11,7 +8,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FileReaderImplTest {
-    private static final String FILE_EXIST_PATH = "src/test/java/core/basesyntax/resources/input.csv";
+    private static final String FILE_EXIST_PATH =
+            "src/test/java/core/basesyntax/resources/input.csv";
     private static final String FILE_NOT_EXIST_PATH = "directory/file.csv";
     private static FileReader fileReader;
 
--- src/test/java/core/basesyntax/service/TransactionServiceImplTest.java
+++ src/test/java/core/basesyntax/service/TransactionServiceImplTest.java
@@ -15,10 +15,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 

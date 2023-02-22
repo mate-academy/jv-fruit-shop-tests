ecfed4841f9c4a50d6f13a791aef0fb8508d6a1f
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-02-22 12:32:48 +0200

    added OperationTest

---
+++ src/test/java/core/basesyntax/model/OperationTest.java
@@ -0,0 +1,31 @@
+package core.basesyntax.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class OperationTest {
+
+    @Test(expected = NoSuchElementException.class)
+    public void getByCode_nullValue_notOk() {
+        Operation.getByCode(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getByCode_emptyValue_notOk() {
+        Operation.getByCode("");
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getByCode_invalidValue_notOk() {
+        Operation.getByCode("bad");
+    }
+
+    @Test
+    public void getByCode_validValue_ok() {
+        Operation expected = Operation.BALANCE;
+        Operation actual = Operation.getByCode("b");
+        Assert.assertEquals(expected, actual);
+    }
+}

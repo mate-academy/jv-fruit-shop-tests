875c8ed443a85a2d3816477e24f81403900f241a
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-02-22 12:44:25 +0200

    added tests for CsvFileReaderServiceImpl.java

--- src/main/java/core/basesyntax/service/impl/CsvFileReaderServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileReaderServiceImpl.java
@@ -7,15 +7,12 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class CsvFileReaderServiceImpl implements FileReaderService {
-    private static final int TITLE_LINE_INDEX = 0;
-
     @Override
-    public List<String> readFromFile(String fromFile) {
-        File file = new File(fromFile);
+    public List<String> readFromFile(String fileName) {
+        File file = new File(fileName);
         List<String> transactions;
         try {
             transactions = Files.readAllLines(file.toPath());
-            transactions.remove(TITLE_LINE_INDEX);
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file " + file, e);
         }
---
+++ src/test/java/core/basesyntax/service/impl/CsvFileReaderServiceImplTest.java
@@ -0,0 +1,48 @@
+package core.basesyntax.service.impl;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvFileReaderServiceImplTest {
+    private static final String EMPTY_FILE_NAME = "";
+    private static final String MISSING_FILE_NAME = "src/test/resources/incorrect.csv";
+    private static final String CORRECT_FILE_NAME = "src/test/resources/testReadFile.csv";
+
+    private static CsvFileReaderServiceImpl readerService;
+
+    @BeforeClass
+    public static void beforeClass() {
+        readerService = new CsvFileReaderServiceImpl();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void readFrom_nullFileName_notOk() {
+        readerService.readFromFile(null);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void readFromFile_emptyFileName_notOk() {
+        readerService.readFromFile(EMPTY_FILE_NAME);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void readFromFile_missingFileName_notOk() {
+        readerService.readFromFile(MISSING_FILE_NAME);
+    }
+
+    @Test
+    public void readFromFile_successRead_ok() {
+        List<String> expected = new ArrayList<>();
+        expected.add("type,fruit,quantity");
+        expected.add("b,banana,20");
+        expected.add("b,apple,100");
+        expected.add("s,banana,100");
+        List<String> actual = readerService.readFromFile(CORRECT_FILE_NAME);
+        Assert.assertEquals("There are differences differences between expected and actual result",
+                expected, actual);
+    }
+}

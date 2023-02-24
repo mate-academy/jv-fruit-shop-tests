d0585e51b8c37f7b8f94df0cde1dee43efb06f7b
iskamele <iskamele@gmail.com>
2023-02-24 19:21:57 +0200

    Added mock for static Storage.

--- src/test/java/core/basesyntax/operation/PurchaseOperationTest.java
+++ src/test/java/core/basesyntax/operation/PurchaseOperationTest.java
@@ -2,18 +2,33 @@ package core.basesyntax.operation;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 import core.basesyntax.database.Storage;
 import core.basesyntax.model.FruitTransaction;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.MockedStatic;
 
 public class PurchaseOperationTest {
     private static OperationHandler operationHandler;
     private static final String FRUIT = "banana";
     private static final Integer FIRST_QUANTITY = 100;
     private static final Integer SECOND_QUANTITY = 30;
+    private static Map<String, Integer> fruitStorage;
+    private static MockedStatic<Storage> mockStorage;
+
+    @BeforeClass
+    public static void beforeClass() {
+        mockStorage = mockStatic(Storage.class);
+        fruitStorage = new HashMap<>();
+    }
 
     @Before
     public void setUp() {
@@ -21,21 +36,27 @@ public class PurchaseOperationTest {
     }
 
     @After
-    public void tearDown() {
-        Storage.getFruitStorage().clear();
+    public void storageClear() {
+        fruitStorage.clear();
+    }
+
+    @AfterClass
+    public static void closeMock() {
+        mockStorage.close();
     }
 
     @Test
     public void handleOperation_getPurchase_Ok() {
         //arrange
-        Storage.getFruitStorage().put(FRUIT, FIRST_QUANTITY);
+        fruitStorage.put(FRUIT, FIRST_QUANTITY);
         FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
                 FRUIT, SECOND_QUANTITY);
         Integer expected = FIRST_QUANTITY - SECOND_QUANTITY;
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
 
         //act
         operationHandler.handleOperation(transaction);
-        Integer actual = Storage.getFruitStorage().get(FRUIT);
+        Integer actual = fruitStorage.get(FRUIT);
 
         //assert
         assertEquals("PurchaseOperation should update a quantity in DB.",
@@ -48,20 +69,21 @@ public class PurchaseOperationTest {
         FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
                 FRUIT, SECOND_QUANTITY);
         Integer expected = SECOND_QUANTITY;
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
 
         //act
         operationHandler.handleOperation(transaction);
-        Integer actual = Storage.getFruitStorage().get(FRUIT);
+        Integer actual = fruitStorage.get(FRUIT);
 
         //assert
         assertEquals("PurchaseOperation should add new transaction in DB.",
                 expected, actual);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void handleOperation_purchaseMoreThanKeptInStorage_NotOk() {
         //arrange
-        Storage.getFruitStorage().put(FRUIT, SECOND_QUANTITY);
+        fruitStorage.put(FRUIT, SECOND_QUANTITY);
         FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
                 FRUIT, FIRST_QUANTITY);
 
--- src/test/java/core/basesyntax/operation/ReturnOperationTest.java
+++ src/test/java/core/basesyntax/operation/ReturnOperationTest.java
@@ -1,18 +1,33 @@
 package core.basesyntax.operation;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 import core.basesyntax.database.Storage;
 import core.basesyntax.model.FruitTransaction;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.MockedStatic;
 
 public class ReturnOperationTest {
     private static OperationHandler operationHandler;
     private static final String FRUIT = "banana";
     private static final Integer FIRST_QUANTITY = 100;
     private static final Integer SECOND_QUANTITY = 30;
+    private static Map<String, Integer> fruitStorage;
+    private static MockedStatic<Storage> mockStorage;
+
+    @BeforeClass
+    public static void beforeClass() {
+        mockStorage = mockStatic(Storage.class);
+        fruitStorage = new HashMap<>();
+    }
 
     @Before
     public void setUp() {
@@ -20,21 +35,27 @@ public class ReturnOperationTest {
     }
 
     @After
-    public void tearDown() {
-        Storage.getFruitStorage().clear();
+    public void storageClear() {
+        fruitStorage.clear();
+    }
+
+    @AfterClass
+    public static void closeMock() {
+        mockStorage.close();
     }
 
     @Test
     public void handleOperation_getReturn_Ok() {
         //arrange
-        Storage.getFruitStorage().put(FRUIT, FIRST_QUANTITY);
+        fruitStorage.put(FRUIT, FIRST_QUANTITY);
         FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
                 FRUIT, SECOND_QUANTITY);
         Integer expected = FIRST_QUANTITY + SECOND_QUANTITY;
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
 
         //act
         operationHandler.handleOperation(transaction);
-        Integer actual = Storage.getFruitStorage().get(FRUIT);
+        Integer actual = fruitStorage.get(FRUIT);
 
         //assert
         assertEquals("ReturnOperation should update a quantity in DB.",
@@ -47,10 +68,11 @@ public class ReturnOperationTest {
         FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
                 FRUIT, SECOND_QUANTITY);
         Integer expected = SECOND_QUANTITY;
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
 
         //act
         operationHandler.handleOperation(transaction);
-        Integer actual = Storage.getFruitStorage().get(FRUIT);
+        Integer actual = fruitStorage.get(FRUIT);
 
         //assert
         assertEquals("ReturnOperation should add new transaction in DB.",

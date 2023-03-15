93fcaba4b49f9beec6bc21cd709e104f4512d737
kromchk <kromchyk@yahoo.com>
2023-03-15 16:15:56 +0200

    realization of fruit-shop-tests, minor changes final

--- src/main/java/core/basesyntax/strategy/impl/ApplyPurchase.java
+++ src/main/java/core/basesyntax/strategy/impl/ApplyPurchase.java
@@ -9,7 +9,7 @@ public class ApplyPurchase implements UnaryOperation {
     @Override
     public void apply(FruitTransaction fruit) {
         if (fruit == null || !Storage.storage.containsKey(fruit.getFruit())
-                || Storage.storage.get(fruit.getFruit()).intValue() < 0
+                || fruit.getQuantity() < 0
                 || Storage.storage.get(fruit.getFruit()).intValue() < fruit.getQuantity()) {
             throw new FruitStoreException("Can't selling " + (fruit != null
                     ? fruit.getFruit() : "null")

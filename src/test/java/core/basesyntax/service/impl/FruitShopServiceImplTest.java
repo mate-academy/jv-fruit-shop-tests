22a2e77fa9a8a6989b2006b44dab1416d7dc76ae
Artem Grunin <artem.grunin.2006@gmail.com>
2023-03-16 19:27:43 +0200

    added FruitShopService Test

--- src/test/java/core/basesyntax/service/impl/FruitShopServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FruitShopServiceImplTest.java
@@ -29,6 +29,13 @@ public class FruitShopServiceImplTest {
         Storage.storage.clear();
     }
 
+    @Before
+    public void beforeEach() {
+        Storage.storage.put(APPLE, 10);
+        Storage.storage.put(BANANA, 20);
+        Storage.storage.put(ORANGE, 30);
+    }
+
     @After
     public void afterAll() {
         Storage.storage.clear();
@@ -36,21 +43,51 @@ public class FruitShopServiceImplTest {
 
     @Test
     public void test_ProcessData_Update_Storage_ok() {
-        Storage.storage.put(APPLE, 10);
-        Storage.storage.put(BANANA, 20);
-        Storage.storage.put(ORANGE, 30);
         List<FruitTransaction> transactions = Arrays.asList(
+                new FruitTransaction(FruitTransaction.Operation.BALANCE, ORANGE, 100),
                 new FruitTransaction(FruitTransaction.Operation.PURCHASE, APPLE, 5),
                 new FruitTransaction(FruitTransaction.Operation.RETURN, BANANA, 10),
-                new FruitTransaction(FruitTransaction.Operation.SUPPLY, PEAR, -1),
-                new FruitTransaction(FruitTransaction.Operation.BALANCE, ORANGE, 0)
+                new FruitTransaction(FruitTransaction.Operation.SUPPLY, PEAR, 15)
         );
         fruitShopService.processData(transactions);
         Map<String, Integer> expectedStorage = new HashMap<>();
         expectedStorage.put(APPLE, 5);
         expectedStorage.put(BANANA, 30);
+        expectedStorage.put(ORANGE, 130);
+        expectedStorage.put(PEAR, 15);
+        assertEquals(expectedStorage, Storage.storage);
+    }
+
+    @Test
+    public void test_ProcessData_Not_Update_Storage_ok() {
+        List<FruitTransaction> transactions = Arrays.asList(
+                new FruitTransaction(FruitTransaction.Operation.PURCHASE, APPLE, 0),
+                new FruitTransaction(FruitTransaction.Operation.RETURN, BANANA, 0),
+                new FruitTransaction(FruitTransaction.Operation.SUPPLY, PEAR, 0),
+                new FruitTransaction(FruitTransaction.Operation.BALANCE, ORANGE, 0)
+        );
+        fruitShopService.processData(transactions);
+        Map<String, Integer> expectedStorage = new HashMap<>();
+        expectedStorage.put(APPLE, 10);
+        expectedStorage.put(BANANA, 20);
         expectedStorage.put(ORANGE, 30);
         expectedStorage.put(PEAR, 0);
+        assertEquals(expectedStorage, Storage.storage);
+    }
+
+    @Test
+    public void test_Negative_ProcessData_Not_Update_Storage_ok() {
+        List<FruitTransaction> transactions = Arrays.asList(
+                new FruitTransaction(FruitTransaction.Operation.PURCHASE, APPLE, -1),
+                new FruitTransaction(FruitTransaction.Operation.RETURN, BANANA, -1),
+                new FruitTransaction(FruitTransaction.Operation.SUPPLY, PEAR, -1),
+                new FruitTransaction(FruitTransaction.Operation.BALANCE, ORANGE, -1)
+        );
+        fruitShopService.processData(transactions);
+        Map<String, Integer> expectedStorage = new HashMap<>();
+        expectedStorage.put(APPLE, 10);
+        expectedStorage.put(BANANA, 20);
+        expectedStorage.put(ORANGE, 30);
         assertEquals(expectedStorage, Storage.storage);
     }
 }

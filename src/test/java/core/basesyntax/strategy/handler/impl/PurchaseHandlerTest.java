49d13ee887df0a8d95fba09ab75560b715574767
Artem Grunin <artem.grunin.2006@gmail.com>
2023-03-16 14:56:06 +0200

    got not electricity commit

---
+++ src/test/java/core/basesyntax/strategy/handler/impl/PurchaseHandlerTest.java
@@ -0,0 +1,76 @@
+package core.basesyntax.strategy.handler.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.strategy.handler.OperationHandler;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PurchaseHandlerTest {
+
+    private static final String APPLE = "apple";
+    private static final int VALID_FRUIT_QUANTITY = 10;
+    private static final int ANOTHER_VALID_FRUIT_QUANTITY = 5;
+    private static final int MORE_THAN_VALID_FRUIT_QUANTITY = 11;
+    private static final String BANANA = "banana";
+    private static final int ZERO_FRUIT_QUANTITY = 0;
+    private static OperationHandler handler;
+
+    @BeforeClass
+    public static void beforeClass() {
+        handler = new PurchaseHandler();
+    }
+
+    @Before
+    public void setUp() {
+        Storage.storage.clear();
+        Storage.storage.put(APPLE, VALID_FRUIT_QUANTITY);
+        Storage.storage.put(BANANA, ANOTHER_VALID_FRUIT_QUANTITY);
+    }
+
+    @Test
+    public void test_Update_FruitStorage_ok() {
+        FruitTransaction transaction = new FruitTransaction(
+                FruitTransaction.Operation.PURCHASE,
+                APPLE,
+                ANOTHER_VALID_FRUIT_QUANTITY);
+        Integer beforeQuantity = Storage.storage.getOrDefault(APPLE, ZERO_FRUIT_QUANTITY);
+        handler.handle(transaction);
+        Integer actualQuantity = Storage.storage.getOrDefault(APPLE, ZERO_FRUIT_QUANTITY);
+        Integer expectedQuantity = beforeQuantity - ANOTHER_VALID_FRUIT_QUANTITY;
+        assertEquals(expectedQuantity, actualQuantity);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test_purchase_quantity_is_greater_than_current() {
+        FruitTransaction transaction = new FruitTransaction(
+                FruitTransaction.Operation.PURCHASE,
+                APPLE,
+                MORE_THAN_VALID_FRUIT_QUANTITY);
+        handler.handle(transaction);
+    }
+
+    @Test
+    public void testPurchaseHandlerShouldReduceFruitQuantityInStorage() {
+        FruitTransaction transaction = new FruitTransaction(
+                FruitTransaction.Operation.PURCHASE,
+                "apple",
+                5);
+        handler.handle(transaction);
+        Integer actualQuantity = Storage.storage.get("apple");
+        int expectedQuantity = 5;
+        assertEquals(expectedQuantity, (int) actualQuantity);
+
+        transaction = new FruitTransaction(
+                FruitTransaction.Operation.PURCHASE,
+                "apple",
+                5);
+        handler.handle(transaction);
+        actualQuantity = Storage.storage.get("apple");
+        expectedQuantity = 0;
+        assertEquals(expectedQuantity, (int) actualQuantity);
+    }
+}

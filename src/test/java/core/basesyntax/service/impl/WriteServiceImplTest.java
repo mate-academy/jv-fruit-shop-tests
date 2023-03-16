224a90f5c9c7fbe8f265061389d0602573852d55
KosMarch <kosmarchenko98@gmail.com>
2023-03-16 13:26:33 +0200

    added test with empty Map

--- src/test/java/core/basesyntax/service/impl/WriteServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/WriteServiceImplTest.java
@@ -3,6 +3,9 @@ package core.basesyntax.service.impl;
 import static org.junit.Assert.assertEquals;
 
 import core.basesyntax.service.WriteService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,15 +14,18 @@ import org.junit.Test;
 
 public class WriteServiceImplTest {
     private WriteService writeService;
+    private ReadServiceImpl readService;
+    private Map<String, Integer> testMap;
 
     @Before
     public void setUp() {
         writeService = new WriteServiceImpl();
+        readService = new ReadServiceImpl();
+        testMap = new HashMap<>();
     }
 
     @Test
     public void writeAndRead_toFile_Ok() {
-        Map<String, Integer> testMap = new HashMap<>();
         testMap.put("banana", 250);
         testMap.put("apple", 61);
         writeService.writeToFile(testMap, "src/test/java/resources/testToFile.csv");
@@ -27,16 +33,25 @@ public class WriteServiceImplTest {
         List<String> expected = List.of("fruit,quantity",
                 "banana,250",
                 "apple,61");
-        ReadServiceImpl readService = new ReadServiceImpl();
         assertEquals(expected, readService.readFromFile("src/test/java/resources/testToFile.csv"));
     }
 
     @Test(expected = RuntimeException.class)
     public void write_invalidPath_notOk() {
-        Map<String, Integer> testMap = new HashMap<>();
         testMap.put("orange", 55);
         testMap.put("apple", 42);
 
         writeService.writeToFile(testMap, "src/test/resources/testToFile.csv");
+    }
+
+    @Test
+    public void write_emptyMap_Ok() {
+        writeService.writeToFile(testMap, "src/test/java/resources/writeEmpty.csv");
+        try {
+            String result = Files.readString(Path.of("src/test/java/resources/writeEmpty.csv"));
+            assertEquals(16, result.length());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

64986ba7f78eb824249ba529111929d4d6018aa8
Artem Grunin <artem.grunin.2006@gmail.com>
2023-03-17 13:34:33 +0200

    refactored test_Reduce_Fruit_Quantity_In_Storage

--- src/test/java/core/basesyntax/strategy/handler/impl/PurchaseHandlerTest.java
+++ src/test/java/core/basesyntax/strategy/handler/impl/PurchaseHandlerTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 public class PurchaseHandlerTest {
     private static final String APPLE = "apple";
     private static final int VALID_FRUIT_QUANTITY = 10;
-    private static final int ANOTHER_VALID_FRUIT_QUANTITY = 5;
+    private static final int ANOTHER_VALID_FRUIT_QUANTITY = 20;
     private static final int VALID_OPERATION_QUANTITY = 5;
     private static final int MORE_THAN_VALID_FRUIT_QUANTITY = 11;
     private static final String BANANA = "banana";
@@ -37,11 +37,11 @@ public class PurchaseHandlerTest {
         FruitTransaction transaction = new FruitTransaction(
                 FruitTransaction.Operation.PURCHASE,
                 APPLE,
-                ANOTHER_VALID_FRUIT_QUANTITY);
+                VALID_FRUIT_QUANTITY);
         Integer beforeQuantity = Storage.storage.getOrDefault(APPLE, ZERO_FRUIT_QUANTITY);
         handler.handle(transaction);
         Integer actualQuantity = Storage.storage.getOrDefault(APPLE, ZERO_FRUIT_QUANTITY);
-        int expectedQuantity = beforeQuantity - ANOTHER_VALID_FRUIT_QUANTITY;
+        int expectedQuantity = beforeQuantity - VALID_FRUIT_QUANTITY;
         assertSame(expectedQuantity, actualQuantity);
     }
 
@@ -58,20 +58,11 @@ public class PurchaseHandlerTest {
     public void test_Reduce_Fruit_Quantity_In_Storage() {
         FruitTransaction transaction = new FruitTransaction(
                 FruitTransaction.Operation.PURCHASE,
-                APPLE,
-                VALID_OPERATION_QUANTITY);
+                BANANA,
+                ANOTHER_VALID_FRUIT_QUANTITY);
         handler.handle(transaction);
-        Integer actualQuantity = Storage.storage.get(APPLE);
-        int expectedQuantity = VALID_FRUIT_QUANTITY - VALID_OPERATION_QUANTITY;
-        assertSame(expectedQuantity, actualQuantity);
-
-        transaction = new FruitTransaction(
-                FruitTransaction.Operation.PURCHASE,
-                APPLE,
-                VALID_OPERATION_QUANTITY);
-        handler.handle(transaction);
-        actualQuantity = Storage.storage.getOrDefault(APPLE, ZERO_FRUIT_QUANTITY);
-        expectedQuantity = ZERO_FRUIT_QUANTITY;
+        Integer actualQuantity = Storage.storage.get(BANANA);
+        int expectedQuantity = ZERO_FRUIT_QUANTITY;
         assertSame(expectedQuantity, actualQuantity);
     }
 

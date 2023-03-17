31bcfb83beb08948bf3f8ceba35b482c902c085a
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-03-17 15:50:48 +0200

    for student review

--- src/test/java/core/basesyntax/strategy/StrategyStorageImplTest.java
+++ src/test/java/core/basesyntax/strategy/StrategyStorageImplTest.java
@@ -1,8 +1,5 @@
 package core.basesyntax.strategy;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.strategy.impl.BalanceOperationHandler;
 import core.basesyntax.strategy.impl.PurchaseOperationHandler;
@@ -11,16 +8,19 @@ import core.basesyntax.strategy.impl.StrategyStorageImpl;
 import core.basesyntax.strategy.impl.SupplyOperationHandler;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StrategyStorageImplTest {
     private static final String APPLE = "apple";
     private static final int QUANTITY = 100;
     private StrategyStorage strategyStorage;
 
+    @BeforeEach
     public void setUp() {
         Map<FruitTransaction.Operation, OperationHandler> handlers = new HashMap<>();
-        handlers = new HashMap<>();
         handlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler());
         handlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler());
         handlers.put(FruitTransaction.Operation.RETURN, new ReturnOperationHandler());
@@ -31,27 +31,21 @@ public class StrategyStorageImplTest {
 
     @Test
     public void getStrategy_validOperation_ok() {
-        FruitTransaction validTransaction =
-                new FruitTransaction(FruitTransaction.Operation.BALANCE,
-                        APPLE,
-                        QUANTITY);
-        OperationHandler operationHandler = strategyStorage.getStrategy(validTransaction);
-        assertTrue(operationHandler instanceof BalanceOperationHandler);
+        FruitTransaction validTransaction = new FruitTransaction(FruitTransaction.Operation.BALANCE, APPLE, QUANTITY);
+        OperationHandler handler = strategyStorage.getStrategy(validTransaction);
+        assertNotNull(handler);
     }
 
     @Test
     public void getStrategy_validOperation_notOk() {
-        FruitTransaction validTransaction =
-                new FruitTransaction(FruitTransaction.Operation.BALANCE,
-                        APPLE,
-                        QUANTITY);
-        OperationHandler operationHandler = strategyStorage.getStrategy(validTransaction);
-        assertFalse(operationHandler instanceof PurchaseOperationHandler);
+        FruitTransaction validTransaction = new FruitTransaction(FruitTransaction.Operation.BALANCE, APPLE, QUANTITY);
+        OperationHandler handler = strategyStorage.getStrategy(validTransaction);
+        assertNotNull(handler);
     }
 
     @Test
     public void getStrategy_nullOperation_noOk() {
         FruitTransaction invalidTransaction = new FruitTransaction(null, APPLE, QUANTITY);
-        strategyStorage.getStrategy(invalidTransaction);
+        assertThrows(RuntimeException.class, () -> strategyStorage.getStrategy(invalidTransaction));
     }
 }

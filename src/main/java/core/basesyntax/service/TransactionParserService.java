cb57bc4e3f879501bdc9af3e6d18c495f6d0b509
Oleksandr Krasnov <redman2775@gmail.com>
2023-03-17 19:18:09 +0200

    Added constant rows; test coverage of tested services little beat less than 100%; maven checkstyle BUILD SUCCESS

--- src/main/java/core/basesyntax/service/TransactionParserService.java
+++ src/main/java/core/basesyntax/service/TransactionParserService.java
@@ -3,6 +3,5 @@ package core.basesyntax.service;
 import core.basesyntax.model.FruitTransaction;
 
 public interface TransactionParserService {
-    //void saveToStorage(List<String> list);
     FruitTransaction saveToStorage(String line);
 }

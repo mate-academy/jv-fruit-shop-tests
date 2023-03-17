1716bab9b6d5c1afc3188bd7438b512ba0d02726
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-03-17 15:23:44 +0200

    made requested changes

--- src/test/java/core/basesyntax/service/impl/FileWriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileWriterServiceImplTest.java
@@ -11,10 +11,9 @@ import org.junit.Test;
 
 public class FileWriterServiceImplTest {
     private static FileWriterService fileWriterService;
-    private static final String HEAD = "fruit,quantity";
     private static final String INVALID_FILE_PATH = "G://unreal_folder.csv";
     private static final String VALID_FILE_PATH = "src/test/resources/new.csv";
-    private static String REPORT = HEAD;
+    private static String REPORT = "fruit,quantity";
 
     @BeforeClass
     public static void beforeClass() {
@@ -27,15 +26,11 @@ public class FileWriterServiceImplTest {
     }
 
     @Test
-    public void writeToFile_validFata_Ok() {
+    public void writeToFile_validFata_Ok() throws IOException {
         File file = new File(VALID_FILE_PATH);
         fileWriterService.writeToFile(REPORT, VALID_FILE_PATH);
-        try {
-            String expected = Files.readAllLines(file.toPath()).get(0);
-            assertEquals(REPORT, expected);
-            file.delete();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        String expected = Files.readAllLines(file.toPath()).get(0);
+        assertEquals(REPORT, expected);
+        file.delete();
     }
 }

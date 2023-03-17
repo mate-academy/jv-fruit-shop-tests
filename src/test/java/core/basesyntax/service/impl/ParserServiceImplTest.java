926c64a8f4c7c573de06fa0659bfc7c4081d4d4c
Oleksii Stepaniak <alexstepanyak@gmail.com>
2023-03-18 01:20:28 +0200

    fixed some mistakes

--- src/test/java/core/basesyntax/service/impl/ParserServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ParserServiceImplTest.java
@@ -7,6 +7,7 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.ParserService;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,14 +15,14 @@ import org.junit.Test;
 public class ParserServiceImplTest {
     private static final String BANANA = "banana";
     private static final String APPLE = "apple";
-    private static final FruitTransaction.Operation BALANCE
-            = FruitTransaction.Operation.BALANCE;
-    private static final FruitTransaction.Operation SUPPLY
-            = FruitTransaction.Operation.SUPPLY;
-    private static final FruitTransaction.Operation PURCHASE
-            = FruitTransaction.Operation.PURCHASE;
-    private static final FruitTransaction.Operation RETURN
-            = FruitTransaction.Operation.RETURN;
+    private static final Optional<FruitTransaction.Operation> BALANCE
+            = Optional.ofNullable(FruitTransaction.Operation.BALANCE);
+    private static final Optional<FruitTransaction.Operation> SUPPLY
+            = Optional.ofNullable(FruitTransaction.Operation.SUPPLY);
+    private static final Optional<FruitTransaction.Operation> PURCHASE
+            = Optional.ofNullable(FruitTransaction.Operation.PURCHASE);
+    private static final Optional<FruitTransaction.Operation> RETURN
+            = Optional.ofNullable(FruitTransaction.Operation.RETURN);
     private static final List<String> VALID_DATA = List.of("type,fruit,quantity", "b,banana,20",
             "b,apple,100", "s,banana,100", "p,banana,13", "r,apple,10",
             "p,apple,20", "p,banana,5", "s,banana,50");
@@ -39,14 +40,14 @@ public class ParserServiceImplTest {
 
     @Before
     public void setUp() {
-        validDataTransactions = List.of(new FruitTransaction(BALANCE, BANANA, 20),
-                new FruitTransaction(BALANCE, APPLE, 100),
-                new FruitTransaction(SUPPLY, BANANA, 100),
-                new FruitTransaction(PURCHASE, BANANA, 13),
-                new FruitTransaction(RETURN, APPLE, 10),
-                new FruitTransaction(PURCHASE, APPLE, 20),
-                new FruitTransaction(PURCHASE, BANANA, 5),
-                new FruitTransaction(SUPPLY, BANANA, 50));
+        validDataTransactions = List.of(new FruitTransaction(BALANCE.get(), BANANA, 20),
+                new FruitTransaction(BALANCE.get(), APPLE, 100),
+                new FruitTransaction(SUPPLY.get(), BANANA, 100),
+                new FruitTransaction(PURCHASE.get(), BANANA, 13),
+                new FruitTransaction(RETURN.get(), APPLE, 10),
+                new FruitTransaction(PURCHASE.get(), APPLE, 20),
+                new FruitTransaction(PURCHASE.get(), BANANA, 5),
+                new FruitTransaction(SUPPLY.get(), BANANA, 50));
         emptyDataTransactions = Collections.emptyList();
     }
 
--- src/test/java/core/basesyntax/strategy/OperationStrategyImplTest.java
+++ src/test/java/core/basesyntax/strategy/OperationStrategyImplTest.java
@@ -17,18 +17,20 @@ import org.junit.Test;
 public class OperationStrategyImplTest {
     private static final Map<FruitTransaction.Operation, OperationHandler> operations
             = new HashMap<>();
-    private static final FruitTransaction.Operation BALANCE_OPERATION
-            = FruitTransaction.Operation.BALANCE;
-    private static final FruitTransaction.Operation SUPPLY_OPERATION
-            = FruitTransaction.Operation.SUPPLY;
-    private static final FruitTransaction.Operation PURCHASE_OPERATION
-            = FruitTransaction.Operation.PURCHASE;
-    private static final FruitTransaction.Operation RETURN_OPERATION
-            = FruitTransaction.Operation.RETURN;
+    private static final String DEFAULT_BANANA = "banana";
+    private static final int DEFAULT_QUANTITY = 20;
+    private static FruitTransaction.Operation balanceOperation;
+    private static FruitTransaction.Operation supplyOperation;
+    private static FruitTransaction.Operation purchaseOperation;
+    private static FruitTransaction.Operation returnOperation;
     private static OperationStrategy operationStrategy;
 
     @BeforeClass
     public static void beforeClass() {
+        balanceOperation = FruitTransaction.Operation.BALANCE;
+        supplyOperation = FruitTransaction.Operation.SUPPLY;
+        purchaseOperation = FruitTransaction.Operation.PURCHASE;
+        returnOperation = FruitTransaction.Operation.RETURN;
         operations.put(FruitTransaction.Operation.BALANCE, new BalanceOperationHandler());
         operations.put(FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler());
         operations.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler());
@@ -37,31 +39,39 @@ public class OperationStrategyImplTest {
     }
 
     @Test
-    public void get_balanceOperation_ok() {
-        OperationHandler expected = operationStrategy.get(BALANCE_OPERATION);
-        OperationHandler actual = operationStrategy.get(BALANCE_OPERATION);
-        assertEquals(expected.getClass(), actual.getClass());
+    public void get_balanceOperationHandler_ok() {
+        FruitTransaction fruitTransaction
+                = new FruitTransaction(balanceOperation, DEFAULT_BANANA, DEFAULT_QUANTITY);
+        String expected = BalanceOperationHandler.class.getSimpleName();
+        String actual = operationStrategy.get(balanceOperation).getClass().getSimpleName();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void get_supplyOperation_ok() {
-        OperationHandler expected = operationStrategy.get(SUPPLY_OPERATION);
-        OperationHandler actual = operationStrategy.get(SUPPLY_OPERATION);
-        assertEquals(expected.getClass(), actual.getClass());
+        FruitTransaction fruitTransaction
+                = new FruitTransaction(supplyOperation, DEFAULT_BANANA, DEFAULT_QUANTITY);
+        String expected = SupplyOperationHandler.class.getSimpleName();
+        String actual = operationStrategy.get(supplyOperation).getClass().getSimpleName();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void get_purchaseOperation_ok() {
-        OperationHandler expected = operationStrategy.get(PURCHASE_OPERATION);
-        OperationHandler actual = operationStrategy.get(PURCHASE_OPERATION);
-        assertEquals(expected.getClass(), actual.getClass());
+        FruitTransaction fruitTransaction
+                = new FruitTransaction(purchaseOperation, DEFAULT_BANANA, DEFAULT_QUANTITY);
+        String expected = PurchaseOperationHandler.class.getSimpleName();
+        String actual = operationStrategy.get(purchaseOperation).getClass().getSimpleName();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void get_returnOperation_ok() {
-        OperationHandler expected = operationStrategy.get(RETURN_OPERATION);
-        OperationHandler actual = operationStrategy.get(RETURN_OPERATION);
-        assertEquals(expected.getClass(), actual.getClass());
+        FruitTransaction fruitTransaction
+                = new FruitTransaction(returnOperation, DEFAULT_BANANA, DEFAULT_QUANTITY);
+        String expected = ReturnOperationHandler.class.getSimpleName();
+        String actual = operationStrategy.get(returnOperation).getClass().getSimpleName();
+        assertEquals(expected, actual);
     }
 
     @Test (expected = RuntimeException.class)

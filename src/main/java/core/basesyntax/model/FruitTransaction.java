ca3af0d29e11f6009b7f173fe69d9fc96fdb3e51
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-03-20 16:03:16 +0200

    add override equals & hashCode

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -1,6 +1,7 @@
 package core.basesyntax.model;
 
 import core.basesyntax.exeption.FruitShopException;
+import java.util.Objects;
 
 public class FruitTransaction {
     private final Operation operation;
@@ -23,6 +24,24 @@ public class FruitTransaction {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FruitTransaction that = (FruitTransaction) o;
+        return quantity == that.quantity && operation == that.operation
+                && Objects.equals(fruit, that.fruit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operation, fruit, quantity);
     }
 
     public enum Operation {

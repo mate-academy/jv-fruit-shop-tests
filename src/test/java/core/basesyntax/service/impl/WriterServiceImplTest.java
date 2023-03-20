1cc3f0ae17b5b21ab6d39093bdca32b2b4c6d7af
Skrudj <ssinilov@gmail.com>
2023-03-20 10:47:15 +0200

    removed unused fields from writer test

--- src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
@@ -2,34 +2,21 @@ package core.basesyntax.service.impl;
 
 import static org.junit.Assert.assertEquals;
 
-import core.basesyntax.db.FruitStorage;
-import core.basesyntax.service.ReportMakerService;
 import core.basesyntax.service.WriterService;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WriterServiceImplTest {
     private static final String VALID_PATH = "src/test/resources/testWriter.csv";
-    private static final String FRUIT = "apple";
-    private static final int QUANTITY = 80;
     private static final String EXPECTED_CONTENT = "fruit,quantity" + "\r\n" + "apple,80" + "\r\n";
     private static WriterService writer;
-    private static ReportMakerService reportMaker;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
-        reportMaker = new ReportMakerServiceImpl();
         writer = new WriterServiceImpl();
-        FruitStorage.fruitStorage.put(FRUIT, QUANTITY);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        FruitStorage.fruitStorage.clear();
     }
 
     @Test

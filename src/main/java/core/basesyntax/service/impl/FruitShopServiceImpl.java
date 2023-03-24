7481f1ddc257b33f8c9f7e64b9e420d5354facde
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-03-24 12:26:34 +0200

    fixed test

--- src/main/java/core/basesyntax/service/impl/FruitShopServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/FruitShopServiceImpl.java
@@ -4,6 +4,7 @@ import core.basesyntax.dao.Storage;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.FruitShopService;
 import core.basesyntax.strategy.OperationStrategy;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +22,9 @@ public class FruitShopServiceImpl implements FruitShopService {
     public Map<String, Integer> finalReport(List<FruitTransaction> parsed) {
         if (parsed == null) {
             throw new RuntimeException("None of the arguments must be null");
+        }
+        if (parsed.isEmpty()) {
+            return new HashMap<>();
         }
         parsed.forEach(t -> operationStrategy.get(t.getOperation()).operate(t));
         return Storage.getMap();

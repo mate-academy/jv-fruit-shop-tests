28855c13ea82e7644345c1c13c9ebdc40cb34a72
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-03-30 11:02:24 +0300

    renamed method name

--- src/test/java/core/basesyntax/FruitTest.java
+++ src/test/java/core/basesyntax/FruitTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
  */
 public class FruitTest {
     @Test
-    public void equals_fruitIsNull_NotOk() {
+    public void equals_fruitIsNotEqual_NotOk() {
         Fruit fruit0 = new Fruit("apple");
         Fruit fruit1 = new Fruit("banana");
         Assert.assertNotEquals(fruit0, fruit1);

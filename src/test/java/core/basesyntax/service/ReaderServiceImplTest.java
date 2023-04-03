9b37a617184f2afb5feb0e76107f3d0e7cde6e94
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-04-03 18:33:56 +0300

    PR with tests

---
+++ src/test/java/core/basesyntax/service/ReaderServiceImplTest.java
@@ -0,0 +1,52 @@
+package core.basesyntax.service;
+
+import core.basesyntax.service.impl.ReaderServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReaderServiceImplTest {
+    private static final String INPUT_FILE_PATH = "src/test/resources/input.csv";
+    private static final String EMPTY_FILE_PATH = "src/test/resources/empty.csv";
+    private ReaderServiceImpl readerService;
+    private List<String> records;
+
+    @Before
+    public void setUp() {
+        readerService = new ReaderServiceImpl();
+        records = new ArrayList<>();
+        records.add("type,fruit,quantity");
+        records.add("b,banana,20");
+        records.add("b,apple,100");
+        records.add("s,banana,102");
+        records.add("p,banana,13");
+        records.add("r,apple,10");
+        records.add("p,apple,20");
+        records.add("p,banana,5");
+        records.add("s,banana,50");
+    }
+
+    @Test
+    public void read_rightFile_Ok() {
+        List<String> actualResult = readerService.read(INPUT_FILE_PATH);
+        Assert.assertEquals("Test failed! You should returned next record "
+                        + records.get(0) + " but you returned "
+                        + actualResult.get(0),
+                records, actualResult);
+    }
+
+    @Test
+    public void read_emptyFile_Ok() {
+        List<String> actualResult = readerService.read(EMPTY_FILE_PATH);
+        records = new ArrayList<>();
+        Assert.assertEquals("Test failed! You should returned empty record.",
+                records, actualResult);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void read_emptyPath_notOk() {
+        readerService.read("");
+    }
+}
---
+++ src/test/java/core/basesyntax/service/ReportServiceImplTest.java
@@ -0,0 +1,28 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.service.impl.ReportServiceImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReportServiceImplTest {
+    private ReportServiceImpl reportService;
+    private String expectedText;
+
+    @Before
+    public void setUp() {
+        reportService = new ReportServiceImpl();
+        expectedText = "banana,154";
+        Storage.fruits.put("banana", 154);
+    }
+
+    @Test
+    public void createReport_rightStorage_Ok() {
+        String text = reportService.createReport();
+        String[] actualResult = text.split(System.lineSeparator());
+        Assert.assertEquals("Test failed! You should returned next text "
+                + expectedText + " but you returned "
+                + actualResult[0], actualResult[0], expectedText);
+    }
+}

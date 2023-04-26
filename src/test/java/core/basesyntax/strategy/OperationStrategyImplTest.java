b0ea034c1f07b4ce6025c2c3665066932f99f93d
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-04-26 12:20:24 +0300

    added fixes after review

--- src/test/java/core/basesyntax/strategy/OperationStrategyImplTest.java
+++ src/test/java/core/basesyntax/strategy/OperationStrategyImplTest.java
@@ -5,58 +5,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import core.basesyntax.model.FruitTransaction;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class OperationStrategyImplTest {
     private static Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
     private static OperationStrategy operationStrategy;
-    private static OperationHandler balanceOperationHandler;
-    private static OperationHandler supplyOperationHandler;
-    private static OperationHandler purchaseOperationHandler;
-    private static OperationHandler returnOperationHandler;
 
     @BeforeAll
     static void beforeAll() {
         operationHandlerMap = new HashMap<>();
         operationStrategy = new OperationStrategyImpl(operationHandlerMap);
-        balanceOperationHandler = new BalanceOperationHandler();
-        supplyOperationHandler = new SupplyOperationHandler();
-        purchaseOperationHandler = new PurchaseOperationHandler();
-        returnOperationHandler = new ReturnOperationHandler();
-    }
-
-    @AfterEach
-    void tearDown() {
-        operationHandlerMap.clear();
+        operationHandlerMap.put(FruitTransaction.Operation.BALANCE,
+                new BalanceOperationHandler());
+        operationHandlerMap.put(FruitTransaction.Operation.SUPPLY,
+                new SupplyOperationHandler());
+        operationHandlerMap.put(FruitTransaction.Operation.PURCHASE,
+                new PurchaseOperationHandler());
+        operationHandlerMap.put(FruitTransaction.Operation.RETURN,
+                new ReturnOperationHandler());
     }
 
     @Test
     void get_checkOnBalanceOperationHandler_Ok() {
-        operationHandlerMap.put(FruitTransaction.Operation.BALANCE, balanceOperationHandler);
         OperationHandler actual = operationStrategy.get(FruitTransaction.Operation.BALANCE);
-        assertEquals(balanceOperationHandler,actual);
+        assertEquals(BalanceOperationHandler.class,actual.getClass());
     }
 
     @Test
     void get_checkOnSupplyOperationHandler_Ok() {
-        operationHandlerMap.put(FruitTransaction.Operation.SUPPLY, supplyOperationHandler);
         OperationHandler actual = operationStrategy.get(FruitTransaction.Operation.SUPPLY);
-        assertEquals(supplyOperationHandler,actual);
+        assertEquals(SupplyOperationHandler.class,actual.getClass());
     }
 
     @Test
     void get_checkOnPurchaseOperationHandler_Ok() {
-        operationHandlerMap.put(FruitTransaction.Operation.PURCHASE, purchaseOperationHandler);
         OperationHandler actual = operationStrategy.get(FruitTransaction.Operation.PURCHASE);
-        assertEquals(purchaseOperationHandler,actual);
+        assertEquals(PurchaseOperationHandler.class,actual.getClass());
     }
 
     @Test
     void get_checkOnReturnOperationHandler_Ok() {
-        operationHandlerMap.put(FruitTransaction.Operation.RETURN, returnOperationHandler);
         OperationHandler actual = operationStrategy.get(FruitTransaction.Operation.RETURN);
-        assertEquals(returnOperationHandler,actual);
+        assertEquals(ReturnOperationHandler.class,actual.getClass());
     }
 }

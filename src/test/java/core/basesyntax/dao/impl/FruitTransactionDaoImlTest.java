3ccbf572b391ceb216645daf163c4e0970e4f438
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-05-11 02:06:48 +0300

    created test for fruitShop

--- src/test/java/core/basesyntax/dao/impl/FruitTransactionDaoImlTest.java
+++ src/test/java/core/basesyntax/dao/impl/FruitTransactionDaoImlTest.java
@@ -2,12 +2,12 @@ package core.basesyntax.dao.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;

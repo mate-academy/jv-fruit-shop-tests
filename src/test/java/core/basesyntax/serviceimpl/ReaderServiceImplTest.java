9c907d3f619959690620ad2a361959ae18be8a6d
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-05-15 17:15:29 +0200

    changes In ReaderServiceImplTest

--- src/test/java/core/basesyntax/serviceimpl/ReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/serviceimpl/ReaderServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import core.basesyntax.service.ReaderService;
 import core.basesyntax.service.impl.ReaderServiceImpl;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 
@@ -11,22 +12,17 @@ public class ReaderServiceImplTest {
     private static final String INVALID_PATH =
             "wrong";
     private static final String VALID_PATH =
-            "src/test/java/resources/testreport.csv";
-    private static final List<String> CORRECT_DATA = List.of("type,fruit,quantity",
-            "b,banana,20",
-            "b,apple,100",
-            "s,banana,100",
-            "p,banana,13",
-            "r,apple,10",
-            "p,apple,20",
-            "p,banana,5",
-            "s,banana,50");
-    private final ReaderService readerService = new ReaderServiceImpl();
+            "src/test/java/core/basesyntax/resources/testreport";
+    private ReaderService readerService = new ReaderServiceImpl();
 
-    @Test(expected = RuntimeException.class)
+    @Test
     public void read_validPath_ok() {
-        List<String> actual = readerService.readFromFile(VALID_PATH);
-        assertEquals(CORRECT_DATA, actual);
+        final List<String> actual = readerService.readFromFile(VALID_PATH);
+        List<String> expected = new ArrayList<>();
+        expected.add("fruit,quantity");
+        expected.add("banana,152");
+        expected.add("apple,90");
+        assertEquals(expected, actual);
     }
 
     @Test (expected = RuntimeException.class)

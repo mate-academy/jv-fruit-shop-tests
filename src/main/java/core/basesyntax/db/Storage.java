42387f737ef60c8d877473e50c6cc9ad81296b6c
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-05-30 21:24:42 +0300

    copy code from the previous task

---
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -0,0 +1,14 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.FruitTransaction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface Storage {
+    Map<String, Integer> storage = new HashMap<>();
+
+    void update(FruitTransaction fruitTransaction, int count);
+
+    int calculateAmount(FruitTransaction fruitTransaction);
+}
---
+++ src/main/java/core/basesyntax/service/impl/FileReaderServiceImpl.java
@@ -0,0 +1,53 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.exception.InvalidValueExeption;
+import core.basesyntax.service.CsvFileReaderService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileReaderServiceImpl implements CsvFileReaderService {
+    private static final int FIRST_LINE = 1;
+
+    private String fileSource;
+
+    public FileReaderServiceImpl(String fileSource) {
+        this.fileSource = fileSource;
+    }
+
+    public String getFileSource() {
+        return fileSource;
+    }
+
+    @Override
+    public List<String> readFile() {
+        Path filePath = Paths.get(fileSource);
+        if (!Files.exists(filePath)) {
+            throw new RuntimeException("File not found: " + fileSource);
+        }
+        try {
+            return Files.lines(filePath)
+                    .skip(FIRST_LINE)
+                    .peek(this::checkData)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read data from file: " + getFileSource(), e);
+        }
+    }
+
+    private void checkData(String line) {
+        if (!isValidFormat(line)) {
+            throw new InvalidValueExeption("Data from " + getFileSource()
+                    + "is in an incorrect format");
+        }
+    }
+
+    private boolean isValidFormat(String line) {
+        String regex = "^[bpsr],\\w+,[0-9]+$";
+        return line.matches(regex);
+    }
+}

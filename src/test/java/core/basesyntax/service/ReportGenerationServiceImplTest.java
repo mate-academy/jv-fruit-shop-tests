12bdb8e4484d0dadc169e0e6723f6f5a9554e95e
Pasha <pavlokrasota@gmail.com>
2023-06-02 12:34:18 +0300

    Removed checkStyle mistake

--- src/test/java/core/basesyntax/service/ReportGenerationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/ReportGenerationServiceImplTest.java
@@ -6,7 +6,6 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.service.impl.ReportGenerationServiceImpl;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;

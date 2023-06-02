275991192b2e67be274796737332ab8e941a5a6d
Arina Polna <polna.arina@gmail.com>
2023-06-02 10:29:00 +0300

    create constants for magic numbers in PurchaseImpTest class

--- src/test/java/core/basesyntax/service/operation/PurchaseHandlerTest.java
+++ src/test/java/core/basesyntax/service/operation/PurchaseHandlerTest.java
@@ -16,6 +16,8 @@ public class PurchaseHandlerTest {
                 = FruitTransaction.Operation.PURCHASE;
     private static final String DEFAULT_FRUIT_NAME = "banana";
     private static final int DEFAULT_FRUIT_QUANTITY = 10;
+    private static final int NEGATIVE_FRUIT_QUANTITY = -8;
+    private static final int EIGHTEEN_FRUIT_QUANTITY = 18;
     private static PurchaseHandler purchaseHandler;
     private FruitTransaction fruitTransaction;
 
@@ -32,15 +34,14 @@ public class PurchaseHandlerTest {
 
     @Test
     public void handle_FruitTransactionWithNegativeQuantity_notOk() {
-        fruitTransaction.setQuantity(-8);
+        fruitTransaction.setQuantity(NEGATIVE_FRUIT_QUANTITY);
         assertThrows(NegativeQuantityException.class, () ->
                     purchaseHandler.handle(fruitTransaction));
     }
 
     @Test
     public void handle_purchaseMoreThanAvailableFruits_notOk() {
-        fruitTransaction.setQuantity(21);
-        Storage.fruitMap.put("banana", 20);
+        Storage.fruitMap.put("banana", 9);
         Storage.fruitMap.put("apple", 100);
         assertThrows(NegativeQuantityException.class, () ->
                     purchaseHandler.handle(fruitTransaction));
@@ -48,7 +49,7 @@ public class PurchaseHandlerTest {
 
     @Test
     public void handle_purchaseValidFruitTransaction_ok() {
-        fruitTransaction.setQuantity(18);
+        fruitTransaction.setQuantity(EIGHTEEN_FRUIT_QUANTITY);
         Storage.fruitMap.put("banana", 20);
         Storage.fruitMap.put("apple", 100);
         purchaseHandler.handle(fruitTransaction);

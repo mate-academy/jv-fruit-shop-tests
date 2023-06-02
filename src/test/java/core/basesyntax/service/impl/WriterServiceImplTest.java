0c92d7df1ad062fc5fd7aff223058a172715e047
Viktor Bykh <bykhviktor21@gmail.com>
2023-06-02 15:00:30 +0300

    fixed first version of hw jv-fruit-shop-tests

--- src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/WriterServiceImplTest.java
@@ -1,30 +1,28 @@
 package core.basesyntax.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 class WriterServiceImplTest {
     private WriterServiceImpl writerService;
-    private Path filePath;
 
     @BeforeEach
-    void setUp(@TempDir Path tempDir) {
-        filePath = tempDir.resolve("test.txt");
+    void setUp() {
         writerService = new WriterServiceImpl();
     }
 
     @Test
     void writeDataToFile_existingFile_ok() throws IOException {
         List<String> data = List.of("Line 1", "Line 2", "Line 3");
-        writerService.writeDataToFile(filePath.toString(), data);
+        Path filePath = Path.of("src/test/resources/testOutput.csv");
+        writerService.writeDataToFile("src/test/resources/testOutput.csv", data);
         List<String> result = Files.readAllLines(filePath);
         assertEquals(data, result);
     }
@@ -32,19 +30,7 @@ class WriterServiceImplTest {
     @Test
     void writeDataToFile_invalidPath_notOk() {
         List<String> data = List.of("Line 1", "Line 2", "Line 3");
-        String invalidPath = "\0";
-        String invalidFilePath = invalidPath + filePath.toString();
-        assertThrows(RuntimeException.class,
-                () -> writerService.writeDataToFile(invalidFilePath, data));
-    }
-
-    @Test
-    void writeDataToFile_readOnlyFile_notOk() throws IOException {
-        Files.createFile(filePath);
-        Files.setAttribute(filePath, "dos:readonly", true);
-
-        List<String> data = List.of("Line 1", "Line 2", "Line 3");
-        assertThrows(RuntimeException.class,
-                () -> writerService.writeDataToFile(filePath.toString(), data));
+        Assertions.assertThrows(RuntimeException.class,
+                () -> writerService.writeDataToFile("test/src/test.csv", data));
     }
 }

dc754818bf8fdf25fcb88fbbf0109510982c0282
Ros <rospsix@gmail.com>
2023-06-13 18:14:42 +0100

    fix checkstyle

--- src/main/java/core/basesyntax/services/impl/WriteToFileServiceImpl.java
+++ src/main/java/core/basesyntax/services/impl/WriteToFileServiceImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.services.impl;
 
 import core.basesyntax.services.interfaces.WriteToFileService;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;

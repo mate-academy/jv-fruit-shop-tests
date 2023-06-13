c75a26e4e9f70cfeaedee205101c9a97c81b285a
Ros <rospsix@gmail.com>
2023-06-13 16:18:49 +0100

    Update

--- src/main/java/core/basesyntax/strategy/handler/BalanceHandler.java
+++ src/main/java/core/basesyntax/strategy/handler/BalanceHandler.java
@@ -6,6 +6,9 @@ import core.basesyntax.model.FruitTransaction;
 public class BalanceHandler implements OperationHandler {
     @Override
     public void operate(FruitTransaction transaction) {
+        if (transaction.getQuantity() < 0) {
+            throw new IllegalArgumentException("Invalid quantity: " + transaction.getQuantity());
+        }
         Storage.fruits.put(transaction.getFruit(), transaction.getQuantity());
     }
 }

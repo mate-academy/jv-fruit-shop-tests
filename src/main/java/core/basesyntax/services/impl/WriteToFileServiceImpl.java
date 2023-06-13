9859e5ec027f8518c78f14c721ccd1522c634af9
Ros <rospsix@gmail.com>
2023-06-13 18:12:47 +0100

    update

--- src/main/java/core/basesyntax/services/impl/WriteToFileServiceImpl.java
+++ src/main/java/core/basesyntax/services/impl/WriteToFileServiceImpl.java
@@ -1,17 +1,19 @@
 package core.basesyntax.services.impl;
 
 import core.basesyntax.services.interfaces.WriteToFileService;
+
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteToFileServiceImpl implements WriteToFileService {
     @Override
     public void writeReportToFile(String reportText, File toFile) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new java.io.FileWriter(toFile))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFile))) {
             bufferedWriter.write(reportText);
         } catch (IOException e) {
-            throw new RuntimeException("can't write data to file: " + toFile, e);
+            throw new RuntimeException("Failed to write data to file: " + toFile, e);
         }
     }
 }

17afcc3a4f1d8ee4ebc97f23169b08a9b890aaea
mIst <bogdankharchenko77@gmail.com>
2023-07-14 20:13:01 +0300

    added fixes after review

--- src/test/java/core/basesyntax/service/impl/FruitShopServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FruitShopServiceImplTest.java
@@ -2,37 +2,54 @@ package core.basesyntax.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.FruitShopService;
+import core.basesyntax.service.operation.BalanceOperationHandler;
 import core.basesyntax.service.operation.OperationHandler;
 import core.basesyntax.service.strategy.OperationStrategy;
-import java.util.ArrayList;
+import core.basesyntax.service.strategy.OperationStrategyImpl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FruitShopServiceImplTest {
-    private static final String APPLE = "apple";
-    private static final String BANANA = "banana";
+    private static final String FRUIT = "apple";
+    private static final int VALID_QUANTITY = 20;
     private static final List<FruitTransaction> FRUIT_TRANSACTIONS = List.of(
-            createFruitTransaction(FruitTransaction.Operation.BALANCE, APPLE, 20),
-            createFruitTransaction(FruitTransaction.Operation.PURCHASE, BANANA, 10)
+            createFruitTransaction(FruitTransaction.Operation.BALANCE, FRUIT, VALID_QUANTITY)
     );
     private static FruitShopService fruitShopService;
-    private static List<FruitTransaction> afterProcess;
+    private static OperationStrategy operationStrategy;
+    private static Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
 
     @BeforeEach
     void setUp() {
-        afterProcess = new ArrayList<>();
-        fruitShopService = new FruitShopServiceImpl(new OperationStrategyTest());
+        operationHandlerMap = new HashMap<>();
+        operationHandlerMap.put(FruitTransaction.Operation.BALANCE,
+                new BalanceOperationHandler());
+        operationStrategy = new OperationStrategyImpl(operationHandlerMap);
+        fruitShopService = new FruitShopServiceImpl(operationStrategy);
+        Storage.fruits.put(FRUIT, VALID_QUANTITY);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.fruits.clear();
     }
 
     @Test
     void processingOperations_validTransactions_Ok() {
+        FruitTransaction transaction = new FruitTransaction();
+        transaction.setQuantity(Storage.fruits.get(FRUIT));
         fruitShopService.processingOperations(FRUIT_TRANSACTIONS);
-        assertEquals(FRUIT_TRANSACTIONS, afterProcess);
+        FruitTransaction actualTransaction = new FruitTransaction();
+        actualTransaction.setQuantity(Storage.fruits.get(FRUIT));
+        assertEquals(transaction, actualTransaction);
 
     }
 
@@ -44,29 +61,5 @@ class FruitShopServiceImplTest {
         transaction.setFruit(fruit);
         transaction.setQuantity(quantity);
         return transaction;
-    }
-
-    private static class OperationStrategyTest implements OperationStrategy {
-        private final Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
-
-        public OperationStrategyTest() {
-            operationHandlerMap = new HashMap<>();
-            operationHandlerMap.put(FruitTransaction.Operation.BALANCE,
-                    new OperationHandlerTest());
-            operationHandlerMap.put(FruitTransaction.Operation.PURCHASE,
-                    new OperationHandlerTest());
-        }
-
-        @Override
-        public OperationHandler getOperationHandler(FruitTransaction.Operation operation) {
-            return operationHandlerMap.get(operation);
-        }
-    }
-
-    private static class OperationHandlerTest implements OperationHandler {
-        @Override
-        public void handleTransaction(FruitTransaction transaction) {
-            afterProcess.add(transaction);
-        }
     }
 }

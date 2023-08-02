3299346249eab6d1093afe77ed22359a510001d6
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-08-03 02:24:47 +0300

    create tests for jv-fruit-shop

--- src/test/java/core/basesyntax/strategy/handler/impl/OperationStrategyImplTest.java
+++ src/test/java/core/basesyntax/strategy/handler/impl/OperationStrategyImplTest.java
@@ -49,8 +49,4 @@ class OperationStrategyImplTest {
         assertThrows(NullPointerException.class, ()
                 -> operationStrategy.getHandler(null));
     }
-
-    @Test
-    void getHandler() {
-    }
 }

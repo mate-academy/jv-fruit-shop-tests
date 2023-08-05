85079f865ce5a3d75178ca54cfff629f725ee610
Vitalii <mkukoba11@ukr.net>
2023-08-05 14:17:29 +0300

    changed import assertTrue and import assertFalse in FileReaderTest

--- src/test/java/core/basesyntax/service/impl/FileReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileReaderServiceImplTest.java
@@ -1,9 +1,9 @@
 package core.basesyntax.service.impl;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import core.basesyntax.service.FileReaderService;
 import java.util.List;

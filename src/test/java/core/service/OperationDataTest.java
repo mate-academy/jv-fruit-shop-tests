39c815478f513a789b5795b2bd26c5387045a80d
DaniilNepelev <kakashilvng@gmail.com>
2023-08-06 22:31:27 +0300

    change pom.xml file for default

---
+++ src/test/java/core/service/OperationDataTest.java
@@ -0,0 +1,25 @@
+package core.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class OperationDataTest {
+    private static final OperationData operationData =
+            new OperationData(OperationType.B, "berry", 1);
+
+    @Test
+    void getOperation() {
+        assertEquals(OperationType.B, operationData.getOperationType());
+    }
+
+    @Test
+    void getFruit() {
+        assertEquals("berry", operationData.getProduct());
+    }
+
+    @Test
+    void getQuantity() {
+        assertEquals(1, operationData.getQuantity());
+    }
+}

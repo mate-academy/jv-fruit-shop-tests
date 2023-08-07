7c0794dd48bc30a07c914466f6f115bca0d75ef0
Iryna Mekh <iryna.mekh@gmail.com>
2023-08-07 19:02:57 +0300

    added small checkstyle fixes

--- src/test/java/core/basesyntax/filedata/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/filedata/FileReaderImplTest.java
@@ -19,8 +19,8 @@ class FileReaderImplTest {
     private static final String EMPTY_LINE_FILE_PATH = "src/test/resources/emptyInput.csv";
     private static final String NUMBERS_INPUT_FILE_PATH = "src/test/resources/numbersInput.csv";
     private static final String SYMBOLS_INPUT_FILE_PATH = "src/test/resources/symbolsInput.csv";
-    private static final String VALID_EXCEPTION_MESSAGE = "Can't read file "
-            + "src\\test\\resources\\validData.csv";
+    private static final String VALID_EXCEPTION_MESSAGE =
+            "Can't read file " + new File(UNEXISTING_FILE_PATH);
     private static final FileReader fileReader = new FileReaderImpl();
 
     @Test

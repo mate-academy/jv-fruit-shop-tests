74b131cfe5e554a72f39ee3d1e36d3c3acb59843
Vitalii <mkukoba11@ukr.net>
2023-08-07 16:14:23 +0300

    fixed checkstyle mistakes

--- src/test/java/core/basesyntax/service/impl/ProccessDataImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ProccessDataImplTest.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;

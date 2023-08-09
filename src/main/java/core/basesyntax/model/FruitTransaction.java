778a02a0b6954b24f38e338c84a7b00e2a3652e6
literandltx <romanskovronn@gmail.com>
2023-08-09 14:11:49 +0300

    added hashCode and equals

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -1,5 +1,7 @@
 package core.basesyntax.model;
 
+import java.util.Objects;
+
 public class FruitTransaction {
     private final Operation operation;
     private final String fruit;
@@ -32,4 +34,31 @@ public class FruitTransaction {
                 + '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        FruitTransaction that = (FruitTransaction) o;
+
+        if (quantity != that.quantity) {
+            return false;
+        }
+        if (operation != that.operation) {
+            return false;
+        }
+        return Objects.equals(fruit, that.fruit);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = operation != null ? operation.hashCode() : 0;
+        result = 31 * result + (fruit != null ? fruit.hashCode() : 0);
+        result = 31 * result + quantity;
+        return result;
+    }
 }

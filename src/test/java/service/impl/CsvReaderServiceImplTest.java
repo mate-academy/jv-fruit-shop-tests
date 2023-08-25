98d545e8dd690e95776e3cd8bcd28e9c1e3adb8f
Tymofii Skrypko <skrypko270901@gmail.com>
2023-08-25 19:55:09 +0200

    implemented all requested changes

--- src/test/java/service/impl/CsvReaderServiceImplTest.java
+++ src/test/java/service/impl/CsvReaderServiceImplTest.java
@@ -1,28 +1,30 @@
 package service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.Test;
 import service.CsvReaderService;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CsvReaderServiceImplTest {
     private static final String EMPTY_FILE_NAME = "src/test/java/test_files/emptyFile.csv";
     private static final String DEFAULT_FILE_NAME = "src/test/java/test_files/defaultFile.csv";
     private static final String NO_FILE_NAME = "";
     private static final CsvReaderService readerService = new CsvReaderServiceImpl();
-    private static final String[] ACTUAL_CONTENT = """
-            b,banana,20
-            b,apple,100
-            s,banana,100
-            p,banana,13
-            r,apple,10
-            p,apple,20
-            p,banana,5
-            s,banana,50
-            b,kiwi,10""".split(System.lineSeparator());
+    private static final String ACTUAL_CONTENT = "b,banana,20\n"
+            + "b,apple,100\n"
+            + "s,banana,100\n"
+            + "p,banana,13\n"
+            + "r,apple,10\n"
+            + "p,apple,20\n"
+            + "p,banana,5\n"
+            + "s,banana,50\n"
+            + "b,kiwi,10";
 
     @Test
     public void defaultFile_Ok() {
-        assertArrayEquals(ACTUAL_CONTENT,
+        assertArrayEquals(ACTUAL_CONTENT.split(System.lineSeparator()),
                 readerService.readFromFile(DEFAULT_FILE_NAME),
                 "Actual content differs from reader work result");
     }

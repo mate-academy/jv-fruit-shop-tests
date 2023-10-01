b45803b42d2cdaa671aaf5bfc6050570b7661bfa
hp <volodya.gata@gmail.com>
2023-10-01 16:42:28 +0300

    added method for reading the file and then checked if they equal to each other

--- src/test/java/core/basesyntax/service/impl/FileWriterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileWriterServiceImplTest.java
@@ -1,10 +1,14 @@
 package core.basesyntax.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.service.FileWriterService;
 import core.basesyntax.storage.Storage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -39,5 +43,29 @@ class FileWriterServiceImplTest {
         String actual = "src/test/resources///";
         assertThrows(
                 RuntimeException.class, () -> fileWriterService.writeToFile(report, actual));
+    }
+
+    @Test
+    void writer_WriteToFile_Ok() {
+        String expected = "fruit,quantity" + System.lineSeparator()
+                + "banana, 152" + System.lineSeparator()
+                + "apple, 90" + System.lineSeparator();
+        fileWriterService.writeToFile(expected, "src/test/resources/validReportFile.csv");
+        String actual = readFromFile("src/test/resources/validReportFile.csv");
+        assertEquals(expected, actual);
+
+    }
+
+    private String readFromFile(String fromFileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append(System.lineSeparator());
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

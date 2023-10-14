59229f218e0cb1baf74b580578eeb587ff014044
Danylo <kdanil2000@gmail.com>
2023-10-14 12:18:43 +0300

    Implement srvice test

---
+++ src/test/java/core/basesyntax/service/impl/DataProcessorServiceImplTest.java
@@ -0,0 +1,68 @@
+package core.basesyntax.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import data.base.Storage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import model.FruitTransaction;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import service.DataProcessorService;
+import service.OperationStrategy;
+import service.impl.DataProcessorServiceImpl;
+import service.impl.OperationStrategyImpl;
+import service.strategy.hendlers.BalanceOperationHandler;
+import service.strategy.hendlers.PurchaseOperationHandler;
+import service.strategy.hendlers.ReturnOperationHandler;
+import service.strategy.hendlers.SupplyOperationHandler;
+
+public class DataProcessorServiceImplTest {
+    private static DataProcessorService dataProcessorService;
+
+    @BeforeAll
+    static void beforeAll() {
+        OperationStrategy operationStrategy = new OperationStrategyImpl(Map.of(
+                FruitTransaction.Operation.BALANCE, new BalanceOperationHandler(),
+                FruitTransaction.Operation.PURCHASE, new PurchaseOperationHandler(),
+                FruitTransaction.Operation.RETURN, new ReturnOperationHandler(),
+                FruitTransaction.Operation.SUPPLY, new SupplyOperationHandler()));
+        dataProcessorService = new DataProcessorServiceImpl(operationStrategy);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.STORAGE.clear();
+    }
+
+    @Test
+    void updateDataInStorage_invalidOperation_notOk() {
+        List<String> data = List.of("b,banana,1", "b,apple,1", "q,banana,1");
+        assertThrows(RuntimeException.class,
+                () -> dataProcessorService.processData(data));
+    }
+
+    @Test
+    void updateDataInStorage_negativeQuantity_notOk() {
+        List<String> data = List.of("b,banana,-1", "b,apple,1", "s,banana,1");
+        assertThrows(RuntimeException.class,
+                () -> dataProcessorService.processData(data));
+    }
+
+    @Test
+    void updateDataInStorage_validData_ok() {
+        List<String> data = new ArrayList<>();
+        data.add("type,fruit,quantity");
+        data.add("b,banana,1");
+        data.add("b,apple,1");
+        data.add("p,banana,1");
+        dataProcessorService.processData(data);
+        assertEquals(Storage.STORAGE.get("banana"), 0);
+        assertEquals(Storage.STORAGE.get("apple"), 1);
+    }
+}
+
+
---
+++ src/test/java/core/basesyntax/service/impl/FileWriterServiceImplTest.java
@@ -0,0 +1,57 @@
+package core.basesyntax.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import service.FileWriterService;
+import service.impl.FileWriterServiceImpl;
+
+public class FileWriterServiceImplTest {
+    private static FileWriterService fileWriterService;
+    private static final String OUTPUT_FILE = "src/test/resources/test_output.csv";
+    private static final String NON_EXISTENT_OUTPUT_FILE
+            = "src/test/someResources/non_existent_output.csv";
+    private static final String EXPECTED_OUTPUT = expectedOutputCreator();
+    private static List<String> output;
+
+    private static String expectedOutputCreator() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("fruit,quantity").append(System.lineSeparator())
+                .append("banana,90").append(System.lineSeparator())
+                .append("apple,100").append(System.lineSeparator());
+        return sb.toString();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        fileWriterService = new FileWriterServiceImpl();
+        output = List.of(
+                "fruit,quantity", "banana,90", "apple,100");
+    }
+
+    @Test
+    void writeToFile_validFile_ok() throws IOException {
+        StringBuilder builder = new StringBuilder();
+        assertDoesNotThrow(() -> fileWriterService.writeIntoFile(output, OUTPUT_FILE));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(OUTPUT_FILE))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line).append(System.lineSeparator());
+            }
+        }
+        assertEquals(EXPECTED_OUTPUT, builder.toString());
+    }
+
+    @Test
+    void writeToFIle_invalidFilePath_NotOk() {
+        assertThrows(RuntimeException.class,
+                () -> fileWriterService.writeIntoFile(output, NON_EXISTENT_OUTPUT_FILE));
+    }
+}

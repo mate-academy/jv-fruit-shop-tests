d01d887b1fcd021020f583f47feb7e927379ecbb
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-17 19:20:04 +0300

    added changes to the class Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,8 +6,8 @@ import core.basesyntax.model.FruitTransaction;
 public class Main {
     public static void main(String[] args) {
         // Add some fruit transactions to the storage
-        Storage.storage.put("apple", 10);
-        Storage.storage.put("banana", 15);
+        Storage.getStorage().put("apple", 10);
+        Storage.getStorage().put("banana", 15);
         FruitTransaction applePurchase = new FruitTransaction(
                 FruitTransaction.Operation.PURCHASE, "apple", 10);
         FruitTransaction bananaPurchase = new FruitTransaction(
@@ -15,14 +15,14 @@ public class Main {
         processTransaction(applePurchase);
         processTransaction(bananaPurchase);
         System.out.println("Updated Storage:");
-        Storage.storage.forEach((fruit, quantity) -> {
+        Storage.getStorage().forEach((fruit, quantity) -> {
             System.out.println(fruit + ": " + quantity);
         });
     }
 
     private static void processTransaction(FruitTransaction transaction) {
         if (transaction.getOperation() == FruitTransaction.Operation.PURCHASE) {
-            Storage.storage.merge(transaction.getFruit(), transaction.getQuantity(), Integer::sum);
+            Storage.getStorage().merge(transaction.getFruit(), transaction.getQuantity(), Integer::sum);
         }
     }
 }

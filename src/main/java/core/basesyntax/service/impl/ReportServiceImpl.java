7f76bd5fab96ada50ee25fe0090f1d5c0ba58a72
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-17 19:19:23 +0300

    added changes to the class ReportServiceImpl

--- src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportServiceImpl.java
@@ -9,7 +9,7 @@ public class ReportServiceImpl implements ReportService {
     @Override
     public String createReport() {
         StringBuilder builder = new StringBuilder(TITLE);
-        Storage.storage.entrySet().stream()
+        Storage.getStorage().entrySet().stream()
                 .map(fruit -> builder.append(fruit.getKey()));
         return builder.toString();
     }

a3a1bd77bc93fc90062fb4f97511bb33d289b30b
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-17 19:17:00 +0300

    added changes to the class OperationHandlerTest

--- src/test/java/core/basesyntax/service/operation/OperationHandlerTest.java
+++ src/test/java/core/basesyntax/service/operation/OperationHandlerTest.java
@@ -13,7 +13,7 @@ class OperationHandlerTest {
 
     @AfterEach
     void afterEach() {
-        Storage.storage.clear();
+        Storage.getStorage().clear();
     }
 
     @Test
@@ -23,7 +23,7 @@ class OperationHandlerTest {
                 new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 15);
         operationHandler.handle(balanceTransaction1);
         int exceptedBananaBalance = 15;
-        int actualBananaBalance = Storage.storage.get("banana");
+        int actualBananaBalance = Storage.getStorage().get("banana");
         assertEquals(exceptedBananaBalance, actualBananaBalance);
     }
 
@@ -37,31 +37,31 @@ class OperationHandlerTest {
     @Test
     void purchaseHandler_validData_ok() {
         operationHandler = new PurchaseOperationHandlerImpl();
-        Storage.storage.put("banana", 20);
+        Storage.getStorage().put("banana", 20);
         FruitTransaction purchaseTransactions1 =
                 new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 10);
         operationHandler.handle(purchaseTransactions1);
         int exceptedBananaBalance = 10;
-        int actualBananaBalance = Storage.storage.get("banana");
+        int actualBananaBalance = Storage.getStorage().get("banana");
         assertEquals(exceptedBananaBalance,actualBananaBalance);
     }
 
     @Test
     void purchaseHandler_buyAll_ok() {
         operationHandler = new PurchaseOperationHandlerImpl();
-        Storage.storage.put("banana", 20);
+        Storage.getStorage().put("banana", 20);
         FruitTransaction fruitTransaction =
                 new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 20);
         operationHandler.handle(fruitTransaction);
         int exceptedBalance = 0;
-        int actualBalance = Storage.storage.get("banana");
+        int actualBalance = Storage.getStorage().get("banana");
         assertEquals(exceptedBalance, actualBalance);
     }
 
     @Test
     void purchaseHandler_buyNotValidQuantity_notOk() {
         operationHandler = new PurchaseOperationHandlerImpl();
-        Storage.storage.put("banana", 20);
+        Storage.getStorage().put("banana", 20);
         FruitTransaction fruitTransaction =
                 new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 21);
         assertThrows(RuntimeException.class, () -> operationHandler.handle(fruitTransaction));
@@ -70,19 +70,19 @@ class OperationHandlerTest {
     @Test
     void returnHandler_validData_Ok() {
         operationHandler = new ReturnOperationHandlerImpl();
-        Storage.storage.put("banana", 20);
+        Storage.getStorage().put("banana", 20);
         FruitTransaction fruitTransaction =
                 new FruitTransaction(FruitTransaction.Operation.RETURN, "banana", 10);
         operationHandler.handle(fruitTransaction);
         int exceptedBalance = 30;
-        int actualBalance = Storage.storage.get("banana");
+        int actualBalance = Storage.getStorage().get("banana");
         assertEquals(exceptedBalance, actualBalance);
     }
 
     @Test
     void returnHandler_notValidData_notOk() {
         operationHandler = new ReturnOperationHandlerImpl();
-        Storage.storage.put("banana", 20);
+        Storage.getStorage().put("banana", 20);
         FruitTransaction fruitTransaction =
                 new FruitTransaction(FruitTransaction.Operation.RETURN, "banana", -1);
         assertThrows(RuntimeException.class, () -> operationHandler.handle(fruitTransaction));
@@ -91,19 +91,19 @@ class OperationHandlerTest {
     @Test
     void supplyHandler_validData_Ok() {
         operationHandler = new SupplyOperationHandlerImpl();
-        Storage.storage.put("banana", 20);
+        Storage.getStorage().put("banana", 20);
         FruitTransaction fruitTransaction =
                 new FruitTransaction(FruitTransaction.Operation.RETURN, "banana", 10);
         operationHandler.handle(fruitTransaction);
         int exceptedBalance = 30;
-        int actualBalance = Storage.storage.get("banana");
+        int actualBalance = Storage.getStorage().get("banana");
         assertEquals(exceptedBalance, actualBalance);
     }
 
     @Test
     void supplyHandler_notValidData_notOk() {
         operationHandler = new SupplyOperationHandlerImpl();
-        Storage.storage.put("banana", 20);
+        Storage.getStorage().put("banana", 20);
         FruitTransaction fruitTransaction =
                 new FruitTransaction(FruitTransaction.Operation.RETURN, "banana", -1);
         assertThrows(RuntimeException.class, () -> operationHandler.handle(fruitTransaction));

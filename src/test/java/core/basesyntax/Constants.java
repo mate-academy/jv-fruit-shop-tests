5fa069db0ee0869b9a7ec1bce8eb2f0587fd9592
artificialLeo <horkovenko.k@gmail.com>
2023-10-31 18:15:07 +0200

    done tests

---
+++ src/test/java/core/basesyntax/Constants.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+public class Constants {
+    public static final String BALANCE_SHORTHAND = "b";
+    public static final String SUPPLY_SHORTHAND = "s";
+    public static final String PURCHASE_SHORTHAND = "p";
+    public static final String RETURN_SHORTHAND = "r";
+    public static final String INVALID_CODE = "invalid";
+    public static final String APPLE = "apple";
+    public static final String ORANGE = "orange";
+    public static final String BANANA = "banana";
+    public static final String MANGO = "mango";
+    public static final String GRAPES = "grapes";
+    public static final String PINEAPPLE = "pineapple";
+    public static final String CHERRY = "cherry";
+    public static final String EXISTING_PATH = "src/test/resources/input.csv";
+    public static final String EXISTING_PATH_OUTPUT = "src/test/resources/output.csv";
+    public static final String INVALID_DATA = "src/test/resources/invalid.csv";
+    public static final String ERROR_PATH = "input.csv";
+    public static final String CSV_DELIMITER = ",";
+
+}

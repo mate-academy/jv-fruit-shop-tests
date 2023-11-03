5c0601695eb70fa9ed63f66af9a2f93777ce9516
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-11-03 12:41:48 +0200

    changed way to equal input data

--- src/test/java/core/basesyntax/service/FileReaderServiceTest.java
+++ src/test/java/core/basesyntax/service/FileReaderServiceTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FileReaderServiceTest {
     private static final String CORRECT_INPUT_FILE_NAME = "src/main/resources/input.csv";
     private static final String WRONG_INPUT_FILE_NAME = "53hsd59.csv";
+    private static final String LINE_SEPARATOR = "\n";
     private static FileReaderService fileReaderService;
 
     @BeforeAll
@@ -17,19 +21,19 @@ public class FileReaderServiceTest {
 
     @Test
     public void read_correctInputData_Ok() {
-        String expected = """
-                type,fruit,quantity
-                b,banana,20
-                b,apple,100
-                s,banana,100
-                p,banana,13
-                r,apple,10
-                p,apple,20
-                p,banana,5
-                s,banana,50
-                """;
+        List<String> expected = List.of(
+                "type,fruit,quantity",
+                "b,banana,20",
+                "b,apple,100",
+                "s,banana,100",
+                "p,banana,13",
+                "r,apple,10",
+                "p,apple,20",
+                "p,banana,5",
+                "s,banana,50");
         System.out.println(expected);
-        String actual = fileReaderService.readInputData(CORRECT_INPUT_FILE_NAME);
+        String inputData = fileReaderService.readInputData(CORRECT_INPUT_FILE_NAME);
+        List<String> actual = Arrays.asList(inputData.split(LINE_SEPARATOR));
         Assertions.assertEquals(expected, actual);
     }
 

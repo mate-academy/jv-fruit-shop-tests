eba5f9dd4818f1da4be6ec98f0f4130ff30e88d0
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-11-07 11:51:11 +0200

    added deletion file method

--- src/test/java/core/basesyntax/service/impl/CsvWriterServiceTest.java
+++ src/test/java/core/basesyntax/service/impl/CsvWriterServiceTest.java
@@ -4,20 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.service.WriterService;
+import java.io.File;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CsvWriterServiceTest {
     private static final String VALID_FILE_PATH = "src/test/resources/test-report.csv";
-
     private static final String REPORT = "type, quantity" + System.lineSeparator()
             + "banana, 100" + System.lineSeparator()
             + "apple, 100" + System.lineSeparator();
+    private static File file;
     private WriterService writer;
 
     @BeforeEach
     void setUp() {
         writer = new CsvWriterService();
+        file = new File(VALID_FILE_PATH);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (file.exists()) {
+            file.delete();
+        }
     }
 
     @Test

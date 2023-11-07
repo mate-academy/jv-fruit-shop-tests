99c4743c1ab93642d2eeb22fa7723bafcdc55d32
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-11-07 09:33:26 +0200

    For now use constants

--- src/test/java/core/basesyntax/service/parser/FruitTransactionDataParserImplTest.java
+++ src/test/java/core/basesyntax/service/parser/FruitTransactionDataParserImplTest.java
@@ -12,7 +12,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class FruitTransactionDataParserImplTest {
+    public static final String FOURTH_FRUIT_NAME = "Lemon";
+    public static final int FIRST_FRUIT_QUANTITY = 10;
+    public static final int SECOND_FRUIT_QUANTITY = 15;
+    public static final int THIRD_FRUIT_QUANTITY = 20;
+    public static final int FOURTH_FRUIT_QUANTITY = 25;
     private static final int AMOUNT_OF_FRUIT_TRANSACTIONS = 4;
+    public static final String FIRST_FRUIT_NAME = "Apple";
+    public static final String SECOND_FRUIT_NAME = "Banana";
+    public static final String THIRD_FRUIT_NAME = "Peach";
+    public static final int FIRST_FRUIT_INDEX = 0;
+    public static final int SECOND_FRUIT_INDEX = 1;
+    public static final int THIRD_FRUIT_INDEX = 2;
+    public static final int FOURTH_FRUIT_INDEX = 3;
     private static List<String> data;
     private static List<FruitTransaction> transactions;
     private static DataParser parser;
@@ -30,21 +42,21 @@ class FruitTransactionDataParserImplTest {
 
         assertEquals(AMOUNT_OF_FRUIT_TRANSACTIONS, transactions.size());
 
-        assertEquals(Operation.BALANCE, transactions.get(0).getOperation());
-        assertEquals("Apple", transactions.get(0).getFruit());
-        assertEquals(10, transactions.get(0).getQuantity());
+        assertEquals(Operation.BALANCE, transactions.get(FIRST_FRUIT_INDEX).getOperation());
+        assertEquals(FIRST_FRUIT_NAME, transactions.get(FIRST_FRUIT_INDEX).getFruit());
+        assertEquals(FIRST_FRUIT_QUANTITY, transactions.get(FIRST_FRUIT_INDEX).getQuantity());
 
-        assertEquals(Operation.SUPPLY, transactions.get(1).getOperation());
-        assertEquals("Banana", transactions.get(1).getFruit());
-        assertEquals(15, transactions.get(1).getQuantity());
+        assertEquals(Operation.SUPPLY, transactions.get(SECOND_FRUIT_INDEX).getOperation());
+        assertEquals(SECOND_FRUIT_NAME, transactions.get(SECOND_FRUIT_INDEX).getFruit());
+        assertEquals(SECOND_FRUIT_QUANTITY, transactions.get(SECOND_FRUIT_INDEX).getQuantity());
 
-        assertEquals(Operation.PURCHASE, transactions.get(2).getOperation());
-        assertEquals("Peach", transactions.get(2).getFruit());
-        assertEquals(20, transactions.get(2).getQuantity());
+        assertEquals(Operation.PURCHASE, transactions.get(THIRD_FRUIT_INDEX).getOperation());
+        assertEquals(THIRD_FRUIT_NAME, transactions.get(THIRD_FRUIT_INDEX).getFruit());
+        assertEquals(THIRD_FRUIT_QUANTITY, transactions.get(THIRD_FRUIT_INDEX).getQuantity());
 
-        assertEquals(Operation.RETURN, transactions.get(3).getOperation());
-        assertEquals("Lemon", transactions.get(3).getFruit());
-        assertEquals(25, transactions.get(3).getQuantity());
+        assertEquals(Operation.RETURN, transactions.get(FOURTH_FRUIT_INDEX).getOperation());
+        assertEquals(FOURTH_FRUIT_NAME, transactions.get(FOURTH_FRUIT_INDEX).getFruit());
+        assertEquals(FOURTH_FRUIT_QUANTITY, transactions.get(FOURTH_FRUIT_INDEX).getQuantity());
     }
 
     @Test

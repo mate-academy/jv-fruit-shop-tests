1aeef039cfe7885e6dc1b7c0a010962945f98779
Dmytro Potapov <potapovds1993@gmail.com>
2023-11-07 19:42:33 +0200

    copyed files

---
+++ src/main/java/core/basesyntax/service/OperationProcess.java
@@ -0,0 +1,7 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.FruitTransaction;
+import java.util.List;
+public interface OperationProcess {
+    void processTransaction(List<FruitTransaction> fruitTransactions);
+}

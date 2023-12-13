27ba2620866ce3909b48b79e7cb7ff2f3fbb5a18
Oleh Shvets <olehpost69@gmail.com>
2023-12-13 19:29:22 +0300

    fourthCommit

--- src/test/java/core/basesyntax/service/ReportCreatorTest.java
+++ src/test/java/core/basesyntax/service/ReportCreatorTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ReportCreatorTest {
+    private static final String APPLE = "Apple";
+    private static final String BANANA = "Banana";
+    private static final String REPORT_HEADER = "fruit,quantity\n";
     private ReportCreator reportCreator;
 
     @BeforeEach
@@ -17,10 +20,10 @@ public class ReportCreatorTest {
 
     @Test
     public void createReport_WithData_Ok() {
-        FruitStorage.FRUITS.put("Apple", 10);
-        FruitStorage.FRUITS.put("Banana", 20);
+        FruitStorage.FRUITS.put(APPLE, 10);
+        FruitStorage.FRUITS.put(BANANA, 20);
         String report = reportCreator.createReport();
-        String expectedReport = "fruit,quantity\nApple,10\nBanana,20";
+        String expectedReport = REPORT_HEADER + APPLE + ",10\n" + BANANA + ",20";
 
         report = report.replaceAll("\\r\\n", "\n");
         expectedReport = expectedReport.replaceAll("\\r\\n", "\n");

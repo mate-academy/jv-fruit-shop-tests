cd37ba95406465b6a96f1410f16663669b3d6439
AdamMudrak <mudrets2000@gmail.com>
2024-01-14 12:44:27 +0200

    Fixed wrong Operation TYPE in FruitTransaction

--- src/test/java/core/basesyntax/services/handlers/PurchaseOperationHandlerTest.java
+++ src/test/java/core/basesyntax/services/handlers/PurchaseOperationHandlerTest.java
@@ -24,15 +24,15 @@ public class PurchaseOperationHandlerTest {
     private static final int EXPECTED_RESULT_QUANTITY_OF_BANANA = 90;
 
     private static final FruitTransaction ORANGE_FRUITTRANSACTION =
-            new FruitTransaction(FruitTransaction.Operation.BALANCE, ORANGE, 10);
+            new FruitTransaction(FruitTransaction.Operation.PURCHASE, ORANGE, 10);
     private static final FruitTransaction APPLE_FRUITTRANSACTION =
-            new FruitTransaction(FruitTransaction.Operation.BALANCE, APPLE, 10);
+            new FruitTransaction(FruitTransaction.Operation.PURCHASE, APPLE, 10);
     private static final FruitTransaction BANANA_FRUITTRANSACTION =
-            new FruitTransaction(FruitTransaction.Operation.BALANCE, BANANA, 10);
+            new FruitTransaction(FruitTransaction.Operation.PURCHASE, BANANA, 10);
     private static final FruitTransaction PURCHASE_NEGATIVE_QUANTITY =
-            new FruitTransaction(FruitTransaction.Operation.BALANCE, BANANA, -1);
+            new FruitTransaction(FruitTransaction.Operation.PURCHASE, BANANA, -1);
     private static final FruitTransaction PURCHASE_WITH_NEGATIVE_RESULT =
-            new FruitTransaction(FruitTransaction.Operation.BALANCE, BANANA, 1000);
+            new FruitTransaction(FruitTransaction.Operation.PURCHASE, BANANA, 1000);
     private static final String EXPECTED_EXCEPTION_MESSAGE_NEGATIVE_NUMBER_PURCHASE =
             "Purchase operation value for banana should've been positive but was " +
                     PURCHASE_NEGATIVE_QUANTITY.getQuantity();

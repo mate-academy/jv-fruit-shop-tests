a24e1c327dbfd52b27faaf155a512bd1d70557e0
AdamMudrak <mudrets2000@gmail.com>
2024-01-14 12:50:35 +0200

    Add comment

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -9,7 +9,7 @@ public class Storage {
 
     public static Map<String, Integer> getFruits() {
         return fruits;
-    }
+    }//need this for easier testing
     public static Set<Map.Entry<String, Integer>> getFruitsEntrySet() {
         return fruits.entrySet();
     }

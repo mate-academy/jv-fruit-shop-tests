01ab83445b2c3cfbf106357e3470e82485b7d937
Yevhen Nedashkivskyi <kesh040593@ukr.net>
2024-01-20 01:02:28 +0200

    Error correction

--- src/test/java/core/basesyntax/service/file/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/service/file/FileReaderImplTest.java
@@ -23,22 +23,22 @@ class FileReaderImplTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = """
-            type,fruit,quantity\r
-            b,banana,20\r
-            b,apple,100\r
-            s,banana,100\r
-            p,banana,13\r
-            r,apple,10\r
-            p,apple,20\r
-            p,banana,5\r
-            s,banana,50"""
+    @ValueSource(strings = {
+            "type,fruit,quantity\r\n"
+                    + "b,banana,20\r\n"
+                    + "b,apple,100\r\n"
+                    + "s,banana,100\r\n"
+                    + "p,banana,13\r\n"
+                    + "r,apple,10\r\n"
+                    + "p,apple,20\r\n"
+                    + "p,banana,5\r\n"
+                    + "s,banana,50"}
     )
     void readFromFile_isOk(String expectedOutputString) {
         String actualOutputString =
                 fileReader.readFromFile(TEST_INPUT_FILE_PATH);
-        assertEquals(expectedOutputString, actualOutputString, "Output string must be:\r\n"
-                + expectedOutputString + "\r\n" + "but was:\r\n"
+        assertEquals(expectedOutputString, actualOutputString, "Output string must be:\n"
+                + expectedOutputString + "\n" + "but was:\n"
                 + actualOutputString);
     }
 

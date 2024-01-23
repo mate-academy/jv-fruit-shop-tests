7d81136533ffeb41e9473ec6d640028f03d9806f
AdamMudrak <mudrets2000@gmail.com>
2024-01-24 01:04:23 +0200

    Add necessary import

--- src/test/java/core/basesyntax/services/fileprocessing/DataSplitterImplTest.java
+++ src/test/java/core/basesyntax/services/fileprocessing/DataSplitterImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import core.basesyntax.services.fileprocessing.impl.DataSplitterImpl;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;

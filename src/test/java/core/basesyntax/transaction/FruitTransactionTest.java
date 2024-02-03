2606b6a158e4d852c06c2ea4a7295ea40f7a0f2f
Serhii Kopervas <sergkopervas@gmail.com>
2024-02-03 20:47:06 +0100

    FruitStoreTests, version 1.0.4 Correction errors. Removing redundant code.

--- src/test/java/core/basesyntax/transaction/FruitTransactionTest.java
+++ src/test/java/core/basesyntax/transaction/FruitTransactionTest.java
@@ -5,10 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
 
 class FruitTransactionTest {
+    private final FruitTransaction transaction = new FruitTransaction(null, null, 0);
+
+    @Test
+    void testGetOperationFromNull_Ok() {
+        assertDoesNotThrow(() -> transaction.getOperation());
+    }
+
+    @Test
+    void testGetFruitFromNull_Ok() {
+        assertDoesNotThrow(() -> transaction.getFruit());
+    }
+
+    @Test
+    void testGetQuantityFromNull_Ok() {
+        assertDoesNotThrow(() -> transaction.getQuantity());
+    }
 
     @Test
     void testToStringWithNullValue() {
-        FruitTransaction transaction = new FruitTransaction(null, null, 0);
+        //FruitTransaction transaction = new FruitTransaction(null, null, 0);
         assertDoesNotThrow(() -> transaction.toString());
     }
 }

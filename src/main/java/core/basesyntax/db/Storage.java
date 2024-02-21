73cfb1583ecc988916eb2097433341f402d18371
Kacper <kacperkukula97@gmail.com>
2024-02-21 17:10:31 +0100

    Second Attempt. Removed some redundant test cases and fixed constants

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    private static Map<String, Integer> fruitStorage = new HashMap<>();
+    private static final Map<String, Integer> fruitStorage = new HashMap<>();
 
     public static Map<String, Integer> getFruitStorage() {
         return fruitStorage;
--- src/test/java/core/basesyntax/service/impl/DataConverterServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/DataConverterServiceImplTest.java
@@ -12,42 +12,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.NoSuchElementException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DataConverterServiceImplTest {
-    private static final List<String> operationsValid1 =
+    private static final List<String> OPERATIONS_SINGLE_FRUIT =
             readFileContents("src/test/resources/operations_valid_1.csv");
-    private static final List<String> operationsValid2 =
+    private static final List<String> OPERATIONS_MULTIPLE_FRUITS =
             readFileContents("src/test/resources/operations_valid_2.csv");
-    private static final List<String> operationsEmpty =
+    private static final List<String> OPERATIONS_EMPTY =
             readFileContents("src/test/resources/operations_empty.csv");
-    private static final List<String> operationsMixedFields =
+    private static final List<String> OPERATIONS_MIXED_FIELDS =
             readFileContents("src/test/resources/operations_mixed_fields.csv");
-    private static final List<String> operationsRandomFields =
+    private static final List<String> OPERATIONS_RANDOM_FIELDS =
             readFileContents("src/test/resources/operations_random_text.csv");
-    private static final List<String> operationsWrongRegex =
+    private static final List<String> OPERATIONS_WRONG_REGEX =
             readFileContents("src/test/resources/operations_random_text.csv");
-    private static DataConverterService dataConverter = new DataConverterServiceImpl();
-
-    private FruitTransaction transaction1;
-    private FruitTransaction transaction2;
-    private FruitTransaction transaction3;
-    private FruitTransaction transaction4;
-    private List<FruitTransaction> transactions;
-
-    @BeforeEach
-    void setUp() {
-        transaction1 = new FruitTransaction();
-        transaction2 = new FruitTransaction();
-        transaction3 = new FruitTransaction();
-        transaction4 = new FruitTransaction();
-
-        transactions = List.of(transaction1, transaction2, transaction3, transaction4);
-    }
+    private static final DataConverterService DATA_CONVERTER = new DataConverterServiceImpl();
 
     @Test
     void getListOfTransactions_validOperationsSingleFruit_Ok() {
+        FruitTransaction transaction1 = new FruitTransaction();
+        FruitTransaction transaction2 = new FruitTransaction();
+        FruitTransaction transaction3 = new FruitTransaction();
+        FruitTransaction transaction4 = new FruitTransaction();
+        List<FruitTransaction> transactions =
+                List.of(transaction1, transaction2, transaction3, transaction4);
+
         transaction1.setOperation(FruitTransaction.Operation.BALANCE);
         transaction1.setQuantity(100);
         transaction1.setFruit("apple");
@@ -64,7 +54,8 @@ class DataConverterServiceImplTest {
         transaction4.setQuantity(5);
         transaction4.setFruit("apple");
 
-        List<FruitTransaction> actual = dataConverter.getListOfTransactions(operationsValid1);
+        List<FruitTransaction> actual =
+                DATA_CONVERTER.getListOfTransactions(OPERATIONS_SINGLE_FRUIT);
 
         for (int i = 0; i < 4; i++) {
             assertEquals(transactions.get(i), actual.get(i));
@@ -73,6 +64,13 @@ class DataConverterServiceImplTest {
 
     @Test
     void getListOfTransactions_validOperationsManyFruits_Ok() {
+        FruitTransaction transaction1 = new FruitTransaction();
+        FruitTransaction transaction2 = new FruitTransaction();
+        FruitTransaction transaction3 = new FruitTransaction();
+        FruitTransaction transaction4 = new FruitTransaction();
+        List<FruitTransaction> transactions =
+                List.of(transaction1, transaction2, transaction3, transaction4);
+
         transaction1.setOperation(FruitTransaction.Operation.BALANCE);
         transaction1.setQuantity(20);
         transaction1.setFruit("banana");
@@ -89,7 +87,8 @@ class DataConverterServiceImplTest {
         transaction4.setQuantity(5990);
         transaction4.setFruit("pear");
 
-        List<FruitTransaction> actual = dataConverter.getListOfTransactions(operationsValid2);
+        List<FruitTransaction> actual =
+                DATA_CONVERTER.getListOfTransactions(OPERATIONS_MULTIPLE_FRUITS);
 
         for (int i = 0; i < 4; i++) {
             assertEquals(transactions.get(i), actual.get(i));
@@ -98,7 +97,8 @@ class DataConverterServiceImplTest {
 
     @Test
     void getListOfTransactions_emptyOperations_Ok() {
-        List<FruitTransaction> actual = dataConverter.getListOfTransactions(operationsEmpty);
+        List<FruitTransaction> actual =
+                DATA_CONVERTER.getListOfTransactions(OPERATIONS_EMPTY);
 
         assertTrue(actual.isEmpty());
     }
@@ -106,19 +106,19 @@ class DataConverterServiceImplTest {
     @Test
     void getListOfTransactions_mixedFieldsOperations_NotOk() {
         assertThrows(NoSuchElementException.class,
-                () -> dataConverter.getListOfTransactions(operationsMixedFields));
+                () -> DATA_CONVERTER.getListOfTransactions(OPERATIONS_MIXED_FIELDS));
     }
 
     @Test
     void getListOfTransactions_wrongRegexOperations_NotOk() {
         assertThrows(IndexOutOfBoundsException.class,
-                () -> dataConverter.getListOfTransactions(operationsWrongRegex));
+                () -> DATA_CONVERTER.getListOfTransactions(OPERATIONS_WRONG_REGEX));
     }
 
     @Test
     void getListOfTransactions_randomTextOperations_NotOk() {
         assertThrows(IndexOutOfBoundsException.class,
-                () -> dataConverter.getListOfTransactions(operationsRandomFields));
+                () -> DATA_CONVERTER.getListOfTransactions(OPERATIONS_RANDOM_FIELDS));
     }
 
     private static List<String> readFileContents(String path) {

7fa10d4319797147777f28418c350b89a4435f95
breakingtumble <yaroslavglavatsky@gmail.com>
2024-03-03 11:19:44 +0200

    fix: checkstyle fix

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -49,10 +49,16 @@ public class FruitTransaction {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         FruitTransaction that = (FruitTransaction) o;
-        return quantity == that.quantity && operation == that.operation && Objects.equals(fruitName, that.fruitName);
+        return quantity == that.quantity
+                && operation == that.operation
+                && Objects.equals(fruitName, that.fruitName);
     }
 
     @Override

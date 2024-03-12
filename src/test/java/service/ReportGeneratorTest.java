f1d799dc6fc7f462c7455e1fa8e5d15d452dd59a
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-03-12 15:20:20 +0200

    Add more tests

--- src/test/java/service/ReportGeneratorTest.java
+++ src/test/java/service/ReportGeneratorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class ReportGeneratorTest {
-    private static final Map<String, Integer> DATA = Map.of("test", 10,"test2",20);
+    private static final String HEADING = "fruit,quantity";
     private static ReportGenerator reportGenerator;
 
     @BeforeAll
@@ -16,9 +16,11 @@ class ReportGeneratorTest {
 
     @Test
     void generate_validReport_ok() {
-        String expected = "fruit,quantity" + System.lineSeparator()
-                + "test2,20" + System.lineSeparator()
-                + "test,10" + System.lineSeparator();
-        Assertions.assertEquals(expected, reportGenerator.generate(DATA));
+        Map<String, Integer> data = Map.of("test1", 10,"test2",20);
+        String test1 = "test1,10";
+        String test2 = "test2,20";
+        String report = reportGenerator.generate(data);
+        Assertions.assertTrue(report.contains(HEADING) && report.contains(test1)
+                        && report.contains(test2), "Report is not correct");
     }
 }

5e501dc6e11b0f1f62f94dc9bdba281702a85a84
Farion Oleksandr <sashafarion@gmail.com>
2024-03-14 22:03:21 +0200

    added JUnit tests

--- src/test/java/core/basesyntax/FileReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/FileReaderServiceImplTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.service.ReaderService;
 import core.basesyntax.service.impl.FileReaderServiceImpl;
-import java.io.IOException;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 

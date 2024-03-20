6a6719f8c0ce974de01b303700b09e7828a1c3d1
makaroshyna <katya23225@gmail.com>
2024-03-20 12:06:13 +0200

    Place Path.of() directly.

--- src/main/java/core/basesyntax/service/impl/DataReaderCsv.java
+++ src/main/java/core/basesyntax/service/impl/DataReaderCsv.java
@@ -9,11 +9,9 @@ import java.util.List;
 public class DataReaderCsv implements DataReader {
     @Override
     public List<String> read(String path) {
-        Path filePath = Path.of(path);
         List<String> strings;
-
         try {
-            strings = Files.readAllLines(filePath);
+            strings = Files.readAllLines(Path.of(path));
         } catch (IOException e) {
             throw new RuntimeException(
                     "Can't find the file by path: " + path, e);

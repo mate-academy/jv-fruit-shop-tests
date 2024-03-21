a750341d1011fb47ea160b3843733dac4c9fd9b4
Valentyn Sharshon <woosay333@gmail.com>
2024-03-21 13:56:43 +0200

    added check for null input file path

--- src/main/java/core/basesyntax/service/impl/CsvFileReader.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileReader.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 
 public class CsvFileReader implements FileReader {
     @Override
     public List<String> readData(String filePath) {
+        Objects.requireNonNull(filePath);
         try {
             return Files.readAllLines(Paths.get(filePath));
         } catch (IOException ex) {

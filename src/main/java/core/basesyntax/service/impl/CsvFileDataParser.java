092e4499d593487bceb822dd70cc9546e879573c
Valentyn Sharshon <woosay333@gmail.com>
2024-03-21 14:56:12 +0200

    added input line validate

--- src/main/java/core/basesyntax/service/impl/CsvFileDataParser.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileDataParser.java
@@ -9,14 +9,24 @@ public class CsvFileDataParser implements core.basesyntax.service.FileDataParser
     private static final int OPERATION_INDEX = 0;
     private static final int QUANTITY_INDEX = 2;
     private static final String REGEX_COMMA_SEPARATOR = ",";
+    public static final String REGEX_INPUT_LINE_VALIDATE = "^[bspr],\\w+,\\d+$";
 
     @Override
     public List<FruitTransaction> parseData(List<String> data) {
         return data.stream()
                 .skip(HEADERS_LINE_NUMBER)
-                .map(line -> line.split(REGEX_COMMA_SEPARATOR))
+                .map(line -> {
+                    validateLine(line);
+                    return line.split(REGEX_COMMA_SEPARATOR);
+                })
                 .map(this::create)
                 .toList();
+    }
+
+    private void validateLine(String line) {
+        if (!line.matches(REGEX_INPUT_LINE_VALIDATE)) {
+            throw new IllegalArgumentException(String.format("Invalid input line %s", line));
+        }
     }
 
     private FruitTransaction create(String[] data) {

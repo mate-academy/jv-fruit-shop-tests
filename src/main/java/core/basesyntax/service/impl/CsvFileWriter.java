55d4a7cc0079561c98d7b0a9af96c0260926af18
Valentyn Sharshon <woosay333@gmail.com>
2024-03-22 00:33:45 +0200

    added some validation

--- src/main/java/core/basesyntax/service/impl/CsvFileWriter.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriter.java
@@ -10,6 +10,12 @@ import java.nio.file.Paths;
 public class CsvFileWriter implements FileWriter {
     @Override
     public void writeData(String filePath, String data) {
+        if (filePath == null || filePath.isEmpty()) {
+            throw new IllegalArgumentException("Report path is null or empty");
+        }
+        if (data == null || data.isEmpty()) {
+            throw new IllegalArgumentException("Data is null or empty");
+        }
         try (var br = Files.newBufferedWriter(Paths.get(filePath), StandardCharsets.UTF_8)) {
             br.write(data);
         } catch (IOException ex) {

7be44a22da9a9e0c77c89e244f26d4ef23ba2514
Valentyn Sharshon <woosay333@gmail.com>
2024-03-21 13:57:44 +0200

    created CsvFileReaderTest test class

---
+++ src/test/java/core/basesyntax/service/impl/CsvFileReaderTest.java
@@ -0,0 +1,53 @@
+package core.basesyntax.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.exception.ReadFromFileException;
+import core.basesyntax.service.FileReader;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CsvFileReaderTest {
+    private final FileReader fileReader = new CsvFileReader();
+
+    @Test
+    void readData_FilePathIsNull_notOk() {
+        assertThrows(NullPointerException.class, () -> fileReader.readData(null));
+    }
+
+    @Test
+    void readData_InvalidFilePath_notOk() {
+        String pathToFile = "src/test/resources/non_existent_file.csv";
+        ReadFromFileException expected = assertThrows(ReadFromFileException.class,
+                () -> fileReader.readData(pathToFile));
+        assertEquals(String.format("Can`t read data from the file %s", pathToFile),
+                expected.getMessage());
+    }
+
+    @Test
+    void readData_FileIsEmpty_Ok() {
+        String pathToFile = "src/test/resources/empty_file.csv";
+        List<String> actual = fileReader.readData(pathToFile);
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    void readData_ReturnsListOfLinesFromInputFile_Ok() {
+        String pathToFile = "src/test/resources/valid_input.csv";
+        List<String> expected = List.of(
+                "type,fruit,quantity",
+                "b,banana,20",
+                "b,apple,100",
+                "s,banana,100",
+                "p,banana,13",
+                "r,apple,10",
+                "p,apple,20",
+                "p,banana,5",
+                "s,banana,50"
+        );
+        List<String> actual = fileReader.readData(pathToFile);
+        assertEquals(expected, actual);
+    }
+}

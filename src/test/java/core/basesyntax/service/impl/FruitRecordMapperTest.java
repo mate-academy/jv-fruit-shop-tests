ee78b7924875485bc12a8346a1e2e5c9bbbd3c5b
Artem Akymenko <akimenko.artem@gmail.com>
2024-03-22 15:24:06 +0200

    Removed unused import.

--- src/test/java/core/basesyntax/service/impl/FruitRecordMapperTest.java
+++ src/test/java/core/basesyntax/service/impl/FruitRecordMapperTest.java
@@ -8,7 +8,6 @@ import core.basesyntax.record.Operation;
 import core.basesyntax.record.Record;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FruitRecordMapperTest {

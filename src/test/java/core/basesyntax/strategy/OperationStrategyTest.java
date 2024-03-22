43af6477fea0807f32909b1c7b52e6dde08ba3d1
Valentyn Sharshon <woosay333@gmail.com>
2024-03-22 14:50:16 +0200

    created OperationStrategyTest test class

---
+++ src/test/java/core/basesyntax/strategy/OperationStrategyTest.java
@@ -0,0 +1,51 @@
+package core.basesyntax.strategy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.impl.StorageDaoImpl;
+import core.basesyntax.model.FruitTransaction.Operation;
+import core.basesyntax.strategy.impl.BalanceOperationHandler;
+import core.basesyntax.strategy.impl.PurchaseOperationHandler;
+import core.basesyntax.strategy.impl.ReturnOperationHandler;
+import core.basesyntax.strategy.impl.SupplyOperationHandler;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class OperationStrategyTest {
+    private static final StorageDao STORAGE_DAO = new StorageDaoImpl();
+    private static final Map<Operation, OperationHandler> STRATEGY_MAP = Map.of(
+            Operation.BALANCE, new BalanceOperationHandler(STORAGE_DAO),
+            Operation.PURCHASE, new PurchaseOperationHandler(STORAGE_DAO),
+            Operation.RETURN, new ReturnOperationHandler(STORAGE_DAO),
+            Operation.SUPPLY, new SupplyOperationHandler(STORAGE_DAO)
+    );
+    private static final OperationStrategy OPERATION_STRATEGY
+            = new OperationStrategy(STRATEGY_MAP);
+
+    @Test
+    void constructor_strategyMapIsNull_notOk() {
+        assertThrows(NullPointerException.class,
+                () -> new OperationStrategy(null));
+    }
+
+    @Test
+    void getHandlerFor_operationIsNull_notOk() {
+        assertThrows(NullPointerException.class,
+                () -> OPERATION_STRATEGY.getHandlerFor(null));
+    }
+
+    @Test
+    void getHandlerFor_returnsOperationHandler_ok() {
+        assertEquals(BalanceOperationHandler.class,
+                OPERATION_STRATEGY.getHandlerFor(Operation.BALANCE).getClass());
+        assertEquals(PurchaseOperationHandler.class,
+                OPERATION_STRATEGY.getHandlerFor(Operation.PURCHASE).getClass());
+        assertEquals(ReturnOperationHandler.class,
+                OPERATION_STRATEGY.getHandlerFor(Operation.RETURN).getClass());
+        assertEquals(SupplyOperationHandler.class,
+                OPERATION_STRATEGY.getHandlerFor(Operation.SUPPLY).getClass());
+    }
+
+}

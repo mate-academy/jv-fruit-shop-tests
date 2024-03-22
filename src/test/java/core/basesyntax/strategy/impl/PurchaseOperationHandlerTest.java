e0b684f99759da9a2897e537a6bce604000958e1
Valentyn Sharshon <woosay333@gmail.com>
2024-03-22 14:50:25 +0200

    created PurchaseOperationHandlerTest test class

---
+++ src/test/java/core/basesyntax/strategy/impl/PurchaseOperationHandlerTest.java
@@ -0,0 +1,64 @@
+package core.basesyntax.strategy.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.impl.StorageDaoImpl;
+import core.basesyntax.exception.NotEnoughProductAmountException;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.model.FruitTransaction.Operation;
+import core.basesyntax.storage.Storage;
+import core.basesyntax.strategy.OperationHandler;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class PurchaseOperationHandlerTest {
+    private static final StorageDao STORAGE_DAO =
+            new StorageDaoImpl();
+    private static final OperationHandler PURCHASE_OPERATION_HANDLER
+            = new PurchaseOperationHandler(STORAGE_DAO);
+    private static final String FRUIT_APPLE = "apple";
+    private static final FruitTransaction FRUIT_TRANSACTION
+            = new FruitTransaction(Operation.PURCHASE, FRUIT_APPLE, 101);
+    private static final FruitTransaction VALID_TRANSACTION
+            = new FruitTransaction(Operation.PURCHASE, FRUIT_APPLE, 99);
+
+    @AfterEach
+    void tearDown() {
+        Storage.fruits.clear();
+    }
+
+    @Test
+    void constructor_storageDaoIsNull_notOk() {
+        assertThrows(NullPointerException.class,
+                () -> new PurchaseOperationHandler(null));
+    }
+
+    @Test
+    void handle_fruitTransactionIsNull_notOk() {
+        assertThrows(NullPointerException.class,
+                () -> PURCHASE_OPERATION_HANDLER.handle(null));
+    }
+
+    @Test
+    void handle_fruitTransactionQuantityBiggerThenAvailableInStorage_notOk() {
+        Storage.fruits.put("apple", 100);
+        NotEnoughProductAmountException expected = assertThrows(
+                NotEnoughProductAmountException.class,
+                () -> PURCHASE_OPERATION_HANDLER.handle(FRUIT_TRANSACTION));
+        assertEquals(
+                String.format("No enough product %s amount: available - %d, needed - %d",
+                FRUIT_TRANSACTION.fruit(),
+                        Storage.fruits.get(FRUIT_APPLE),
+                        FRUIT_TRANSACTION.quantity()),
+                expected.getMessage());
+    }
+
+    @Test
+    void handle_putProductWithNewQuantityToTheStorage_notOk() {
+        Storage.fruits.put("apple", 100);
+        PURCHASE_OPERATION_HANDLER.handle(VALID_TRANSACTION);
+        assertEquals(1, Storage.fruits.get(FRUIT_APPLE));
+    }
+}

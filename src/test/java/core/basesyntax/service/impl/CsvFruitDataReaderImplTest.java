e2dea4bb4d0e72225373d0d0562264b3f0591d48
Mykola Skrypalov <skripalov1@gmail.com>
2024-03-24 01:50:20 +0200

    should be fixed now

--- src/test/java/core/basesyntax/service/impl/CsvFruitDataReaderImplTest.java
+++ src/test/java/core/basesyntax/service/impl/CsvFruitDataReaderImplTest.java
@@ -7,16 +7,15 @@ import core.basesyntax.exception.CantReadFromFileException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CsvFruitDataReaderImplTest {
-    private static final String CORRECT_FILE_PATH = "src\\resources\\file.csv";
-    private static final String INCORRECT_FILE_PATH = "src\\resources\\incorrect_path.csv";
-    private static final String EMPTY_FILE_PATH = "src\\resources\\emptyfile.csv";
+    private static final String CORRECT_FILE_PATH = "src\\test\\resources\\file.csv";
+    private static final String INCORRECT_FILE_PATH = "src\\test\\resources\\incorrect_path.csv";
+    private static final String EMPTY_FILE_PATH = "src\\test\\resources\\emptyfile.csv";
     private static final List<String> DATA_LIST = new ArrayList<>();
     private static final List<String> EMPTY_DATA_LIST = new ArrayList<>();
-    private static CsvFruitDataReaderImpl dataReader;
+    private static CsvFruitDataReaderImpl dataReader = new CsvFruitDataReaderImpl();
 
     @BeforeAll
     static void setUp() {
@@ -31,11 +30,6 @@ class CsvFruitDataReaderImplTest {
         DATA_LIST.add("s,banana,50");
     }
 
-    @BeforeEach
-    void setUpReader() {
-        dataReader = new CsvFruitDataReaderImpl();
-    }
-
     @Test
     void readData_FileExists_Ok() {
         List<String> dataFromMethod = dataReader.readData(CORRECT_FILE_PATH);
@@ -43,19 +37,19 @@ class CsvFruitDataReaderImplTest {
     }
 
     @Test
-    void readDataNonExistentFile_NotOk() {
+    void readData_NonExistentFile_NotOk() {
         assertThrows(CantReadFromFileException.class,
                 () -> dataReader.readData(INCORRECT_FILE_PATH));
     }
 
     @Test
-    void readDataFromEmptyFile_Ok() {
+    void readData_FromEmptyFile_Ok() {
         List<String> dataFromMethod = dataReader.readData(EMPTY_FILE_PATH);
         assertEquals(EMPTY_DATA_LIST, dataFromMethod);
     }
 
     @Test
-    void readDataWithNullParam_NotOk() {
+    void readData_WithNullParam_NotOk() {
         assertThrows(CantReadFromFileException.class,
                 () -> dataReader.readData(null));
     }

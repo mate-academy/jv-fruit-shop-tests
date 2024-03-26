e77b41ad3ccb4ec1f227ef09e14f0fb37de76941
Ivan Prystaia <greqit@gmail.com>
2024-03-26 12:48:28 +0200

    try to fix

--- src/test/java/core/basesyntax/MainTest.java
+++ src/test/java/core/basesyntax/MainTest.java
@@ -13,11 +13,11 @@ class MainTest {
     void main_createsOutputFileWithCorrectContent() {
         Main.main(new String[]{});
 
-        File outputFile = new File("src/main/resources/output_file.csv");
+        File outputFile = new File("test.csv");
         assertTrue(outputFile.exists());
 
         List<String> lines = new CsvReaderImpl()
-                .readDataFromFile("src/main/resources/output_file.csv");
+                .readDataFromFile("test.csv");
 
         assertEquals("fruit,quantity", String.join("\n", lines));
     }

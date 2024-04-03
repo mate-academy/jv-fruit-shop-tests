aa1fa6eb245672a9818f92273e286c56be89f5a9
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-04-03 13:18:38 +0300

    finished task

--- src/test/java/core/basesyntax/ProductsShopTest.java
+++ src/test/java/core/basesyntax/ProductsShopTest.java
@@ -17,6 +17,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class ProductsShopTest {
 
     private static DataReaderServiceImpl dataReaderService;

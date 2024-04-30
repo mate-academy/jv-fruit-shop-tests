a20c64450825dbed4d61301312ed6a1a1ebcda37
Nazar <nazarmo13@gmail.com>
2024-04-30 16:28:56 +0300

    added tests

--- src/test/java/core/basesyntax/strategy/activities/SupplyHandlerTest.java
+++ src/test/java/core/basesyntax/strategy/activities/SupplyHandlerTest.java
@@ -12,25 +12,38 @@ import org.junit.jupiter.api.Test;
 class SupplyHandlerTest {
     private static final String APPLE_FRUIT = "apple";
     private static final int APPLE_VALID_AMOUNT = 30;
+    OperationHandler supplyHandler;
     private Storage storage;
 
     @BeforeEach
     void setUp() {
+        supplyHandler = new SupplyHandler();
         storage = new StorageImpl();
     }
 
     @Test
     void executeTransaction_OperationTest_Ok() {
-        FruitTransaction transaction2 = new FruitTransaction(
+        FruitTransaction transaction = new FruitTransaction(
                 FruitTransaction.Operation.PURCHASE,
                 APPLE_FRUIT,
                 APPLE_VALID_AMOUNT
         );
-        storage.setValue("apple", 60);
-        OperationHandler purchaseHandler = new PurchaseHandler();
-        purchaseHandler.executeTransaction(transaction2);
+        supplyHandler.executeTransaction(transaction);
 
         assertEquals(APPLE_VALID_AMOUNT, storage.getValue(APPLE_FRUIT));
+    }
+
+    @Test
+    public void executeTransaction_withNullValue() {
+        FruitTransaction transaction = new FruitTransaction(
+                FruitTransaction.Operation.PURCHASE,
+                APPLE_FRUIT,
+                APPLE_VALID_AMOUNT
+        );
+        storage.setValue(APPLE_FRUIT, null);
+        supplyHandler.executeTransaction(transaction);
+
+        assertEquals(30, storage.getValue(APPLE_FRUIT));
     }
 
     @AfterEach

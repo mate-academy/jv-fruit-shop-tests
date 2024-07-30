471ee2ab3bf6f7c330dc72505c408b8044ec8911
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-07-30 19:37:28 +0200

    First solution

---
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -0,0 +1,24 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.ReportGenerator;
+import java.util.Map;
+
+public class ReportGeneratorImpl implements ReportGenerator {
+    private static final String HEADER = "fruit,quantity";
+
+    @Override
+    public String generateReport(Map<String, Integer> fruitsQuantityAfterDay) {
+        if (fruitsQuantityAfterDay == null) {
+            throw new RuntimeException("Map is null");
+        }
+        StringBuilder report = new StringBuilder();
+        report.append(HEADER);
+        for (Map.Entry<String, Integer> entry : fruitsQuantityAfterDay.entrySet()) {
+            report.append("\n");
+            report.append(entry.getKey());
+            report.append(",");
+            report.append(entry.getValue());
+        }
+        return report.toString();
+    }
+}

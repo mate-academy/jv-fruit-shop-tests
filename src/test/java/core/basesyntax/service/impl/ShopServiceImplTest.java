91893e97df3aee21c4a1f91ffea0c373d6b9b583
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-08-20 08:02:02 +0300

    ShopServiceImplTest.java

---
+++ src/test/java/core/basesyntax/service/impl/ShopServiceImplTest.java
@@ -0,0 +1,47 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.handler.OperationHandler;
+import core.basesyntax.handler.impl.BalanceOperation;
+import core.basesyntax.handler.impl.PurchaseOperation;
+import core.basesyntax.handler.impl.ReturnOperation;
+import core.basesyntax.handler.impl.SupplyOperation;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.ShopService;
+import core.basesyntax.strategy.OperationStrategy;
+import core.basesyntax.strategy.impl.OperationStrategyImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopServiceImplTest {
+    private ShopService shopService;
+    private Map<String, Integer> fruitStorage;
+
+    @BeforeEach
+    void setUp() {
+        fruitStorage = new HashMap<>();
+        Map<FruitTransaction.Operation, OperationHandler> operationHandlers = Map.of(
+                FruitTransaction.Operation.BALANCE, new BalanceOperation(fruitStorage),
+                FruitTransaction.Operation.PURCHASE, new PurchaseOperation(fruitStorage),
+                FruitTransaction.Operation.RETURN, new ReturnOperation(fruitStorage),
+                FruitTransaction.Operation.SUPPLY, new SupplyOperation(fruitStorage)
+        );
+        OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlers);
+        shopService = new ShopServiceImpl(operationStrategy);
+    }
+
+    @Test
+    void process_validTransactions_ok() {
+        List<FruitTransaction> transactions = List.of(
+                new FruitTransaction(FruitTransaction.Operation.BALANCE, "banana", 100),
+                new FruitTransaction(FruitTransaction.Operation.SUPPLY, "banana", 50),
+                new FruitTransaction(FruitTransaction.Operation.PURCHASE, "banana", 30),
+                new FruitTransaction(FruitTransaction.Operation.RETURN, "banana", 10)
+        );
+        shopService.process(transactions);
+        assertEquals(130, fruitStorage.get("banana"));
+    }
+}

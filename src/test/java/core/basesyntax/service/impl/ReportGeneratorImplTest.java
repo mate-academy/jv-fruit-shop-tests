bfeab128b7ea42334012baf59d1823ab45339471
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-08-20 08:01:23 +0300

    ReportGeneratorImplTest.java

---
+++ src/test/java/core/basesyntax/service/impl/ReportGeneratorImplTest.java
@@ -0,0 +1,27 @@
+package core.basesyntax.service.impl;
+
+import core.basesyntax.service.ReportGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportGeneratorImplTest {
+    private ReportGenerator reportGenerator;
+    private Map<String, Integer> fruitStorage;
+
+    @BeforeEach
+    void setUp() {
+        fruitStorage = new HashMap<>();
+        reportGenerator = new ReportGeneratorImpl(fruitStorage);
+    }
+
+    @Test
+    void createReport_validData_ok() {
+        fruitStorage.put("banana", 152);
+        fruitStorage.put("apple", 90);
+        String expectedReport = "fruit,quantity\nbanana,152\napple,90\n";
+        assertEquals(expectedReport, reportGenerator.createReport());
+    }
+}

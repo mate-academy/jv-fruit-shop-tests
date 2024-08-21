9107d425203f625eae2c984136e2caad6b023137
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-08-21 07:47:09 +0300

    BalanceOperationTest.java

---
+++ src/test/java/core/basesyntax/handler/impl/BalanceOperationTest.java
@@ -0,0 +1,28 @@
+package core.basesyntax.handler.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BalanceOperationTest {
+    private BalanceOperation balanceOperation;
+    private Map<String, Integer> fruitStorage;
+
+    @BeforeEach
+    void setUp() {
+        fruitStorage = new HashMap<>();
+        balanceOperation = new BalanceOperation(fruitStorage);
+    }
+
+    @Test
+    void apply_validTransaction_ok() {
+        FruitTransaction transaction = new FruitTransaction();
+        transaction.setFruit("banana");
+        transaction.setQuantity(100);
+        balanceOperation.apply(transaction);
+        assertEquals(100, fruitStorage.get("banana"));
+    }
+}

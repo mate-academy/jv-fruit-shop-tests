b2e96a008440fafb93edf1c0465871a7abdd1d38
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-08-21 07:48:12 +0300

    SupplyOperationTest.java

---
+++ src/test/java/core/basesyntax/handler/impl/SupplyOperationTest.java
@@ -0,0 +1,29 @@
+package core.basesyntax.handler.impl;
+
+import core.basesyntax.model.FruitTransaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplyOperationTest {
+    private SupplyOperation supplyOperation;
+    private Map<String, Integer> fruitStorage;
+
+    @BeforeEach
+    void setUp() {
+        fruitStorage = new HashMap<>();
+        supplyOperation = new SupplyOperation(fruitStorage);
+    }
+
+    @Test
+    void apply_validTransaction_ok() {
+        fruitStorage.put("banana", 100);
+        FruitTransaction transaction = new FruitTransaction();
+        transaction.setFruit("banana");
+        transaction.setQuantity(30);
+        supplyOperation.apply(transaction);
+        assertEquals(130, fruitStorage.get("banana"));
+    }
+}

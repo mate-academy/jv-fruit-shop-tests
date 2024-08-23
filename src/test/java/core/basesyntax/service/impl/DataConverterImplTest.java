1824d1b146278a8f08c6b151da2141b5253d3504
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-08-23 07:39:10 +0300

    DataConverterImplTest.java

--- src/test/java/core/basesyntax/service/impl/DataConverterImplTest.java
+++ src/test/java/core/basesyntax/service/impl/DataConverterImplTest.java
@@ -5,11 +5,6 @@ import core.basesyntax.service.DataConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 class DataConverterImplTest {
     private DataConverter dataConverter;
 

ae67f6ea223e410683126f818ebb0be43818b548
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-08-23 07:40:23 +0300

    PurchaseOperationTest.java

--- src/test/java/core/basesyntax/handler/impl/PurchaseOperationTest.java
+++ src/test/java/core/basesyntax/handler/impl/PurchaseOperationTest.java
@@ -4,11 +4,6 @@ import core.basesyntax.model.FruitTransaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class PurchaseOperationTest {
     private PurchaseOperation purchaseOperation;
     private Map<String, Integer> fruitStorage;

4026aa2d4e79f40f82a71360e9d31281805132f4
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-09 12:09:56 +0300

    ReportGeneratorImplTest.java

--- src/test/java/core/basesyntax/service/impl/ReportGeneratorImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportGeneratorImplTest.java
@@ -3,11 +3,9 @@ package core.basesyntax.service.impl;
 import core.basesyntax.service.ReportGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ReportGeneratorImplTest {
     private ReportGenerator reportGenerator;
@@ -24,6 +22,7 @@ class ReportGeneratorImplTest {
         fruitStorage.put("banana", 152);
         fruitStorage.put("apple", 90);
         String expectedReport = "fruit,quantity\nbanana,152\napple,90\n";
-        assertEquals(expectedReport, reportGenerator.createReport());
+        String actualReport = reportGenerator.createReport();
+        assertEquals(expectedReport, actualReport);
     }
 }

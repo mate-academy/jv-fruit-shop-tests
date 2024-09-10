14222370dae427908d493fbc67144016fa143855
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-10 13:05:41 +0300

    Implemented test methods for FileReader

--- src/test/java/core/basesyntax/service/fileservice/FileReaderServiceImplTest.java
+++ src/test/java/core/basesyntax/service/fileservice/FileReaderServiceImplTest.java
@@ -1,4 +1,41 @@
+package core.basesyntax.service.fileservice;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FileReaderServiceImplTest {
-  
+    private static final String PATH_TO_FILE = "src/test/resources/fileToReadTest.csv";
+    private static final String INVALID_PATH_TO_FILE = "src/test/name.csv";
+    private static final String SEPARATOR = "\n";
+
+    private FileReaderService fileReaderService;
+
+    @BeforeEach
+    void setUp() {
+        fileReaderService = new FileReaderServiceImpl();
+    }
+
+    @Test
+    void readValidFile_ok() {
+        String expected = """
+                type,fruit,quantity
+                r,apple,10
+                p,apple,20
+                p,banana,5
+                s,banana,50
+                """;
+        List<String> actual = fileReaderService.readFromFile(PATH_TO_FILE);
+        for (int i = 0; i < actual.size(); i++) {
+            assertEquals(expected.split(SEPARATOR)[i], actual.get(i));
+        }
+    }
+
+    @Test
+    void readInvalidFile_notOk() {
+        assertThrows(RuntimeException.class, () -> fileReaderService.readFromFile(INVALID_PATH_TO_FILE));
+    }
 }

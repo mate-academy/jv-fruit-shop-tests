ba99c9ec78bb218574b762597dc1e8edba069c71
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-10 13:25:36 +0300

    Created custom exception to throw if user enters incorrect data

---
+++ src/main/java/core/basesyntax/exception/InvalidFormatException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class InvalidFormatException extends RuntimeException {
+    public InvalidFormatException(String message) {
+        super(message);
+    }
+}

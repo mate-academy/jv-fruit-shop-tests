b5327d9c55a8b67883a39606b749df6b0373efee
oKukHtina <olakukhtina95@gmail.com>
2024-09-14 16:27:13 +0300

    fixed code, added tests for handler/transactions

--- src/test/java/core/basesyntax/processing/RawDataProcessorTest.java
+++ src/test/java/core/basesyntax/processing/RawDataProcessorTest.java
@@ -1,9 +1,6 @@
 package core.basesyntax.processing;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import core.basesyntax.enums.Operation;
-import core.basesyntax.exceptions.NegativeFruitBalanceException;
 import core.basesyntax.handler.TransactionHandler;
 import core.basesyntax.handler.impl.BalanceTransactionHandler;
 import core.basesyntax.handler.impl.PurchaseTransactionHandler;
@@ -17,22 +14,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RawDataProcessorTest {
-    private static final int ZERO_INDEX_POSITION = 0;
-    private static final int FIRST_INDEX_POSITION = 1;
-    private static final int SECOND_INDEX_POSITION = 2;
-    private static final int THIRD_INDEX_POSITION = 3;
-    private static final int FOURTH_INDEX_POSITION = 4;
-    private static final int SEVENTH_INDEX_POSITION = 7;
     private static final int QUANTITY_EXAMPLE = 20;
     private static final String FRUIT_BANANA = "banana";
     private static final String FRUIT_APPLE = "apple";
     private static final String FILE_TO_READ =
             "src/test/resources/db/ReadTransactionTest";
-    private static final String FILE_WITH_NEGATIVE_BALANCE =
-            "src/test/resources/db/fileWithNegativeBalance";
-    private static final String FILE_WITH_DIFFERENT_TYPES =
-            "src/test/resources/db/ReadTransactionWithDifferentTypes";
-
     private CsvFileReader fileReader;
     private RawDataProcessor rawDataProcessor;
 
@@ -59,45 +45,5 @@ class RawDataProcessorTest {
         );
 
         Assertions.assertEquals(expectedMap, actualProcessMap);
-    }
-
-    @Test
-    void checkTypesOperation_Ok() {
-        fileReader = new CsvFileReader(FILE_WITH_DIFFERENT_TYPES);
-        List<List<String>> actualLists = fileReader.readTransactions();
-
-        Assertions.assertEquals(
-                Operation.BALANCE.getCode(),
-                actualLists.get(ZERO_INDEX_POSITION).get(ZERO_INDEX_POSITION)
-        );
-        Assertions.assertEquals(
-                Operation.BALANCE.getCode(),
-                actualLists.get(FIRST_INDEX_POSITION).get(ZERO_INDEX_POSITION)
-        );
-        Assertions.assertEquals(
-                Operation.SUPPLY.getCode(),
-                actualLists.get(SECOND_INDEX_POSITION).get(ZERO_INDEX_POSITION)
-        );
-        Assertions.assertEquals(
-                Operation.PURCHASE.getCode(),
-                actualLists.get(THIRD_INDEX_POSITION).get(ZERO_INDEX_POSITION)
-        );
-        Assertions.assertEquals(
-                Operation.RETURN.getCode(),
-                actualLists.get(FOURTH_INDEX_POSITION).get(ZERO_INDEX_POSITION)
-        );
-        Assertions.assertEquals(
-                Operation.SUPPLY.getCode(),
-                actualLists.get(SEVENTH_INDEX_POSITION).get(ZERO_INDEX_POSITION)
-        );
-    }
-
-    @Test
-    void processWithNegativeBalance_NotOk() {
-        fileReader = new CsvFileReader(FILE_WITH_NEGATIVE_BALANCE);
-        List<List<String>> actualLists = fileReader.readTransactions();
-
-        assertThrows(NegativeFruitBalanceException.class,
-                () -> rawDataProcessor.process(actualLists));
     }
 }

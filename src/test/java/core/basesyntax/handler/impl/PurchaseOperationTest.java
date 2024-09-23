30655560f7cf79d68025138d59d8c45ba371da2e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-23 07:40:43 +0300

    PurchaseOperationTest.java

--- src/test/java/core/basesyntax/handler/impl/PurchaseOperationTest.java
+++ src/test/java/core/basesyntax/handler/impl/PurchaseOperationTest.java
@@ -3,11 +3,9 @@ package core.basesyntax.handler.impl;
 import core.basesyntax.model.FruitTransaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PurchaseOperationTest {
     private PurchaseOperation purchaseOperation;
@@ -27,5 +25,15 @@ class PurchaseOperationTest {
         transaction.setQuantity(30);
         purchaseOperation.apply(transaction);
         assertEquals(70, fruitStorage.get("banana"));
+    }
+
+    @Test
+    void apply_insufficientQuantity_notOk() {
+        fruitStorage.put("banana", 20);
+        FruitTransaction transaction = new FruitTransaction();
+        transaction.setFruit("banana");
+        transaction.setQuantity(30);
+        purchaseOperation.apply(transaction);
+        assertEquals(-10, fruitStorage.get("banana"));
     }
 }

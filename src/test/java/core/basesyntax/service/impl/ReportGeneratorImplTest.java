a7ac09ee52edf02e821ad41ba6c5685c0dabddc6
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-26 07:51:34 +0300

    ReportGeneratorImplTest.java

--- src/test/java/core/basesyntax/service/impl/ReportGeneratorImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportGeneratorImplTest.java
@@ -1,11 +1,12 @@
 package core.basesyntax.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import core.basesyntax.service.ReportGenerator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class ReportGeneratorImplTest {
     private ReportGenerator reportGenerator;

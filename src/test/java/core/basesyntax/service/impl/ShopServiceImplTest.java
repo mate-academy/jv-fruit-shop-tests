4eed7c0e8b2f8dd058d1b7380556f6537ddc605a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-26 13:37:27 +0300

    ShopServiceImplTest.java

--- src/test/java/core/basesyntax/service/impl/ShopServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ShopServiceImplTest.java
@@ -1,12 +1,12 @@
 package core.basesyntax.service.impl;
 
-import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.strategy.OperationStrategy;
-import core.basesyntax.strategy.impl.OperationStrategyImpl;
 import core.basesyntax.handler.OperationHandler;
 import core.basesyntax.handler.impl.BalanceOperation;
 import core.basesyntax.handler.impl.PurchaseOperation;
 import core.basesyntax.handler.impl.SupplyOperation;
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.strategy.OperationStrategy;
+import core.basesyntax.strategy.impl.OperationStrategyImpl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,9 +24,12 @@ public class ShopServiceImplTest {
     @BeforeEach
     public void setUp() {
         Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap = new HashMap<>();
-        operationHandlerMap.put(FruitTransaction.Operation.BALANCE, new BalanceOperation(Storage.fruitStorage));
-        operationHandlerMap.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation(Storage.fruitStorage));
-        operationHandlerMap.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation(Storage.fruitStorage));
+        operationHandlerMap.put(FruitTransaction.Operation.BALANCE, 
+                new BalanceOperation(Storage.fruitStorage));
+        operationHandlerMap.put(FruitTransaction.Operation.SUPPLY, 
+                new SupplyOperation(Storage.fruitStorage));
+        operationHandlerMap.put(FruitTransaction.Operation.PURCHASE, 
+                new PurchaseOperation(Storage.fruitStorage));
         OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlerMap);
         shopService = new ShopServiceImpl(operationStrategy);
     }

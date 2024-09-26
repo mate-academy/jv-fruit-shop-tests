6de1419ee63c0c4ea1fa597fd4602b6e5c2654c7
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-26 13:41:32 +0300

    ShopServiceImplTest.java

--- src/test/java/core/basesyntax/service/impl/ShopServiceImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ShopServiceImplTest.java
@@ -26,11 +26,11 @@ public class ShopServiceImplTest {
     public void setUp() {
         Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap = new HashMap<>();
         operationHandlerMap.put(FruitTransaction.Operation.BALANCE, 
-                new BalanceOperation(Storage.getFruitStorage()));
+                new BalanceOperation(Storage.fruitStorage));
         operationHandlerMap.put(FruitTransaction.Operation.SUPPLY, 
-                new SupplyOperation(Storage.getFruitStorage()));
+                new SupplyOperation(Storage.fruitStorage));
         operationHandlerMap.put(FruitTransaction.Operation.PURCHASE, 
-                new PurchaseOperation(Storage.getFruitStorage()));
+                new PurchaseOperation(Storage.fruitStorage));
         OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlerMap);
         shopService = new ShopServiceImpl(operationStrategy);
     }

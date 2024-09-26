e53f0fd0ecedeeb57303a80422f8376f77a9a51e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-26 10:47:37 +0300

    FileWriterImplTest.java

---
+++ src/test/java/core/basesyntax/util/impl/FileWriterImplTest.java
@@ -0,0 +1,32 @@
+package core.basesyntax.util.impl;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FileWriterImplTest {
+    private FileWriterImpl fileWriter;
+
+    @BeforeEach
+    public void setUp() {
+        fileWriter = new FileWriterImpl();
+    }
+
+    @Test
+    public void writeFile_validData_ok() throws IOException {
+        String data = "test content";
+        String filePath = "src/test/resources/outputFile.txt";
+        fileWriter.write(data, filePath);
+        String actual = new String(Files.readAllBytes(Paths.get(filePath)));
+        assertEquals(data, actual);
+    }
+
+    @Test
+    public void writeFile_invalidFilePath_throwsException() {
+        assertThrows(IOException.class, () -> fileWriter.write("data", "invalid/path.txt"));
+    }
+}

178cd82438293a92942c0ea402a80dc0a18b776d
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-26 10:52:07 +0300

    FileReaderImplTest.java

--- src/test/java/core/basesyntax/util/impl/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/util/impl/FileReaderImplTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;

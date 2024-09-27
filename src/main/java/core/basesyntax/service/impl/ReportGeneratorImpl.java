bdf532dbcc8e7d1100f17fb69e385a0788dee72e
IvanIKS <kryanus2005@gmail.com>
2024-09-27 10:23:47 +0300

    Fixed import order.

--- src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
+++ src/main/java/core/basesyntax/service/impl/ReportGeneratorImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.service.ReportGenerator;
 import core.basesyntax.service.ShopService;
-
 import java.util.List;
 import java.util.Map;
 

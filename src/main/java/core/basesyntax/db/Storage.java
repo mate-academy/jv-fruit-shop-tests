dbf3b7ce72ca70fb357ecf7c73a5e2ff240420eb
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-02 09:32:43 +0300

    Storage.java

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -1,8 +1,12 @@
 package core.basesyntax.db;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import core.basesyntax.model.FruitTransaction;
 
 public class Storage {
     public static final Map<String, Integer> fruitStorage = new HashMap<>();
+    public static final List<FruitTransaction> transactions = new ArrayList<>();
 }

2239a022a940997c69d6d3d9129d9efa1d4245c1
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-02 09:33:12 +0300

    FruitDaoImpl.java

--- src/main/java/core/basesyntax/dao/impl/FruitDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/FruitDaoImpl.java
@@ -1,18 +1,15 @@
 package core.basesyntax.dao.impl;
 
-import core.basesyntax.dao.FruitDao;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.FruitTransaction;
 import java.util.List;
 
-public class FruitDaoImpl implements FruitDao {
-    @Override
-    public void add(FruitTransaction transaction) {
-        Storage.transactions.add(transaction);
-    }
-
-    @Override
+public class FruitDaoImpl {
     public List<FruitTransaction> getAll() {
         return Storage.transactions;
+    }
+
+    public void add(FruitTransaction transaction) {
+        Storage.transactions.add(transaction);
     }
 }

3e64f3f9b003693d5960f12326b2e35f3b0751ca
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-02 09:44:24 +0300

    FileWriterImplTest.java

--- src/test/java/core/basesyntax/util/impl/FileWriterImplTest.java
+++ src/test/java/core/basesyntax/util/impl/FileWriterImplTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.util.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.junit.jupiter.api.BeforeEach;

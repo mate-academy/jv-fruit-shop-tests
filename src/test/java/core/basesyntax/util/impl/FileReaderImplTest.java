03f4b9617620a36c330822c4538ae1d3f0d8f48f
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-02 09:39:10 +0300

    FileReaderImplTest.java

--- src/test/java/core/basesyntax/util/impl/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/util/impl/FileReaderImplTest.java
@@ -1,38 +1,17 @@
 package core.basesyntax.util.impl;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.util.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FileReaderImplTest {
-    private static final String VALID_FILE_PATH = "src/test/resources/testFile.txt";
-    private static final String INVALID_FILE_PATH = "invalid/path.txt";
-    private static final String TEST_CONTENT = "test content";
-    private static final String TEST_DIRECTORY = "src/test/resources";
-    private FileReaderImpl fileReader;
-
-    @BeforeEach
-    public void setUp() throws IOException {
-        fileReader = new FileReaderImpl();
-        Files.createDirectories(Paths.get(TEST_DIRECTORY));
-        Files.write(Paths.get(VALID_FILE_PATH), List.of(TEST_CONTENT));
-    }
-
-    @Test
-    public void readFile_validFilePath_ok() throws IOException {
-        List<String> expected = List.of(TEST_CONTENT);
-        List<String> actual = fileReader.read(VALID_FILE_PATH);
-        assertEquals(expected, actual);
-    }
+    private static final String INVALID_PATH = "invalid/path.txt";
+    private FileReader fileReader = new FileReaderImpl();
 
     @Test
     public void readFile_invalidFilePath_throwsException() {
-        assertThrows(IOException.class, () -> fileReader.read(INVALID_FILE_PATH));
+        assertThrows(IOException.class, () -> fileReader.read(INVALID_PATH));
     }
 }

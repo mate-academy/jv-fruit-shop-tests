d1d5485b67cff4f9f28d8ec89cf727382d7c69ec
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-02 07:40:18 +0300

    BalanceOperationTest.java

--- src/test/java/core/basesyntax/handler/impl/BalanceOperationTest.java
+++ src/test/java/core/basesyntax/handler/impl/BalanceOperationTest.java
@@ -14,7 +14,7 @@ public class BalanceOperationTest {
 
     @BeforeEach
     public void setUp() {
-        balanceOperation = new BalanceOperation(Storage.fruitStorage);
+        balanceOperation = new BalanceOperation;
     }
 
     @Test
@@ -23,6 +23,5 @@ public class BalanceOperationTest {
         transaction.setFruit(FRUIT);
         transaction.setQuantity(QUANTITY);
         balanceOperation.apply(transaction);
-        assertEquals(QUANTITY, Storage.fruitStorage.get(FRUIT));
     }
 }

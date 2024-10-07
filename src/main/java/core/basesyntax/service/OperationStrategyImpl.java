e468f1b9c646b3d795812bde254a55baa600623f
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-10-07 19:22:44 +0200

    1

---
+++ src/main/java/core/basesyntax/service/OperationStrategyImpl.java
@@ -0,0 +1,19 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.operation.OperationHandler;
+
+import java.util.Map;
+
+public class OperationStrategyImpl implements OperationStrategy {
+    public OperationStrategyImpl(Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap) {
+        this.operationHandlerMap = operationHandlerMap;
+    }
+
+    private Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
+
+    @Override
+    public OperationHandler get(FruitTransaction.Operation operation) {
+        return operationHandlerMap.get(operation);
+    }
+}
---
+++ src/main/java/core/basesyntax/service/operation/ShopServiceImpl.java
@@ -0,0 +1,13 @@
+package core.basesyntax.service.operation;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.ShopService;
+
+import java.util.List;
+
+public class ShopServiceImpl implements ShopService {
+    @Override
+    public void process(List<FruitTransaction> transactions) {
+
+    }
+}

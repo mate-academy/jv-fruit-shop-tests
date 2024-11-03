032eeb5caac2b94fa2bba0b250dd3ec79f783abe
jekaG <jekagrusha@gmail.com>
2024-11-03 22:32:57 +0200

    create tests

---
+++ src/main/java/core/basesyntax/service/ShopService.java
@@ -0,0 +1,16 @@
+package core.basesyntax.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ShopService {
+    void process(List<FruitTransaction> transactions);
+
+    void addFruits(String fruit, int quantity);
+
+    void setFruits(String fruit, int quantity);
+
+    int getQuantity(String fruit);
+
+    Map<String, Integer> getFruits();
+}

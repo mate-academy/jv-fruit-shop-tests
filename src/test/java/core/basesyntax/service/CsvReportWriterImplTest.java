8b20df540d3e939537aab1bef08153103ee18859
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-11-25 04:30:48 +0100

    Final

--- src/test/java/core/basesyntax/service/CsvReportWriterImplTest.java
+++ src/test/java/core/basesyntax/service/CsvReportWriterImplTest.java
@@ -1,7 +1,6 @@
 package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
 import java.io.IOException;

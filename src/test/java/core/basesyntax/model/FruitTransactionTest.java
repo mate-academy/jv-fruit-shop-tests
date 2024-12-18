dae273420b3d97235482eac8127fc0280dc7c167
Roman Chygryn <chygryn.ra@gmail.com>
2024-12-18 15:57:35 +0100

    fix iomport

--- src/test/java/core/basesyntax/model/FruitTransactionTest.java
+++ src/test/java/core/basesyntax/model/FruitTransactionTest.java
@@ -1,6 +1,5 @@
 package core.basesyntax.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 

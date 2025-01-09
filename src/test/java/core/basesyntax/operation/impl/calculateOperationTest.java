e48ed6003a29f5e8438f02e7e5eec4c18040dc81
Teslenko Daniil <burgalhashem@gmail.com>
2025-01-10 00:31:45 +0200

    maked fix

--- src/test/java/core/basesyntax/operation/impl/calculateOperationTest.java
+++ src/test/java/core/basesyntax/operation/impl/calculateOperationTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class calculateOperationTest {
+class CalculateOperationTest {
     private OperationHandler balanceOperation = new BalanceOperation();
     private OperationHandler purchaseOperation = new PurchaseOperation();
     private OperationHandler returnOperation = new ReturnOperation();

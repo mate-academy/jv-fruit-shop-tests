8fed45d49a0e897cf330ec59879d814276173007
Kate Kraska <kate2002.kk42@gmail.com>
2025-01-15 15:19:25 +0200

    test for Shop Service1

--- src/main/java/core/basesyntax/model/FruitTransaction.java
+++ src/main/java/core/basesyntax/model/FruitTransaction.java
@@ -64,10 +64,10 @@ public class FruitTransaction {
 
     @Override
     public String toString() {
-        return "FruitTransaction{" +
-                "operation='" + operation + '\'' +
-                ", fruit='" + fruit + '\'' +
-                ", quantity=" + quantity +
-                '}';
+        return "FruitTransaction{"
+                + "operation='" + operation + '\''
+                + ", fruit='" + fruit + '\''
+                + ", quantity=" + quantity
+                + '}';
     }
 }

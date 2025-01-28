4319fc060b0f5d838b0882afcc5062aaada90392
Bohdan_SSX <mr.dimov77@gmail.com>
2025-01-28 14:40:04 +0200

    Added CsvFileReaderTest

---
+++ src/test/java/core/basesyntax/CsvFileReaderTest.java
@@ -0,0 +1,47 @@
+package core.basesyntax;
+
+import core.basesyntax.service.CsvFileReader;
+import core.basesyntax.service.impl.CsvFileReaderImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvFileReaderTest {
+    private static final String VALID_FILE = "src/main/resources/testFiles/validFile.csv";
+    private static final String EMPTY_FILE = "src/main/resources/testFiles/emptyFile.csv";
+    private static final String NON_EXISTING_FILE = "src/main/resources/testFiles/nonExistingFile.csv";
+    private final CsvFileReader reader = new CsvFileReaderImpl();
+    private final List<String> expectedData = fillExpectedDataArray();
+
+    @Test
+    public void read_existingValidFile_Ok() {
+        List<String> readData = reader.read(VALID_FILE);
+        Assertions.assertEquals(expectedData, readData);
+    }
+
+    @Test
+    public void read_existingEmptyFile_notOk() {
+        Assertions.assertThrows(RuntimeException.class, () -> reader.read(EMPTY_FILE), "Empty CSV file");
+
+    }
+
+    @Test
+    public void read_nonExistingFile_notOk() {
+        Assertions.assertThrows(RuntimeException.class, () -> reader.read(NON_EXISTING_FILE), "Cannot read file nonExistingFile.csv");
+    }
+
+    private List<String> fillExpectedDataArray() {
+        List<String> data = new ArrayList<>();
+        data.add("type,fruit,quantity");
+        data.add("b,banana,20");
+        data.add("b,apple,100");
+        data.add("s,banana,100");
+        data.add("p,banana,13");
+        data.add("r,apple,10");
+        data.add("p,apple,20");
+        data.add("p,banana,5");
+        data.add("s,banana,50");
+        return data;
+    }
+}

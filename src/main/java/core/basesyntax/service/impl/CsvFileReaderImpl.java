97fbd82f71d59568ae2cdc63be386cd693a604bf
Bohdan_SSX <mr.dimov77@gmail.com>
2025-01-28 14:03:39 +0200

    Changed CsvFileReaderImpl

--- src/main/java/core/basesyntax/service/impl/CsvFileReaderImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileReaderImpl.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CsvFileReaderImpl implements CsvFileReader {
@@ -15,7 +16,11 @@ public class CsvFileReaderImpl implements CsvFileReader {
     public List<String> read(String filename) {
         Path path = Paths.get(filename);
         try (Stream<String> lines = Files.lines(path)) {
-            return lines.toList();
+            List<String> lineList = lines.collect(Collectors.toList());
+            if (lineList.isEmpty()) {
+                throw new RuntimeException("Empty CSV file");
+            }
+            return lineList;
         } catch (IOException e) {
             throw new RuntimeException("Cannot read file: " + filename, e);
         }

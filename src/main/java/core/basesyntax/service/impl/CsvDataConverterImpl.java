cca6559a7eb8da4839b9a71c7a30b155ebe4e8fa
Bohdan_SSX <mr.dimov77@gmail.com>
2025-01-28 16:58:35 +0200

    Modified CsvDataConverterImpl. Added Pattern for fruit name

--- src/main/java/core/basesyntax/service/impl/CsvDataConverterImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvDataConverterImpl.java
@@ -5,10 +5,13 @@ import core.basesyntax.service.DataConverter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CsvDataConverterImpl implements DataConverter {
     private static final String COMMA_DELIMITER = ",";
     private static final int FRUIT_INDEX = 1;
+    private static final Pattern FRUIT_NAME_PATTERN = Pattern.compile("^?[a-zA-Z]?[a-z]+\\s?[a-zA-Z]?[a-z]+$");
     private static final int OPERATION_INDEX = 0;
     private static final int QUANTITY_INDEX = 2;
     private static final int VALUES_IN_LINE = 3;
@@ -30,11 +33,16 @@ public class CsvDataConverterImpl implements DataConverter {
         FruitTransaction fruitTransaction = new FruitTransaction();
         fruitTransaction.setOperation(FruitTransaction.Operation
                 .fromString(values.get(OPERATION_INDEX)));
-        if (values.get(FRUIT_INDEX) == null || values.get(FRUIT_INDEX).isEmpty()) {
+        if (values.get(FRUIT_INDEX) == null || values.get(FRUIT_INDEX).isEmpty() || isValidFruitName(values.get(FRUIT_INDEX))) {
             throw new RuntimeException("Fruit cannot be null or empty");
         }
         fruitTransaction.setFruit(values.get(FRUIT_INDEX));
         fruitTransaction.setQuantity(Integer.parseInt(values.get(QUANTITY_INDEX)));
         return fruitTransaction;
+    }
+
+    private boolean isValidFruitName(String fruit) {
+        Matcher matcher = FRUIT_NAME_PATTERN.matcher(fruit);
+        return matcher.matches();
     }
 }

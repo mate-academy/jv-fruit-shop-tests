f3abc532306dc7bd70acfc3e235644dca1c87fe7
Bohdan_SSX <mr.dimov77@gmail.com>
2025-01-31 18:03:15 +0200

    Checkstyle import + code string length fixes

--- src/main/java/core/basesyntax/service/impl/CsvDataConverterImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvDataConverterImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service.impl;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.DataConverter;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -11,7 +10,8 @@ import java.util.regex.Pattern;
 public class CsvDataConverterImpl implements DataConverter {
     private static final String COMMA_DELIMITER = ",";
     private static final int FRUIT_INDEX = 1;
-    private static final Pattern FRUIT_NAME_PATTERN = Pattern.compile("^?[a-zA-Z]?[a-z]+\\s?[a-zA-Z]?[a-z]+$");
+    private static final Pattern FRUIT_NAME_PATTERN
+            = Pattern.compile("^?[a-zA-Z]?[a-z]+\\s?[a-zA-Z]?[a-z]+$");
     private static final int OPERATION_INDEX = 0;
     private static final int QUANTITY_INDEX = 2;
     private static final int VALUES_IN_LINE = 3;
@@ -33,7 +33,8 @@ public class CsvDataConverterImpl implements DataConverter {
         FruitTransaction fruitTransaction = new FruitTransaction();
         fruitTransaction.setOperation(FruitTransaction.Operation
                 .fromString(values.get(OPERATION_INDEX)));
-        if (values.get(FRUIT_INDEX) == null || values.get(FRUIT_INDEX).isEmpty() || isValidFruitName(values.get(FRUIT_INDEX))) {
+        if (values.get(FRUIT_INDEX) == null || values.get(FRUIT_INDEX).isEmpty()
+                || !isValidFruitName(values.get(FRUIT_INDEX))) {
             throw new RuntimeException("Fruit cannot be null or empty");
         }
         fruitTransaction.setFruit(values.get(FRUIT_INDEX));
--- src/main/java/core/basesyntax/service/impl/CsvFileWriterImpl.java
+++ src/main/java/core/basesyntax/service/impl/CsvFileWriterImpl.java
@@ -1,14 +1,14 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.service.CsvFileWriter;
-
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CsvFileWriterImpl implements CsvFileWriter {
     @Override
     public void write(String content, String filename) {
-        try (BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(filename))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             writer.write(content);
         } catch (IOException e) {
             throw new RuntimeException("Failed to write to file: " + filename, e);

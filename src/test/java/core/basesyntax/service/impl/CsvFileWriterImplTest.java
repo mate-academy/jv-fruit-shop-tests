3cca076ed7adf24a69d98fa68689821eed3694f9
Bohdan_SSX <mr.dimov77@gmail.com>
2025-02-03 16:56:34 +0200

    write_validData_Ok method was made isolated for testing

--- src/test/java/core/basesyntax/service/impl/CsvFileWriterImplTest.java
+++ src/test/java/core/basesyntax/service/impl/CsvFileWriterImplTest.java
@@ -3,25 +3,41 @@ package core.basesyntax.service.impl;
 import core.basesyntax.service.CsvFileReader;
 import core.basesyntax.service.CsvFileWriter;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CsvFileWriterImplTest {
 
-    private static final String PATH_TO_FILE = "src/main/resources/testFiles/fileWriterTest.csv";
+    private static final String pathToFile = "src/test/resources/fileWriterTest.csv";
     private static final String REPORT = "fruit,quantity"
             + System.lineSeparator()
             + "banana,152"
             + System.lineSeparator()
             + "apple,90";
     private final CsvFileWriter fileWriter = new CsvFileWriterImpl();
-    private final CsvFileReader fileReader = new CsvFileReaderImpl();
+
 
     @Test
-    public void write_validData_Ok() {
-        fileWriter.write(REPORT, PATH_TO_FILE);
+    public void write_validData_Ok() throws IOException {
+        fileWriter.write(REPORT, pathToFile);
         String expectedAfterRead = "[fruit,quantity, banana,152, apple,90]";
-        assertEquals(expectedAfterRead, fileReader.read(PATH_TO_FILE).toString());
+        Path path = Paths.get(pathToFile);
+        Stream<String> lines = Files.lines(path);
+        List<String> actualRead = lines.toList();
+
+        assertEquals(expectedAfterRead, actualRead.toString());
     }
 
     @Test

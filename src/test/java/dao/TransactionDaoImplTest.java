a818aaef432d91b023935e19088c57a30dc8539e
denDy1224 <denchik12327@gmail.com>
2025-02-12 20:41:47 +0100

    configure_ide_code_style_config_test_file_name

--- src/test/java/dao/TransactionDaoImplTest.java
+++ src/test/java/dao/TransactionDaoImplTest.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TransactionDaoImplTests {
+class TransactionDaoImplTest {
     
     private TransactionDaoImpl transactionDao;
     

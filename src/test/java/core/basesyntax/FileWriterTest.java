985ce47725d031b0855a1424c3b1f2ccb2c66ff4
Geks <gekatonheyr@ukr.net>
2025-03-13 14:23:13 +0200

    Had covered fruit-shop solution with tests. Excluded Main.java from testing. Tried to cover all the business logic and write a couple of test for each method.

--- src/test/java/core/basesyntax/FileWriterTest.java
+++ src/test/java/core/basesyntax/FileWriterTest.java
@@ -2,10 +2,6 @@ package core.basesyntax;
 
 import core.basesyntax.db.FileWriter;
 import core.basesyntax.db.FileWriterImpl;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -24,25 +20,5 @@ public class FileWriterTest {
         FileWriter fileWriter = new FileWriterImpl();
         String data = "Some data to test";
         Assertions.assertThrows(RuntimeException.class, () -> fileWriter.write(data, fileName));
-    }
-
-    @Test
-    void write_fileAlreadyLocked_notOk() throws IOException {
-        String fileName = "src/main/resources/report.csv";
-        FileWriter fileWriter = new FileWriterImpl();
-        String data = "Some data to test";
-        FileLock fileLock = null;
-        try {
-            RandomAccessFile raf = new RandomAccessFile("src/main/resources/report.csv", "rw");
-            FileChannel channel = raf.getChannel();
-            fileLock = channel.lock();
-            Assertions.assertThrows(RuntimeException.class, () -> fileWriter.write(data, fileName));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (fileLock != null) {
-                fileLock.release();
-            }
-        }
     }
 }

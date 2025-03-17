028adc8570d90b50657aa9e81eacfadd8aedbb97
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-03-17 12:06:11 +0200

    fix module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,7 @@
-module $MODULE_NAME$ {
+module mate.academy.jvfruitshoptests {
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.engine;
+    requires org.junit.platform.commons;
+
+    opens core.basesyntax to org.junit.platform.commons, org.junit.jupiter.api;
 }

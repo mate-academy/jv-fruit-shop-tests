71d78428c5921bd7e07793f19eb78944e7c173ba
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-03-17 12:21:04 +0200

    delete module-info

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module mate.academy.jvfruitshoptests {
-    requires org.junit.jupiter.api;
-    requires org.junit.jupiter.engine;
-    requires org.junit.platform.commons;
-
-    opens core.basesyntax to org.junit.platform.commons, org.junit.jupiter.api;
-}

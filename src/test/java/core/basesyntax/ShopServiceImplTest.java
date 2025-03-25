a30be8ea5ef83e19635d67b61cfcf792f8285af4
Mateusz <mateusz.kiryk@o2.pl>
2025-03-25 18:47:12 +0100

    Eight solution

--- src/test/java/core/basesyntax/ShopServiceImplTest.java
+++ src/test/java/core/basesyntax/ShopServiceImplTest.java
@@ -3,8 +3,16 @@ package core.basesyntax;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.impl.ShopServiceImpl;
+import core.basesyntax.strategy.OperationHandler;
+import core.basesyntax.strategy.impl.BalanceOperation;
+import core.basesyntax.strategy.impl.PurchaseOperation;
+import core.basesyntax.strategy.impl.ReturnOperation;
+import core.basesyntax.strategy.impl.SupplyOperation;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -32,6 +40,73 @@ class ShopServiceImplTest {
     @Test
     void shouldThrowExceptionWhenRemovingMoreThanAvailable() {
         shopService.addFruit("apple", 10);
-        assertThrows(IllegalArgumentException.class, () -> shopService.removeFruit("apple", 15));
+        assertThrows(IllegalArgumentException.class, ()
+                -> shopService.removeFruit("apple", 15));
+    }
+
+    @Test
+    void shouldProcessSupplyTransactionCorrectly() {
+        Map<FruitTransaction.Operation, OperationHandler> handlers = new HashMap<>();
+        handlers.put(FruitTransaction
+                .Operation.SUPPLY, new SupplyOperation());
+        shopService = new ShopServiceImpl(handlers);
+
+        List<FruitTransaction> transactions = List.of(
+                new FruitTransaction(FruitTransaction
+                        .Operation.SUPPLY, "banana", 20)
+        );
+        shopService.process(transactions);
+        assertEquals(20, shopService
+                .getFruitQuantity("banana"));
+    }
+
+    @Test
+    void shouldProcessBalanceTransactionCorrectly() {
+
+        Map<FruitTransaction.Operation, OperationHandler> handlers = new HashMap<>();
+        handlers.put(FruitTransaction
+                .Operation.BALANCE, new BalanceOperation());
+        shopService = new ShopServiceImpl(handlers);
+
+        List<FruitTransaction> transactions = List.of(
+                new FruitTransaction(FruitTransaction
+                        .Operation.BALANCE, "banana", 30)
+        );
+        shopService.process(transactions);
+        assertEquals(30, shopService
+                .getFruitQuantity("banana"));
+    }
+
+    @Test
+    void shouldProcessPurchaseTransactionCorrectly() {
+        Map<FruitTransaction.Operation, OperationHandler> handlers = new HashMap<>();
+        handlers.put(FruitTransaction
+                .Operation.PURCHASE, new PurchaseOperation());
+        shopService = new ShopServiceImpl(handlers);
+
+        shopService.addFruit("orange", 15);
+
+        List<FruitTransaction> transactions = List.of(
+                new FruitTransaction(FruitTransaction
+                        .Operation.PURCHASE, "orange", 5)
+        );
+        shopService.process(transactions);
+        assertEquals(10, shopService.getFruitQuantity("orange"));
+    }
+
+    @Test
+    void shouldProcessReturnTransactionCorrectly() {
+        Map<FruitTransaction.Operation, OperationHandler> handlers = new HashMap<>();
+        handlers.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
+        shopService = new ShopServiceImpl(handlers);
+
+        shopService.addFruit("grape", 10);
+
+        List<FruitTransaction> transactions = List.of(
+                new FruitTransaction(FruitTransaction
+                        .Operation.RETURN, "grape", 5)
+        );
+        shopService.process(transactions);
+        assertEquals(15, shopService.getFruitQuantity("grape"));
     }
 }

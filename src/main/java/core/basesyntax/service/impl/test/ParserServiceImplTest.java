5e2e26b6813c997860d1edc6ca2e4a5bdeae2760
nazariiykolesnikov <nazarkolesnikov148@ukr.net>
2025-04-06 01:37:38 +0300

    first_commit

---
+++ src/main/java/core/basesyntax/service/impl/test/ParserServiceImplTest.java
@@ -0,0 +1,56 @@
+package core.basesyntax.service.impl.test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.ParserService;
+import java.util.ArrayList;
+import java.util.List;
+import core.basesyntax.service.impl.ParserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ParserServiceImplTest {
+    private ParserService parserService;
+
+    @BeforeEach
+    void setUp() {
+        parserService = new ParserServiceImpl();
+    }
+
+    @Test
+    void parse_null_notOk() {
+        assertThrows(NullPointerException.class, () -> {
+            parserService.parse(null);
+        });
+    }
+
+    @Test
+    void parse_csvData_ok() {
+        List<String> dataFromFile = new ArrayList<>();
+        dataFromFile.add("type,fruit,quantity");
+        dataFromFile.add("b,banana,20");
+        dataFromFile.add("b,apple,100");
+        dataFromFile.add("s,banana,100");
+
+        FruitTransaction firstTransaction = new FruitTransaction();
+        firstTransaction.setOperation(FruitTransaction.Operation.BALANCE);
+        firstTransaction.setFruit("banana");
+        firstTransaction.setQuantity(20);
+
+        FruitTransaction secondTransaction = new FruitTransaction();
+        secondTransaction.setOperation(FruitTransaction.Operation.BALANCE);
+        secondTransaction.setFruit("apple");
+        secondTransaction.setQuantity(100);
+
+        FruitTransaction thirdTransaction = new FruitTransaction();
+        thirdTransaction.setOperation(FruitTransaction.Operation.SUPPLY);
+        thirdTransaction.setFruit("banana");
+        thirdTransaction.setQuantity(100);
+
+        List<FruitTransaction> anticipatedResult = List.of(firstTransaction, secondTransaction, thirdTransaction);
+        List<FruitTransaction> actualResult = parserService.parse(dataFromFile);
+        assertArrayEquals(anticipatedResult.toArray(), actualResult.toArray());
+    }
+}

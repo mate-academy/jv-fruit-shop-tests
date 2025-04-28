acdcd437073d23d1f8a2d80aa1e6aa746522c882
vdsfww <loppprt@gmail.com>
2025-04-28 17:12:40 +0300

    fix error422

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -11,7 +11,6 @@ import core.basesyntax.service.FruitTransactionParser;
 import core.basesyntax.service.InventoryService;
 import core.basesyntax.service.ReportGeneratorService;
 import core.basesyntax.strategy.OperationStrategyProvider;
-
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;

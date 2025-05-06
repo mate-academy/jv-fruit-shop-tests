88f510a466aa35b555175dc33d2a73335742acf8
Maks <lisichenkowork@gmail.com>
2025-05-06 19:36:55 +0200

    fourth commit

--- src/main/java/core/basesyntax/fileservice/FileWriterImpl.java
+++ src/main/java/core/basesyntax/fileservice/FileWriterImpl.java
@@ -3,7 +3,6 @@ package core.basesyntax.fileservice;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
